efcce73fb99da8affd07fb32556ad5f02eab6170
tranglich <tranglich@gmail.com>
2008-11-16 16:13:06 +0000

    No more validation data for Group model.

--- app/models/group.php
+++ app/models/group.php
@@ -3,12 +3,6 @@ class Group extends AppModel {
 
 	var $name = 'Group';
 	var $actsAs = array('Acl' => array('requester'));
-	
-	var $validate = array(
-		'name' => array('alphanumeric'),
-		'created' => array('date'),
-		'modified' => array('date')
-	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $hasMany = array(
