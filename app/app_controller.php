f6cb811dd50764aab18ce7f7440d98966ac5f4ed
tranglich <tranglich@gmail.com>
2008-12-22 00:44:46 +0000

    LogsHelper

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,7 +1,7 @@
 <?php 
 class AppController extends Controller { 
     var $components = array('Acl','Auth','Permissions');
-	var $helpers = array('Sentences','Date', 'Html', 'Form');
+	var $helpers = array('Sentences','Date', 'Html', 'Form', 'Logs');
 	
     function beforeFilter() { 
 		Security::setHash('md5');
