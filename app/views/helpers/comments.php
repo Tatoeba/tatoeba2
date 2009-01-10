cccdab34b3b49c5215719ddd30c652feeabd9d4b
tranglich <tranglich@gmail.com>
2009-01-10 15:09:58 +0000

    Tooltip for sentence comments

--- app/views/helpers/comments.php
+++ app/views/helpers/comments.php
@@ -1,7 +1,7 @@
 <?php
 class CommentsHelper extends AppHelper {
 
-	var $helpers = array('Form', 'Date');
+	var $helpers = array('Form', 'Date', 'Tooltip');
 
 	function displayComment($username, $datetime, $comment){
 		echo '<div class="comment">';
@@ -18,8 +18,10 @@ class CommentsHelper extends AppHelper {
 	
 	function displayCommentForm($sentence_id){
 		echo $this->Form->create('SentenceComment', array("action"=>"save"));
+		$this->Tooltip->display(__('Even though you can comment on the whole group of sentences, keep in mind that your comment will be <strong>linked only to the main sentence</strong> (at the top).',true));
+		__('Add a comment : ');
 		echo $this->Form->input('sentence_id', array("type"=>"hidden", "value"=>$sentence_id));
-		echo $this->Form->input('text', array("label"=> __('Add a comment : ',true)));
+		echo $this->Form->input('text', array("label"=> ""));
 		echo $this->Form->end('Submit');
 	}
 	
