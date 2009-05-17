e3c4a44f8209e16904d3ae0a2fb13fc2955b3936
tranglich <tranglich@gmail.com>
2009-05-17 23:04:55 +0000

    Took out indirect translations.

--- app/models/translation.php
+++ app/models/translation.php
@@ -2,21 +2,5 @@
 class Translation extends AppModel{
 	var $name = 'Translation';
 	var $useTable = 'sentences';
-	
-	var $hasAndBelongsToMany = array(
-		'IndirectTranslation' => array(
-			'className' => 'IndirectTranslation',
-			'joinTable' => 'sentences_translations',
-			'foreignKey' => 'translation_id',
-			'associationForeignKey' => 'sentence_id',
-			'conditions' => '',
-			'order' => '',
-			'limit' => '',
-			'unique' => true,
-			'finderQuery' => '',
-			'deleteQuery' => '',
-			'insertQuery' => ''
-		)
-	);
 }
 ?>
