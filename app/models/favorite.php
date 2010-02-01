8126857e6d2ba3837a2382855328c9b82ab34b5c
Allan SIMON <allan.simon@supinfo.com>
2010-02-01 19:49:49 +0000

    code sniff for favorite and sinogram model

--- app/models/favorite.php
+++ app/models/favorite.php
@@ -1,52 +1,79 @@
 <?php
-/*
-    Tatoeba Project, free collaborativ creation of languages corpuses project
-    Copyright (C) 2009  TATOEBA Project(should be changed)
+/**
+ * Tatoeba Project, free collaborativ creation of languages corpuses project
+ * Copyright (C) 2009 Allan SIMON <allan.simon@supinfo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5 
+ *
+ * @category PHP
+ * @package  Tatoeba
+ * @author   Allan Simon <allan.simon@supinfo.com>
+ * @license  Affero General Public License
+ * @link     http://tatoeba.org
+ */
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/**
+ * Controller for contributions.
+ *
+ * @category Favorite
+ * @package  Models
+ * @author   Allan Simon <allan.simon@supinfo.com>
+ * @license  Affero General Public License
+ * @link     http://tatoeba.org
+ */
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+class Favorite extends AppModel
+{
+    public $name = 'Favorite';
+    public $useTable = 'sentences';
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-class Favorite extends AppModel{
-	var $name = 'Favorite';
-	var $useTable = 'sentences';
-
-	var $actsAs = array('ExtendAssociations','Containable');
+    public $actsAs = array('ExtendAssociations','Containable');
 
 
-	var $hasAndBelongsToMany = array(
-		'User' => array(
-			'className' => 'User',
-			'joinTable' => 'favorites_users',
-			'foreignKey' => 'favorite_id',
-			'associationForeignKey' => 'user_id',
-			'conditions' => '',
-			'order' => '',
-			'limit' => '',
-			'unique' => true,
-			'finderQuery' => '',
-			'deleteQuery' => '',
-			'insertQuery' => ''
-		)
-	);
-    /*
-    ** get number of favorite sentence of a user
-    */
-    function numberOfFavoritesOfUser($userId){
-        $result = $this->query("
+    public $hasAndBelongsToMany = array(
+        'User' => array(
+            'className' => 'User',
+            'joinTable' => 'favorites_users',
+            'foreignKey' => 'favorite_id',
+            'associationForeignKey' => 'user_id',
+            'conditions' => '',
+            'order' => '',
+            'limit' => '',
+            'unique' => true,
+            'finderQuery' => '',
+            'deleteQuery' => '',
+            'insertQuery' => ''
+        )
+    );
+
+    /**
+     * get number of favorite sentence of a user
+     *
+     * @param int $userId the user's id
+     *
+     * @return array
+     */
+    public function numberOfFavoritesOfUser($userId)
+    {
+        $result = $this->query(
+            "
             SELECT count(user_id) AS numberOfFavorites FROM favorites_users
             WHERE user_id = $userId
-        ");
+            "
+        );
 
         return $result[0][0]["numberOfFavorites"];
     }
@@ -54,12 +81,13 @@ class Favorite extends AppModel{
     /**
      * retrieve all favorites of a user
      *
-     * @param int $user_id user to retrieve favorites 
+     * @param int $userId user to retrieve favorites 
      * 
      * @return array 
      */
 
-     public function getAllFavoritesOfUser($userId){
+    public function getAllFavoritesOfUser($userId)
+    {
         
         // TODO HACK SPOTTED the main things here is favorite, not user
         // so the query should be reviewed moreover it's all but optimized 
@@ -69,17 +97,19 @@ class Favorite extends AppModel{
         $user = $user->read();
 
         return $user;
-     }
+    }
 
     /**
      * add a sentence to current user's ones
      *
      * @param int $sentenceId id of the sentence to favorite
+     * @param int $userId     id of the user who want to add
      *
      * @return bool
      */
 
-    public function addFavorite($sentenceId, $userId){
+    public function addFavorite($sentenceId, $userId)
+    {
         return $this->Favorite->habtmAdd(
             'User',
             $sentenceId,
@@ -88,14 +118,16 @@ class Favorite extends AppModel{
     } 
 
     /**
-     * add a sentence to current user's ones
+     * remove a sentence to current user's ones
      *
-     * @param int $sentenceId id of the sentence to favorite
+     * @param int $sentenceId id of the sentence to unfavorite
+     * @param int $userId     id of the user who want to remove
      *
      * @return bool
      */
 
-    public function removeFavorite($sentenceId, $userId){
+    public function removeFavorite($sentenceId, $userId)
+    {
         return $this->Favorite->habtmDelete(
             'User',
             $sentenceId,
