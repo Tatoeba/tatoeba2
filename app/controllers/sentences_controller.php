66cabec435bca8af1a1f2c26a91d6735b7be161e
tranglich <tranglich@gmail.com>
2010-02-06 23:38:34 +0000

    Added a space in adopt flash message string.

--- app/controllers/sentences_controller.php
+++ app/controllers/sentences_controller.php
@@ -302,7 +302,7 @@ class SentencesController extends AppController
         if ($this->Sentence->save($data)) {
             $this->flash(
                 __(
-                    'You are now the owner of this sentence and can modify it as'.
+                    'You are now the owner of this sentence and can modify it as '.
                     'you wish. It is your responsibility to make sure that it '.
                     ' doesn\'t have any mistake and, if possible, is not linked to '.
                     'wrong translations.', true
