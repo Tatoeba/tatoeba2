572ab9cbdb90ec0b42aecd76a83f060dca659b42
Allan SIMON <allan.simon@supinfo.com>
2010-02-16 19:31:40 +0000

    cosmectic commit

--- app/webroot/js/favorites.add.js
+++ app/webroot/js/favorites.add.js
@@ -20,85 +20,84 @@
 
 $(document).ready(function(){
 
-// there's only one  "click" function for favorite/unfavorite ( and a test inside to make the difference
-// instead of two "click" functions because of the way JQuery works
-// so as it is done only one time at the beginning and as we change the class of the block after the first loading
-//   jquery can't add to it "onclick" 
-// 
-// so to get trough this we need to make a single function and a test for each class
+    var host = self.location.hostname;
+    var port = self.location.port;
+// there's only one  "click" function for favorite/unfavorite
+// ( and a test inside to make the difference instead of two 
+// "click" functions because of the way JQuery works so as it
+// is done only one time at the beginning and as we change the
+// class of the block after the first loading jquery can't add
+// to it  "onclick"  so to get trough this we need to make a
+// single function and a test for each class
 
-	$(".favorite").click(function(){
-		var favorite_id = $(this).parent().attr("id").slice(1);
-		var favorite_option = $(this);
-		
-		/*******************************
-		/ the sentence can be favorite 
-		********************************/
+    $(".favorite").click(function(){
+        var favorite_id = $(this).parent().attr("id").slice(1);
+        var favorite_option = $(this);
+        
+        /*******************************
+        / the sentence can be favorite 
+        ********************************/
 
-		if (favorite_option.hasClass("add")){
-			
-			$("#_"+favorite_id+"_in_process").show();
+        if (favorite_option.hasClass("add")){
+            
+            $("#_"+favorite_id+"_in_process").show();
 
-			$.post("http://" + self.location.hostname + ":" + self.location.port + "/favorites/add_favorite/"+ favorite_id
-				, {}	
-				,function(data){
-					// if add retrieve no data , then for a reason or an other, the sentence couldn't have been added 
-					// so we change nothing
-					if ( null != data && "" != data ){
- 						// this second test is here because with debug enable, the function always retrieve data
-						// so we test if the retrieving data is an <a> </a> 
-						if ( data[1] == "a" ){
-							
-							favorite_option.html(data);
-							favorite_option.removeClass("add").addClass("remove");
-							
-						}
-					}
-		
-					$("#_"+favorite_id+"_in_process").hide();
-					
-				}
-			);
-			
+            $.post("http://" + host + ":" + port + "/favorites/add_favorite/"+ favorite_id
+                , {}    
+                ,function(data){
+                    // if add retrieve no data , then for a reason or
+                    // an other, the sentence couldn't have been added 
+                    // so we change nothing
+                    if ( null != data && "" != data ){
+                        // this second test is here because with debug
+                        // enable, the function always retrieve data
+                        // so we test if the retrieving data is an <a> </a> 
+                        if ( data[1] == "a" ){
+                            
+                            favorite_option.html(data);
+                            favorite_option.removeClass("add").addClass("remove");
+                            
+                        }
+                    }
+        
+                    $("#_"+favorite_id+"_in_process").hide();
+                    
+                }
+            );
+            
 
-		}
-	
-		/*******************************
-		/ the sentence can be unfavorite 
-		********************************/
+        }
+    
+        /*******************************
+        / the sentence can be unfavorite 
+        ********************************/
 
-		else if (favorite_option.hasClass("remove")){
-			
-			$("#_"+favorite_id+"_in_process").show();
-			
-			$.post("http://" + self.location.hostname + ":" + self.location.port + "/favorites/remove_favorite/"+ favorite_id
-				, {}
-				,function(data){
-					// if add retrieve no data , then for a reason or an other, the sentence couldn't have been added					
-					// so we change nothing
-					if ( null != data && "" != data ){
- 						// this second test is here because with debug enable, the function always retrieve data
-						// so we test if the retrieving data is an <a> </a> 
-						if ( data[1] == "a" ){
-							
-							favorite_option.html(data);
-							favorite_option.removeClass("remove").addClass("add");
-							
-						}
+        else if (favorite_option.hasClass("remove")){
+            
+            $("#_"+favorite_id+"_in_process").show();
+            
+            $.post("http://" + host + ":" + port + "/favorites/remove_favorite/"+ favorite_id
+                , {}
+                ,function(data){
+                    // if add retrieve no data , then for a reason or an
+                    // other, the sentence couldn't have been added                    
+                    // so we change nothing
+                    if ( null != data && "" != data ){
+                        // this second test is here because with debug
+                        // enable, the function always retrieve data
+                        // so we test if the retrieving data is an <a> </a> 
+                        if ( data[1] == "a" ){
 
-					}
-		
-					$("#_"+favorite_id+"_in_process").hide();
-				}
-			);
-			
+                            favorite_option.html(data);
+                            favorite_option.removeClass("remove").addClass("add");
 
-		}
+                        }
+                    }
+                    $("#_"+favorite_id+"_in_process").hide();
+                }
+            );
+            
 
-
-
-	});
-
-
-
+        }
+    });
 });
