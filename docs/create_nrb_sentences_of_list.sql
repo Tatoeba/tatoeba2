562dcab96e24056bbb433529781039e151e8526a
Biptaste <biptaste@gmail.com>
2010-03-16 21:33:39 +0000

    re #126 add docs/create_nrb_sentences_of_list.sql

---
+++ docs/create_nrb_sentences_of_list.sql
@@ -0,0 +1,37 @@
+Delimiter |
+-- DROP PROCEDURE create_nrb_sentences_of_list | --
+CREATE PROCEDURE create_nrb_sentences_of_list()
+BEGIN
+
+    DECLARE done INT DEFAULT 0;
+    DECLARE list_id INT(11);
+    DECLARE nbr_sentences MEDIUMINT(8);
+    
+    DECLARE curseur_list CURSOR FOR
+        SELECT `sentences_lists`.`id`
+        FROM `sentences_lists`;
+    
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done = 1;
+    
+    OPEN curseur_list ;
+    
+    ALTER TABLE  `sentences_lists`
+    ADD  `numberOfSentences` MEDIUMINT( 8 ) UNSIGNED NOT NULL DEFAULT  '0' AFTER  `user_id`;
+    
+    REPEAT
+        FETCH curseur_list INTO list_id ;
+        
+        SELECT COUNT(`sentences_sentences_lists`.`sentences_list_id`) INTO nbr_sentences
+            FROM `sentences_lists`
+            LEFT JOIN `sentences_sentences_lists`
+            ON `sentences_lists`.`id` = `sentences_sentences_lists`.`sentences_list_id`
+            WHERE `sentences_lists`.`id` = list_id
+            GROUP BY `sentences_lists`.`id`;
+        
+        UPDATE `sentences_lists` SET `sentences_lists`.`numberOfSentences` = nbr_sentences
+            WHERE `sentences_lists`.`id` = list_id;
+    
+    UNTIL done    
+    END REPEAT;
+
+END |
