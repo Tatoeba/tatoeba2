31c2ddf4ed02a2e396dc6f8150fc8e7b3be92403
tranglich <tranglich@gmail.com>
2010-04-11 16:43:14 +0000

    re #216.
    Modified code into something that is a bit clearer. Made it all static.

--- app/models/current_user.php
+++ app/models/current_user.php
@@ -1,72 +1,92 @@
 <?php
-/*
- * Static methods that can be used to retrieve the logged in user
- * from anywhere
+/**
+ * Tatoeba Project, free collaborative creation of multilingual corpuses project
+ * Copyright (C) 2010  HO Ngoc Phuong Trang <tranglich@gmail.com>
  *
- * Copyright (c) 2008 Matt Curry
- * www.PseudoCoder.com
- * http://github.com/mcurry/cakephp/tree/master/snippets/static_user
- * http://www.pseudocoder.com/archives/2008/10/06/accessing-user-sessions-from-models-or-anywhere-in-cakephp-revealed/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * @author      Matt Curry <matt@pseudocoder.com>
- * @license     MIT
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  Tatoeba
+ * @author   HO Ngoc Phuong Trang <tranglich@gmail.com>
+ * @license  Affero General Public License
+ * @link     http://tatoeba.org
  */
  
+/**
+ * Static class that stores the Auth information of the user. This is the only
+ * solution I found to be able to easily access the Auth::user() information from
+ * everywhere. It also enables to retrieve the current user's permissions from 
+ * everywhere. 
+ *
+ * NOTE: Still not sure if it's the best idea, regarding the permissions part.
+ *
+ * @category CurrentUser
+ * @package  Models
+ * @author   HO Ngoc Phuong Trang <tranglich@gmail.com>
+ * @license  Affero General Public License
+ * @link     http://tatoeba.org
+ */
 class CurrentUser extends AppModel
 {
     public $useTable = null;
     
-    public public $hasMany = array(
-        'SentenceComments',
-        'Contributions',
-        'Sentences',
-        'SentencesLists'
-    );
+    private static $auth;
     
-    function &getInstance($user=null) 
+    
+    /**
+     * Store the Auth information of the user, i.e. the value is the one returned 
+     * by Auth::user(). Used in app_controller.php, in beforeFilter().
+     * 
+     * @param array $user 
+     *
+     * @return void
+     */
+    public static function store($user)
     {
-        static $instance = array();
-
-        if ($user) {
-        $instance[0] =& $user;
-        }
-
-        if (!$instance) {
-        trigger_error(__("User not set.", true), E_USER_WARNING);
-        return false;
-        }
-
-        return $instance[0];
+        self::$auth = $user;
     }
-     
-    function store($user) 
-    {
-        if (empty($user)) {
-        return false;
-        }
-
-        CurrentUser::getInstance($user);
-    }
-     
-    function get($path) {
-        $_user =& CurrentUser::getInstance();
-        
+    
+    
+    /**
+     * Returns value of the Auth::user() array. Slightly modified version of:
+     *
+     * Copyright (c) 2008 Matt Curry
+     * www.PseudoCoder.com
+     * http://github.com/mcurry/cakephp/tree/master/snippets/static_user
+     * http://www.pseudocoder.com/archives/2008/10/06/accessing-user-sessions-from-models-or-anywhere-in-cakephp-revealed/
+     *
+     * @author      Matt Curry <matt@pseudocoder.com>
+     * @license     MIT
+     */
+    public static function get($path) {
         $path = str_replace('.', '/', $path);
         if (strpos($path, 'User') !== 0) {
-        $path = sprintf('User/%s', $path);
+            $path = sprintf('User/%s', $path);
         }
-
+        
         if (strpos($path, '/') !== 0) {
-        $path = sprintf('/%s', $path);
+            $path = sprintf('/%s', $path);
         }
 
-        $value = Set::extract($path, $_user);
+        $value = Set::extract($path, self::$auth);
 
         if (!$value) {
-        return false;
+            return false;
         }
-
+        
         return $value[0];
     }
     
@@ -76,9 +96,9 @@ class CurrentUser extends AppModel
      * 
      * @return bool
      */
-    public function isAdmin()
+    public static function isAdmin()
     {
-        return CurrentUser::get('group_id') == 1;
+        return self::get('group_id') == 1;
     }
     
     
@@ -87,9 +107,9 @@ class CurrentUser extends AppModel
      * 
      * @return bool
      */
-    public function isTrusted()
+    public static function isTrusted()
     {
-        return CurrentUser::get('group_id') < 4;
+        return self::get('group_id') < 4;
     }
     
     
@@ -100,21 +120,23 @@ class CurrentUser extends AppModel
      * 
      * @return bool
      */
-    public function isOwnerOfSentence($sentenceId)
+    public static function isOwnerOfSentence($sentenceId)
     {
-        $sentence = $this->Sentence->find(
+        $Sentence = ClassRegistry::init('Sentence');
+        $result = $Sentence->find(
             'first',
             array(
                 'fields' => array(),
                 'conditions' => array(
                     'Sentence.id' => $sentenceId,
-                    'Sentence.user_id' => CurrentUser::get('id')
+                    'Sentence.user_id' => self::get('id')
                 ),
                 'contain' => array()
             )
         );
-        return !empty($sentence);
+        return !empty($result);
     }
+    
     
     /**
      * Indicates if current user can link/unlink translations to the sentence of
@@ -124,15 +146,15 @@ class CurrentUser extends AppModel
      * 
      * @return bool
      */
-    public function canLinkAndUnlink($sentenceId)
+    public static function canLinkAndUnlink($sentenceId)
     {
-        if (CurrentUser::isAdmin()) {
+        if (self::isAdmin()) {
             return true;
         }
         
-        $sentenceBelongsToUser = CurrentUser::isOwnerOfSentence($sentenceId);
+        $sentenceBelongsToUser = self::isOwnerOfSentence($sentenceId);
         
-        return $sentenceBelongsToUser && CurrentUser::isTrusted();
+        return $sentenceBelongsToUser && self::isTrusted();
     }
 }
 ?>
