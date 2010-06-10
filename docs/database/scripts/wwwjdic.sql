43dd2b5533768dcf7251f3364f7c2de4e8bf51bd
tranglich <tranglich@gmail.com>
2010-06-10 21:45:19 +0000

    Fixing export script for wwwjdic.

--- docs/database/scripts/wwwjdic.sql
+++ docs/database/scripts/wwwjdic.sql
@@ -1,10 +1,6 @@
-SELECT st.sentence_id, st.translation_id, s.text, t.text, sa.text
-FROM sentences s 
-	LEFT JOIN sentence_annotations sa ON s.id = sa.sentence_id
-	LEFT JOIN sentences_translations st ON st.sentence_id = s.id
-	LEFT JOIN sentences t ON st.translation_id = t.id
-WHERE s.lang = 'jpn' 
-    AND (t.lang = 'eng' OR t.text IS NULL)
-    AND sa.text IS NOT NULL
-INTO OUTFILE '/home/tatoeba/www/app/tmp/wwwjdic.csv'
-FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+SELECT sa.sentence_id, sa.meaning_id, s.text, t.text, sa.text
+FROM sentence_annotations sa 
+  JOIN sentences s ON s.id = sa.sentence_id
+  JOIN sentences t ON t.id = sa.meaning_id
+    WHERE sa.meaning_id != -1
+INTO OUTFILE '/var/tmp/wwwjdic.csv';
