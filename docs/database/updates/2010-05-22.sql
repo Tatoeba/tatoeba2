c6fae16f7a0f4720524dbfc5f5bfd1715a1634b6
tranglich <tranglich@gmail.com>
2010-06-18 19:25:50 +0000

    Added comment, just for the record, in 2010-05-22.sql

--- docs/database/updates/2010-05-22.sql
+++ docs/database/updates/2010-05-22.sql
@@ -2,3 +2,6 @@
 ALTER TABLE contributions ADD id_temps INT UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (id_temps);
 ALTER TABLE contributions DROP COLUMN contributions.id ;
 ALTER TABLE contributions change id_temps id  INT UNSIGNED NOT NULL AUTO_INCREMENT ;
+
+-- NOTE: Also execute last_contributions_triggers.sql in 'scripts' folder.
+-- The table last_contributions in that script.
