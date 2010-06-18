0d5e0cf35f231befabdf94030e2cfab9a937370a
tranglich <tranglich@gmail.com>
2010-06-18 19:31:48 +0000

    Added SQL to export tags.

--- docs/database/scripts/weekly_exports.sql
+++ docs/database/scripts/weekly_exports.sql
@@ -11,3 +11,8 @@ INTO OUTFILE '/var/tmp/sentences.csv';
 -- Links between sentences
 SELECT sentence_id, translation_id FROM `sentences_translations`
 INTO OUTFILE '/var/tmp/links.csv';
+
+-- Sentences tags
+SELECT ts.sentence_id, t.name FROM `tags` t JOIN `tags_sentences` ts
+  ON t.id = ts.tag_id
+INTO OUTFILE '/var/tmp/tags.csv';
