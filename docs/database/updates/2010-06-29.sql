85893f18aecc740fef11569de6cc36acb9718ac5
Allan SIMON <allan.simon@supinfo.com>
2010-06-30 17:38:19 +0000

    re #266 added an index to the table to speed up filtering on audio

--- docs/database/updates/2010-06-29.sql
+++ docs/database/updates/2010-06-29.sql
@@ -1,4 +1,4 @@
 -- add support for audio in database --
 
 ALTER TABLE `sentences` ADD COLUMN `hasaudio` ENUM('no','from_users', 'shtooka') NOT NULL DEFAULT 'no';
-
+CREATE INDEX hasaudio_idx ON sentences (hasaudio) ;
