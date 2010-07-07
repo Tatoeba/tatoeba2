223d271f2821a21ab0da98755508eb0d15a9f962
Biptaste <biptaste@gmail.com>
2010-07-07 13:56:45 +0000

    re #298 optimize code to filter sentence which aren't associated language

--- app/controllers/sentences_controller.php
+++ app/controllers/sentences_controller.php
@@ -542,35 +542,24 @@ class SentencesController extends AppController
         $this->helpers[] = 'ShowAll'; 
         
         $model = 'Sentence';
+        
         if ($lang == 'und') {
-            $pagination = array(
-                'Sentence' => array(
-                    'fields' => array(
-                        'id',
-                    ),
-                    'conditions' => array(
-                        'lang' => null,
-                    ),
-                    'contain' => array(),
-                    'limit' => 10,
-                    'order' => "Sentence.id desc"
-                )
-            );
-        } else {
-            $pagination = array(
-                'Sentence' => array(
-                    'fields' => array(
-                        'id',
-                    ),
-                    'conditions' => array(
-                        'lang' => $lang,
-                    ),
-                    'contain' => array(),
-                    'limit' => 10,
-                    'order' => "Sentence.id desc"
-                )
-            );
+        	$lang = null;
         }
+
+        $pagination = array(
+            'Sentence' => array(
+                'fields' => array(
+                    'id',
+                ),
+                'conditions' => array(
+                    'lang' => $lang,
+                ),
+                'contain' => array(),
+                'limit' => 10,
+                'order' => "Sentence.id desc"
+            )
+        );
 
         // filter or not sentences-with-audio-only  
         $audioOnly = false ;
@@ -581,41 +570,23 @@ class SentencesController extends AppController
 
 
         if (!empty($notTranslatedIn) && $notTranslatedIn != 'none') {
-            if ($lang == 'und') {
-                $model = 'SentenceNotTranslatedIn';
-                $pagination = array(
-                    'SentenceNotTranslatedIn' => array(
-                        'fields' => array(
-                            'id',
-                        ),
-                        'conditions' => array(
-                            'source' => null,
-                            'translatedIn' => $translationLang,
-                            'notTranslatedIn' => $notTranslatedIn,
-                            'audioOnly' => $audioOnly,
-                        ),
-                        'contain' => array(),
-                        'limit' => 10,
-                    )
-                );
-            } else {
-                $model = 'SentenceNotTranslatedIn';
-                $pagination = array(
-                    'SentenceNotTranslatedIn' => array(
-                        'fields' => array(
-                            'id',
-                        ),
-                        'conditions' => array(
-                            'source' => $lang,
-                            'translatedIn' => $translationLang,
-                            'notTranslatedIn' => $notTranslatedIn,
-                            'audioOnly' => $audioOnly,
-                        ),
-                        'contain' => array(),
-                        'limit' => 10,
-                    )
-                );
-            }
+
+            $model = 'SentenceNotTranslatedIn';
+            $pagination = array(
+                'SentenceNotTranslatedIn' => array(
+                    'fields' => array(
+                        'id',
+                    ),
+                    'conditions' => array(
+                        'source' => $lang,
+                        'translatedIn' => $translationLang,
+                        'notTranslatedIn' => $notTranslatedIn,
+                        'audioOnly' => $audioOnly,
+                    ),
+                    'contain' => array(),
+                    'limit' => 10,
+                )
+            );
         }
 
         $allSentences = $this->_common_sentences_pagination(
