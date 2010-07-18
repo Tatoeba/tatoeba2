c9b54e4f111a039d0dc35428009dffd93b0bacc6
tranglich <tranglich@gmail.com>
2010-07-18 17:15:01 +0000

    Simply took out href.

--- app/views/helpers/menu.php
+++ app/views/helpers/menu.php
@@ -84,7 +84,7 @@ class MenuHelper extends AppHelper
                 );
             });
             </script>
-            <a href="#self" ><?php echo $translateButton;?></a>
+            <a href="#toto" ><?php echo $translateButton;?></a>
            <?php 
         } else {
             echo $this->Html->link(
@@ -201,7 +201,7 @@ class MenuHelper extends AppHelper
             });
             </script>
             
-            <a href="#self" ><?php echo $adoptImage; ?></a>
+            <a href="#toto" ><?php echo $adoptImage; ?></a>
             <?php
         } else {
             echo $this->Html->link(
@@ -270,7 +270,7 @@ class MenuHelper extends AppHelper
             });
             </script>
         
-            <a href="#self" ><?php echo $favoriteImage;?></a>
+            <a href="#toto" ><?php echo $favoriteImage;?></a>
 
             <?php
         } else {
