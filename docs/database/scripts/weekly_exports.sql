637b93ada52a6522b2113b242d1e340913a8fbdb
tranglich <tranglich@gmail.com>
2011-07-11 17:06:02 +0000

    re #365.
    Added SQL to export sentences with username, created and modified date.
    File will be named "sentences_detailed.csv".

--- docs/database/scripts/weekly_exports.sql
+++ docs/database/scripts/weekly_exports.sql
@@ -8,6 +8,11 @@ INTO OUTFILE '/var/tmp/jpn_indices.csv';
 SELECT id, lang, text FROM `sentences`
 INTO OUTFILE '/var/tmp/sentences.csv';
 
+-- Sentences with more data
+SELECT s.id, s.lang, s.text, u.username, s.created, s.modified 
+FROM `sentences` s LEFT JOIN `users` u ON s.user_id = u.id
+INTO OUTFILE '/var/tmp/sentences_detailed.csv';
+
 -- Links between sentences
 SELECT sentence_id, translation_id FROM `sentences_translations`
 INTO OUTFILE '/var/tmp/links.csv';
