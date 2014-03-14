bd095c8fa2c30efd992c57a5bf502e49320bff65
alanfgh <alanf.tatoeba@gmail.com>
2014-03-14 08:40:04 +0100

    changed curl reference from tatoeba.fr to tatoeba.org

--- app/controllers/components/lucene.php
+++ app/controllers/components/lucene.php
@@ -69,7 +69,7 @@ class LuceneComponent extends Object
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_REFERER, "http://tatoeba.fr");
+        curl_setopt($ch, CURLOPT_REFERER, "http://tatoeba.org");
         $body = curl_exec($ch);
         curl_close($ch);
         
