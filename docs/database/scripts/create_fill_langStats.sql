5f667cc4309766a91b941d4a74a602f04670ca6d
Trang <tranglich@gmail.com>
2014-03-15 18:57:21 +0100

    #115 Cleaned up and added comments for the script create_fill_langStats.sql

--- docs/database/scripts/create_fill_langStats.sql
+++ docs/database/scripts/create_fill_langStats.sql
@@ -1,27 +1,11 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: tatoeba_4
--- ------------------------------------------------------
--- Server version	5.0.75-0ubuntu10.2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `langStats`
+-- 
+-- This script will create the table langStats, which contains
+-- the list of supported languages in Tatoeba as well as the
+-- number of sentence in each language.
 --
 
+-- Deleting and creating the table again.
 DROP TABLE IF EXISTS `langStats`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `langStats` (
   `lang` varchar(4) CHARACTER SET utf8 NOT NULL,
   `numberOfSentences` mediumint(8) unsigned NOT NULL DEFAULT '0',
@@ -29,19 +13,11 @@ CREATE TABLE `langStats` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `lang` (`lang`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+-- Making sure that there's no entry in the "sentences" table
+-- that has lang as an empty string.
 UPDATE sentences SET lang = NULL WHERE lang = '';
 
+-- Inserting the stats into langStats
 INSERT INTO langStats (lang, numberOfSentences)
     SELECT lang , count(*) FROM sentences GROUP BY lang;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-01-18 13:04:47
