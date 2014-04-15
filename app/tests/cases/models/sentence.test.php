1956356bac33f535774c388f8028586d43c6c2dc
時流 <jiru@users.noreply.github.com>
2014-04-16 01:46:39 +0200

    Created a simple test for adding sentence.

---
+++ app/tests/cases/models/sentence.test.php
@@ -0,0 +1,42 @@
+<?php
+/* Sentence Test cases generated on: 2014-04-15 01:07:30 : 1397516850*/
+App::import('Model', 'Sentence');
+
+class SentenceTestCase extends CakeTestCase {
+	var $fixtures = array(
+		'app.sentence',
+		'app.user',
+		'app.group',
+		'app.country',
+		'app.sentence_comment',
+		'app.contribution',
+		'app.sentences_list',
+		'app.sentences_sentences_list',
+		'app.wall',
+		'app.wall_thread',
+		'app.favorites_user',
+		'app.tag',
+		'app.tags_sentence',
+		'app.language',
+		'app.link',
+		'app.sentence_annotation',
+	);
+
+	function startTest() {
+		$this->Sentence =& ClassRegistry::init('Sentence');
+	}
+
+	function endTest() {
+		unset($this->Sentence);
+		ClassRegistry::flush();
+	}
+
+	function testSaveNewSentence_addsOneSentence() {
+		$oldNumberOfSentences = $this->Sentence->find('count');
+		$this->Sentence->saveNewSentence('Hello world.', 'eng', 1);
+		$newNumberOfSentences = $this->Sentence->find('count');
+		$sentencesAdded = $newNumberOfSentences - $oldNumberOfSentences;
+
+		$this->assertEqual($sentencesAdded, 1);
+	}
+}
