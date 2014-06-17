d27b5d3a7cdfda26b718526232c772cb1bdd99f2
Pallav <pallavshah.ps@gmail.com>
2014-06-17 11:50:19 +0530

    admin/imouto-devel/ansible/roles: Add role 'setup_tatodetect'
    The role setup_tatodetect installs and configures tatodetect.

---
+++ imouto-devel/ansible/roles/setup_tatodetect/files/tatodetect.js
@@ -0,0 +1,39 @@
+{
+"service" : {
+"api" : "http",
+"port" : 4242
+},
+
+    "cache" : {
+        "backend" : "thread_shared"
+    },
+"http" : {
+"script_names" : ["/tatodetect"]
+},
+"localization" : {
+"encoding" : "utf-8",
+"messages" : {
+"paths" : [ "../locale" ],
+"domains" : [ "hello" ]
+},
+"locales" : [ "en_GB.UTF-8", "fr_FR.UTF-8" ]
+},
+    "session" : {
+        "expire" : "renew",
+        "timeout" : 604800,
+        "location" : "server",
+        "server" : {
+            "storage" : "memory"
+        }
+    },
+"tatodetect" : {
+        "web" : "http://127.0.0.1:4242/",
+        "interfacelangs" : [
+            ["en" , "en_GB.UTF-8", "English"],
+            ["fr" , "fr_FR.UTF-8", "Français"]
+        ],
+        "sqlite3" : {
+            "path" : "/etc/ngrams.db"
+        }
+}
+}
