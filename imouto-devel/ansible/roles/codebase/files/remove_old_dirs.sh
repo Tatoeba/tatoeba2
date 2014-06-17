9a635ae3d486cf07a71d0a021102fceb541f41f0
Pallav <pallavshah.ps@gmail.com>
2014-06-17 11:29:32 +0530

    admin/imouto-devel/ansible/roles: Add role 'codebase'
    The role codebase contains tasks that setup the tatoeba code
    directory, fetch latest code form github, import sentences in
    the db and set the website up and running on local machine.

---
+++ imouto-devel/ansible/roles/codebase/files/remove_old_dirs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e -u
+
+revision_limit="3" #Default number of old revisions to keep
+
+if [ "$#" -lt 1 ] ; then
+	exit 1
+fi
+
+if [ "$#" -ge 2 ] ; then
+	if [ "$2" -ge 1 ]; then
+		revision_limit=$2
+	fi
+fi
+
+repo_dir=$1 #Path to directory where all old revisions will be stored
+
+dir_count=$(ls -1 -p $1/versions | grep / | wc -l) #Number of revisions present currently
+
+if [ "$dir_count" -ge "$revision_limit" ] ; then #If number of revisions exceed the limit, remove the old ones
+	
+	count=$((revision_limit - 1))
+	array=(`ls -1 -p $repo_dir/versions/ | grep / | head -n -$count`)
+	
+	for var in "${array[@]}"
+	do
+  		echo "Removing ${var}"
+  		rm -rf "$repo_dir""/versions/""$var"
+	done
+	
+fi
+
+#Create new directory for new revision
+dir_name=$(date +%F--%H-%M-%S)
+mkdir -p "$repo_dir""/versions/""$dir_name"
+rm -rf "$repo_dir""/versions/current"
+ln -sf "$repo_dir""/versions/""$dir_name" "$repo_dir""/versions/current"
