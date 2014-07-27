d020e0e6bdf90930e63ca5b9ea9ee43f3e1790ad
Trang <tranglich@gmail.com>
2014-07-27 21:52:20 +0200

    CakePHP 1.3: added 'id' column to tags_sentences
    Same kind of issue as in 3e5f5a2d0508b8efc661f12ba392635e0fc3c33e.
    
    Updated the tables scripts as well, fixed auto_increment, and added some
    comments.

--- docs/database/updates/2014-07-27.sql
+++ docs/database/updates/2014-07-27.sql
@@ -1,3 +1,16 @@
+-- Adding id column to sentences_translations because CakePHP
+-- requires an primary key when using the model->save() function.
+ALTER TABLE sentences_translations AUTO_INCREMENT = 1;
 ALTER TABLE sentences_translations 
-ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
-ADD PRIMARY KEY (`id`);
+  ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
+  ADD PRIMARY KEY (`id`);
+
+
+-- Removing auto_increment on tags_sentences.tag_id.
+-- (why the hell was there an auto_increment on that field...?)
+ALTER TABLE tags_sentences CHANGE tag_id tag_id INT NOT NULL;
+-- Adding id column to tags_sentences.
+ALTER TABLE tags_sentences AUTO_INCREMENT = 1;
+ALTER TABLE tags_sentences
+  ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
+  ADD PRIMARY KEY (`id`);
