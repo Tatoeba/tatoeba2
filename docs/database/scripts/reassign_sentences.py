bba569451169c66f103f8099bcc6af86c3adeae0
alanfgh <alanf.tatoeba@gmail.com>
2014-08-23 22:41:39 -0400

    improved arg-passing to reassign_sentences.py

--- docs/database/scripts/reassign_sentences.py
+++ docs/database/scripts/reassign_sentences.py
@@ -1,4 +1,4 @@
-#python_mysql_connector.py
+#reassign_sentences.py
 
 #Author: alanf
 
@@ -8,6 +8,11 @@
 # To run on the server, you must specify --username, --pwd, --db, --host, --port. See
 # python_mysql_connector.py for more details on both these arguments and the 
 # required mysql.connector package.
+
+# Sample call on the VM, where 100000 is the user id to whom the sentences are to be reassigned 
+# and "/home/tatoeba/somedir/temp.txt" is a file, each line of which is a sentence id that is
+# to be reassigned:
+#     python reassign_sentences.py 100000 "/home/tatoeba/somedir/temp.txt"
 
 import mysql.connector
 import codecs
@@ -21,14 +26,25 @@ class SentenceReassigner(PythonMySQLConnector):
     def __init__(self):
         PythonMySQLConnector.__init__(self)
 
-    def process_file(self, new_user_id, filename):
+    def init_parser(self):
+        PythonMySQLConnector.init_parser(self)
+        self.parser.add_argument('new_user_id', type=int, 
+            help='id of user to whom sentences will be reassigned')
+        self.parser.add_argument('sent_id_listfile',
+            help='name of listfile; each line must be the id of a sentence to be reassigned')
+
+    def process_args(self, argv):
+        PythonMySQLConnector.process_args(self, argv)
+
+    def process_file(self):
         #It would be more efficient to use prepared statements, but for some reason,
         #the apt-get command only wants to install a version of mysql.connector that
         #is too old to support it, so we use ordinary statements.
         self.cursor = self.cnx.cursor()
-        sent_ids = [line.strip() for line in open(filename, "r")]
+        sent_ids = [line.strip() for line in open(self.parsed.sent_id_listfile, "r")]
         for sent_id in sent_ids:
-            stmt = "UPDATE sentences SET user_id = {0} WHERE id = {1}".format(new_user_id, sent_id)
+            stmt = "UPDATE sentences SET user_id = {0} WHERE id = {1}".format(
+                self.parsed.new_user_id, sent_id)
             if self.parsed.dry_run:
                 self.print_output(stmt)
             else:
@@ -43,8 +59,5 @@ if __name__ == "__main__":
     reassigner.process_args(sys.argv)
     reassigner.connect()
     reassigner.set_log_file()
-    # Sample call, where 1000 is the user id to whom the sentences are to be reassigned 
-    # and "/home/tatoeba/somedir/temp.txt" is a file, each line of which is a sentence id
-    # that is to be reassigned to user 100000.
-    # reassigner.process_file(100000, "/home/tatoeba/somedir/temp.txt")
+    reassigner.process_file()
     reassigner.disconnect()
