61eaa2362b02b1c89f5d8e3a5c285435d79fa3fe
時流 <jiru@users.noreply.github.com>
2015-01-26 06:49:15 +0000

    Fixed carriage return of SQL initialization files.

--- docs/database/tables/private_messages.sql
+++ docs/database/tables/private_messages.sql
@@ -1,1 +1,21 @@
----- Table structure for table `private_messages`---- This table stores the private messages.-- TODO for Etienne--DROP TABLE IF EXISTS `private_messages`;CREATE TABLE `private_messages` (  `id` int(11) NOT NULL AUTO_INCREMENT,  `recpt` int(11) NOT NULL,  `sender` int(11) NOT NULL,  `user_id` int(11) NOT NULL DEFAULT '0',  `date` datetime NOT NULL,  `folder` enum('Inbox','Sent','Trash') CHARACTER SET utf8 NOT NULL DEFAULT 'Inbox',  `title` varchar(255) CHARACTER SET utf8 NOT NULL,  `content` text CHARACTER SET utf8 NOT NULL,  `isnonread` tinyint(4) NOT NULL DEFAULT '1',  PRIMARY KEY (`id`),  KEY `idx_recpt` (`recpt`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `private_messages`
+--
+-- This table stores the private messages.
+-- TODO for Etienne
+--
+
+DROP TABLE IF EXISTS `private_messages`;
+CREATE TABLE `private_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recpt` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL,
+  `folder` enum('Inbox','Sent','Trash') CHARACTER SET utf8 NOT NULL DEFAULT 'Inbox',
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `content` text CHARACTER SET utf8 NOT NULL,
+  `isnonread` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `idx_recpt` (`recpt`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
