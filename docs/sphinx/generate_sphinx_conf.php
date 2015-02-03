f7ff5a963c767dd03c43339a61e5ecdd7089f82a
時流 <jiru@users.noreply.github.com>
2015-02-03 22:44:54 +0000

    Use same charset_table for CJK and other indexes
    Actually, contents of ngram_chars do not need to be repeated
    in charset_table. As a result, CJK indexes and other indexes
    can share the same charset_table. Yay!

--- docs/sphinx/generate_sphinx_conf.php
+++ docs/sphinx/generate_sphinx_conf.php
@@ -378,8 +378,6 @@ index cjk_common_index : common_index
 {
     ngram_len               = 1
     ngram_chars             = U+3000..U+2FA1F
-    charset_table           = <?php echo implode(', ', array_merge($charsetTableBase, array('U+3000..U+2FA1F'))); ?>
-
 }
 
 #################################################
