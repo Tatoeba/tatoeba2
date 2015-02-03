e42cb61855e072f6f980def551122ee39e14bf6b
時流 <jiru@users.noreply.github.com>
2015-02-03 23:38:18 +0000

    Normalize fullwidth/halfwidth characters in Sphinx
    Closes #437.
    
    Latin fullwidth normalized to halfwidth.
    Katakana halfwidth normalized to fullwidth.
    Hangul halfwidth normalized to fullwidth.
    
    Note that this incidentally make fullwidth punctuation characters like '！'
    ignored. These were previously interpreted as searchable characters. Since
    we want to normalize fullwidth '！' to halfwidth '!' and we ignore
    halfwidth '!' (otherwise it would be part of words it sticks to),
    fullwidth '！' now gets ignored too.

--- docs/sphinx/generate_sphinx_conf.php
+++ docs/sphinx/generate_sphinx_conf.php
@@ -309,7 +309,23 @@ $charsetTableBase = array(
     'U+0A4B..U+0A4D', 'U+0A51', 'U+0A59..U+0A5C', 'U+0A5E', 'U+0A66..U+0A75',
     # Gujarati
     'U+0A81..U+0A83', 'U+0A85..U+0A8D', 'U+0A8F..U+0A91', 'U+0A93..U+0AA8', 'U+0AAA..U+0AB0', 'U+0AB2', 'U+0AB3',
-    'U+0AB5..U+0AB9', 'U+0ABC..U+0AC5', 'U+0AC7..U+0AC9', 'U+0ACB..U+0ACD', 'U+0AD0', 'U+0AE0..U+0AE3', 'U+0AE6..U+0AEF'
+    'U+0AB5..U+0AB9', 'U+0ABC..U+0AC5', 'U+0AC7..U+0AC9', 'U+0ACB..U+0ACD', 'U+0AD0', 'U+0AE0..U+0AE3', 'U+0AE6..U+0AEF',
+    # Latin fullwidth to halfwidth
+    'U+FF10..U+FF19->U+30..U+39', 'U+FF21..U+FF3A->U+61..U+7A', 'U+FF41..U+FF5A->U+61..U+7A',
+    # Katakana halfwidth to fullwidth
+    'U+FF66->U+30F2', 'U+FF67->U+30A1', 'U+FF68->U+30A3', 'U+FF69->U+30A5', 'U+FF6A->U+30A7',
+    'U+FF6B->U+30A9', 'U+FF6C->U+30E3', 'U+FF6D->U+30E5', 'U+FF6E->U+30E7', 'U+FF6F->U+30C3',
+    'U+FF70->U+30FC', 'U+FF71->U+30A2', 'U+FF72->U+30A4', 'U+FF73->U+30A6', 'U+FF74->U+30A8',
+    'U+FF75->U+30AA', 'U+FF76->U+30AB', 'U+FF77->U+30AD', 'U+FF78->U+30AF', 'U+FF79->U+30B1',
+    'U+FF7A->U+30B3', 'U+FF7B->U+30B5', 'U+FF7C->U+30B7', 'U+FF7D->U+30B9', 'U+FF7E->U+30BB',
+    'U+FF7F->U+30BD', 'U+FF80->U+30BF', 'U+FF81->U+30C1', 'U+FF82->U+30C4', 'U+FF83->U+30C6',
+    'U+FF84->U+30C8', 'U+FF85..U+FF8A->U+30CA..U+30CF', 'U+FF8B->U+30D2', 'U+FF8C->U+30D5',
+    'U+FF8D->U+30D8', 'U+FF8E->U+30DB', 'U+FF8F..U+FF93->U+30DE..U+30E2', 'U+FF94->U+30E4',
+    'U+FF95->U+30E6', 'U+FF96->U+30E8', 'U+FF97->U+30E9', 'U+FF98->U+30EA', 'U+FF99->U+30EB',
+    'U+FF9A->U+30EC', 'U+FF9B->U+30ED', 'U+FF9C->U+30EF', 'U+FF9D->U+30F3',
+    # Hangul halfwidth to fullwidth
+    'U+FFA1..U+FFBE->U+3131..U+314E', 'U+FFC2..U+FFC7->U+314F..U+3154',
+    'U+FFCA..U+FFCF->U+3155..U+315A', 'U+FFD2..U+FFD7->U+315B..U+3160', 'U+FFDA..U+FFDC->U+3161..U+3163',
 );
 
 $languagesWithoutWordBoundaries = array(
@@ -377,7 +393,7 @@ index common_index
 index cjk_common_index : common_index
 {
     ngram_len               = 1
-    ngram_chars             = U+3000..U+2FA1F
+    ngram_chars             = U+3000..U+FEFF, U+FFF0..U+2FA1F
 }
 
 #################################################
