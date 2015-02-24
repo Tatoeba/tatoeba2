8e11a8577d92e62b97248a5062f1a520b1a890f9
時流 <jiru@users.noreply.github.com>
2015-02-24 13:10:18 +0000

    Add tests to cover Sinogram::search()

--- app/tests/cases/models/sinogram.test.php
+++ app/tests/cases/models/sinogram.test.php
@@ -14,9 +14,34 @@ class SinogramTestCase extends CakeTestCase {
 		ClassRegistry::flush();
 	}
 
+	function assertGlyphs($expected, $result) {
+		$result = array_map(function($rec) {
+			return isset($rec['Sinogram']['glyph']) ? $rec['Sinogram']['glyph'] : null;
+		}, $result);
+		$this->assertEqual($expected, $result);
+	}
+
 	function testSearch_EmptySubglyphReturnsEmptyResults() {
 		$excepted = array();
 		$result = $this->Sinogram->search(array());
 		$this->assertEqual($excepted, $result);
+	}
+
+	function testSearch_ReturnsOneGlyphs() {
+		$excepted = array('蝴');
+		$result = $this->Sinogram->search(array('月', '虫', '古'));
+		$this->assertGlyphs($excepted, $result);
+	}
+
+	function testSearch_ReturnSeveralGlyphs() {
+		$excepted = array('朗', '蓢', '蝴');
+		$result = $this->Sinogram->search(array('月', '虫'));
+		$this->assertGlyphs($excepted, $result);
+	}
+
+	function testSearch_ReturnsProvidedGlyphWhenOneGlyphPassed() {
+		$excepted = array('蓢', '朗');
+		$result = $this->Sinogram->search(array('朗'));
+		$this->assertGlyphs($excepted, $result);
 	}
 }
