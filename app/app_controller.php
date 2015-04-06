2372487bf317565c4ba09c4b713ba2ff90ad0e51
時流 <jiru@users.noreply.github.com>
2015-04-06 16:39:43 +0000

    Get rid of $_SERVER["REQUEST_URI"]
    In order to have testable code.

--- app/app_controller.php
+++ app/app_controller.php
@@ -121,7 +121,7 @@ class AppController extends Controller
         Configure::write('Config.language', $lang);
 
         // Forcing the URL to have the (correct) language in it.
-        $url = $_SERVER["REQUEST_URI"];
+        $url = Router::reverse($this->params);
         if (!empty($langInURL) && $langInCookie && $langInURL != $langInCookie) {
             // We're are now going to remove the language from the URL and set
             // $langURL to null so that we get the the correct URL through
