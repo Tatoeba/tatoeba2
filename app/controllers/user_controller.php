Tatoeba/tatoeba2

commit 9b219f3d7579e529ab99ad426b10cc2d5e7e4ce2

Coerce data saved from forms.
Signed-off-by: Trang <tranglich@gmail.com>

app/controllers/user_controller.php
@@ -321,8 +321,10 @@ class UserController extends AppController
 
         $saved = false;
         if (!empty($this->data)) {
-            $this->data['User']['id'] = $currentUserId;
-            $saved = $this->User->save($this->data);
+            $allowedFields = array('name', 'country_id', 'birthday', 'homepage');
+            $basicInfos = $this->filterKeys($this->data['User'], $allowedFields);
+            $basicInfos['id'] = $currentUserId;
+            $saved = $this->User->save($basicInfos);
         }
 
         if ($saved) {
@@ -382,11 +384,11 @@ class UserController extends AppController
             $collapsibleTranslationsEnabled = $this->data['User']['collapsible_translations_enabled'];
             $this->Cookie->write('collapsible_translations_enabled', $collapsibleTranslationsEnabled, false, "+1 month");;
 
-            $this->data['User']['id'] = $currentUserId;
-            $this->data['User']['lang'] = $this->_language_settings(
-                $this->data['User']['lang']
+            $dataToSave = array(
+                'id' => $currentUserId,
+                'lang' => $this->_language_settings($this->data['User']['lang'])
             );
-            if ($this->User->save($this->data)) {
+            if ($this->User->save($dataToSave)) {
                 // Needed so that the information is updated for the Auth component.
                 $user = $this->User->read(null, $currentUserId);
                 $this->Session->write($this->Auth->sessionKey, $user['User']);

app/app_controller.php
@@ -265,5 +265,18 @@ class AppController extends Controller
         }
         return 'eng';
     }
+
+    /**
+     * Returns $array containing only $allowedKeys keys.
+     *
+     * @param array $array  An associative array
+     * @param array $allowedKeys Allowed keys inside $array
+     *
+     * @return string Filtered array.
+     */
+    public function filterKeys($array, $allowedKeys)
+    {
+        return array_intersect_key($array, array_flip($allowedKeys));
+    }
 }
 ?>

app/controllers/components/common_sentence.php
@@ -88,15 +88,17 @@ class CommonSentenceComponent extends Object
             $lang = null;
         }
 
-        $this->data['Sentence']['id'] = $sentenceId;
-        $this->data['Sentence']['lang'] = $lang;
-        $this->data['Sentence']['user_id'] = $userId;
-        $this->data['Sentence']['text'] = $text;
-        $this->data['Sentence']['correctness'] = $correctness;
+        $sentenceData = array(
+            'id' => $sentenceId,
+            'lang' => $lang,
+            'user_id' => $userId,
+            'text' => $text,
+            'correctness' => $correctness,
+        );
 
         $Sentence = ClassRegistry::init('Sentence');
 
-        $isSaved = $Sentence->save($this->data);
+        $isSaved = $Sentence->save($sentenceData);
 
         return $isSaved;
     }

app/controllers/private_messages_controller.php
@@ -146,7 +146,21 @@ class PrivateMessagesController extends AppController
                 $this->redirect(array('action' => 'folder', 'Sent'));
             }
 
-            $this->data['PrivateMessage']['sender'] = $currentUserId;
+            $now = date("Y/m/d H:i:s", time());
+            $messageToSend = array(
+                'sender'    => $currentUserId,
+                'date'      => $now,
+                'folder'    => 'Inbox',
+                'title'     => $this->data['PrivateMessage']['title'],
+                'content'   => $this->data['PrivateMessage']['content'],
+                'isnonread' => 1,
+            );
+
+            $messageToSave = array_merge($messageToSend, array(
+                'user_id'   => $currentUserId,
+                'folder'    => 'Sent',
+                'isnonread' => 0,
+            ));
 
             $recptArray = explode(',', $this->data['PrivateMessage']['recpt']);
 
@@ -158,21 +172,16 @@ class PrivateMessagesController extends AppController
 
                 // we send the msg only if the user exists.
                 if ($recptId) {
-
-                    $this->data['PrivateMessage']['recpt'] = $recptId;
-                    $this->data['PrivateMessage']['user_id'] = $recptId;
-                    $this->data['PrivateMessage']['folder'] = 'Inbox';
-                    $this->data['PrivateMessage']['date']
-                        = date("Y/m/d H:i:s", time());
-                    $this->data['PrivateMessage']['isnonread'] = 1;
-                    $this->PrivateMessage->save($this->data);
+                    $message = $messageToSend;
+                    $message['recpt'] = $recptId;
+                    $message['user_id'] = $recptId;
+                    $this->PrivateMessage->save($message);
                     $this->PrivateMessage->id = null;
 
                     // we need to save the msg to our outbox folder of course.
-                    $this->data['PrivateMessage']['user_id'] = $currentUserId;
-                    $this->data['PrivateMessage']['folder'] = 'Sent';
-                    $this->data['PrivateMessage']['isnonread'] = 0;
-                    $this->PrivateMessage->save($this->data);
+                    $message = $messageToSave;
+                    $message['recpt'] = $recptId;
+                    $this->PrivateMessage->save($message);
                     $this->PrivateMessage->id = null;
                 } else {
                     $this->Session->setFlash(

app/controllers/sentence_annotations_controller.php
@@ -114,13 +114,17 @@ class SentenceAnnotationsController extends AppController
         $sentenceId = Sanitize::paranoid(
             $this->data['SentenceAnnotation']['sentence_id']
         );
-
         $this->data['SentenceAnnotation']['user_id'] = CurrentUser::get('id');
+        $text = trim($this->data['SentenceAnnotation']['text']);
 
-        $this->data['SentenceAnnotation']['text']
-            = trim($this->data['SentenceAnnotation']['text']);
-
-        if ($this->SentenceAnnotation->save($this->data)) {
+        $annotation = array(
+            'id'          => $this->data['SentenceAnnotation']['id'],
+            'sentence_id' => $sentenceId,
+            'meaning_id'  => $this->data['SentenceAnnotation']['meaning_id'],
+            'text'        => $text,
+            'user_id'     => CurrentUser::get('id'),
+        );
+        if ($this->SentenceAnnotation->save($annotation)) {
             $this->flash(
                 'Index saved.',
                 "/sentence_annotations/show/".$sentenceId

app/controllers/sentence_comments_controller.php
@@ -199,10 +199,11 @@ class SentenceCommentsController extends AppController
             );
         }
 
+        $allowedFields = array('sentence_id', 'text');
+        $comment = $this->filterKeys($this->data['SentenceComment'], $allowedFields);
+        $comment['user_id'] = $userId;
 
-        $this->data['SentenceComment']['user_id'] = $userId;
-
-        if ($this->SentenceComment->save($this->data)) {
+        if ($this->SentenceComment->save($comment)) {
             $sentenceId = $this->data['SentenceComment']['sentence_id'];
             $participants = $this->SentenceComment->getEmailsFromComments(
                 $sentenceId
@@ -325,7 +326,11 @@ class SentenceCommentsController extends AppController
                     );
                 }
                 //save comment
-                if ($this->SentenceComment->save($this->data)) {
+                $commentUpdate = array(
+                    'id'   => $commentId,
+                    'text' => $this->data['SentenceComment']['text'],
+                );
+                if ($this->SentenceComment->save($commentUpdate)) {
                     $this->Session->setFlash(
                         __("Changes to your comment have been saved.", true)
                     );

app/controllers/sentences_lists_controller.php
@@ -238,8 +238,11 @@ class SentencesListsController extends AppController
     {
         $listName = $this->data['SentencesList']['name'];
         if (!empty($this->data) && trim($listName) != '') {
-            $this->data['SentencesList']['user_id'] = $this->Auth->user('id');
-            $this->SentencesList->save($this->data);
+            $newList = array(
+                'name'    => $listName,
+                'user_id' => $this->Auth->user('id'),
+            );
+            $this->SentencesList->save($newList);
             $this->redirect(array("action"=>"edit", $this->SentencesList->id));
         } else {
             $this->redirect(array("action"=>"index"));

app/controllers/users_controller.php
@@ -344,13 +344,14 @@ class UsersController extends AppController
 
         // At this point, we're fine, so we can create the user
         $this->User->create();
-        $this->data['User']['since'] = date("Y-m-d H:i:s");
-        $this->data['User']['group_id'] = 4;
-        $this->User->set($this->data);
+        $allowedFields = array('username', 'password', 'email');
+        $newUser = $this->filterKeys($this->data['User'], $allowedFields);
+        $newUser['since']    = date("Y-m-d H:i:s");
+        $newUser['group_id'] = 4;
 
         // And we save
-        if ($this->User->save($this->data)) {
-            $this->Auth->login($this->data);
+        if ($this->User->save($newUser)) {
+            $this->Auth->login($newUser);
             $this->redirect(
                 array(
                     'controller' => 'pages',
@@ -378,12 +379,12 @@ class UsersController extends AppController
                 $newPassword = $this->User->generatePassword();
 
                 // data to save
-                $this->data['User']['id'] = $user['User']['id'];
-                $this->data['User']['password'] = $this->Auth->password(
-                    $newPassword
+                $updatePasswordData = array(
+                    'id' => $user['User']['id'],
+                    'password' => $this->Auth->password($newPassword)
                 );
 
-                if ($this->User->save($this->data)) { // if saved
+                if ($this->User->save($updatePasswordData)) { // if saved
                     // prepare message
                     $subject = __('Tatoeba, new password', true);
                     $message = __('Your login: ', true)

commit c2cd19e5a99b4ada5c87590a51a7a56d91bf08e9

Fix settings edition regression
introduced by 9b219f3d7579e529ab99ad426b10cc2d5e7e4ce2, sorry.

app/controllers/user_controller.php
@@ -321,7 +321,7 @@ class UserController extends AppController
 
         $saved = false;
         if (!empty($this->data)) {
-            $allowedFields = array('name', 'country_id', 'birthday', 'homepage');
+            $allowedFields = array('name', 'country_id', 'birthday', 'homepage', 'email');
             $basicInfos = $this->filterKeys($this->data['User'], $allowedFields);
             $basicInfos['id'] = $currentUserId;
             $saved = $this->User->save($basicInfos);
@@ -386,7 +386,9 @@ class UserController extends AppController
 
             $dataToSave = array(
                 'id' => $currentUserId,
-                'lang' => $this->_language_settings($this->data['User']['lang'])
+                'lang' => $this->_language_settings($this->data['User']['lang']),
+                'send_notifications' => $this->data['User']['send_notifications'],
+                'is_public' => $this->data['User']['is_public'],
             );
             if ($this->User->save($dataToSave)) {
                 // Needed so that the information is updated for the Auth component.