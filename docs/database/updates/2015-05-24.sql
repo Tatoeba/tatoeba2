2d98b3a39f877b00b07c46293b898fbbc492ef5b
時流 <jiru@users.noreply.github.com>
2015-05-24 09:40:26 +0000

    Fix SQL update script
    Set values the same way they are set by CakePHP.

--- docs/database/updates/2015-05-24.sql
+++ docs/database/updates/2015-05-24.sql
@@ -1,8 +1,8 @@
 ALTER TABLE users ADD settings BLOB NOT NULL AFTER `description`;
 
 UPDATE users SET settings = CONCAT('{',
-    '"is_public":',          IF(is_public,'true','false'),          ',',
-    '"lang":',               '"',COALESCE(lang,''),'"',
+    '"is_public":',          IF(is_public,'1','0'),',',
+    '"lang":',               IF(lang is null,'null', CONCAT('"',lang,'"')),
 '}');
 
 ALTER TABLE users DROP is_public;
