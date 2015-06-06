713b74269d536634665d5d9ab655f4cc1b37c203
Trang <tranglich@gmail.com>
2015-06-07 01:26:20 +0200

    Updated models.py.

--- docs/tatoeba2-django/tatoeba2/models.py
+++ docs/tatoeba2-django/tatoeba2/models.py
@@ -275,7 +275,6 @@ class Users(models.Model):
     username = models.CharField(unique=True, max_length=20)
     password = models.CharField(max_length=50)
     email = models.CharField(unique=True, max_length=100)
-    lang = models.CharField(max_length=100, blank=True)
     since = models.DateTimeField()
     last_time_active = models.IntegerField()
     level = models.IntegerField()
@@ -287,7 +286,7 @@ class Users(models.Model):
     homepage = models.CharField(max_length=255)
     image = models.CharField(max_length=255)
     country_id = models.CharField(max_length=2, blank=True)
-    is_public = models.IntegerField()
+    settings = models.TextField()
     class Meta:
         db_table = 'users'
 
