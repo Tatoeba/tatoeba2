b19196075a2946dcc2295d210c9e2c44eafb3a40
Trang <tranglich@gmail.com>
2015-06-28 22:28:39 +0200

    Updated SQL for sentences_translations and tags_sentences.

--- docs/database/tables/tags_sentences.sql
+++ docs/database/tables/tags_sentences.sql
@@ -4,10 +4,12 @@
 
 DROP TABLE IF EXISTS `tags_sentences`;
 CREATE TABLE `tags_sentences` (
-  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `sentence_id` int(11) DEFAULT NULL,
   `added_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `tag_id` (`tag_id`),
   KEY `sentence_id` (`sentence_id`)
