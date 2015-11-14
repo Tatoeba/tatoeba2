f9d030a16b012012867293abb6b7ab38677fefba
Trang <tranglich@gmail.com>
2015-11-14 20:03:19 +0100

    #925 Included announcement below the search bar and added close system.

--- app/webroot/js/generic_functions.js
+++ app/webroot/js/generic_functions.js
@@ -70,6 +70,16 @@ $(document).ready(function() {
     $('#clearSearch').click(function() {
         $('#SentenceQuery').val('').focus();
     });
+
+    // Announcement
+    var announcementId = $('.announcement').attr('data-announcement-id');
+    if ($.cookie(announcementId) === undefined) {
+        $('.announcement').show();
+    }
+    $('.announcement .close').click(function() {
+        $('.announcement').hide();
+        $.cookie(announcementId, 'done', {'expires' : 7});
+    });
 });
 
 /**
