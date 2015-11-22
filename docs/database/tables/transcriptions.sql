432eff840adbbb015825f977fe946b5e9fbcc3bf
時流 <jiru@users.noreply.github.com>
2015-11-22 03:45:38 +0000

    Add sql declaration of the transcriptions table.

---
+++ docs/database/tables/transcriptions.sql
@@ -0,0 +1,20 @@
+--
+-- Table structure for table `transcriptions`
+--
+
+-- Contains all the transcriptions and alternative scripts associated
+-- to the sentences using the sentence_id foreign key.
+-- See also app/model/transcriptions.php
+
+DROP TABLE IF EXISTS `transcriptions`;
+CREATE TABLE `transcriptions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sentence_id` int(11) NOT NULL,
+  `script` varchar(4) NOT NULL,
+  `text` varbinary(10000) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_transcriptions` (`sentence_id`,`script`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8;
