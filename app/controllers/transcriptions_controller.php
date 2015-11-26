Tatoeba/tatoeba2

commit 16fb2ed76c5a080c66ba8acad55f344e1cbae371

Simplify Transcription::generateTranscription()
Don’t needlessly wrap the transcription into an array.
This is a reminiscence of the removed subtranscriptions feature.

app/views/transcriptions/view.ctp
@@ -45,6 +45,6 @@ if ($validationErrors) {
 
 if ($transcr && $lang) {
     echo $javascript->link('transcriptions.edit_in_place.js', true);
-    $transcriptions->displayTranscriptions($transcr, $lang, $sentenceOwnerId);
+    $transcriptions->displayTranscription($transcr, $lang, $sentenceOwnerId);
 }
 ?>

app/controllers/tools_controller.php
@@ -116,7 +116,6 @@ class ToolsController extends AppController
         );
         if (!empty($query)) {
             $result = $this->Transcription->generateTranscription($sentence, 'Hrkt');
-            $result = $result[0];
         }
         
         $this->set('query', $query);
@@ -150,7 +149,7 @@ class ToolsController extends AppController
             );
 
             if ($transcr) {
-                $this->set('convertedText', $transcr[0]['text']);
+                $this->set('convertedText', $transcr['text']);
                 $this->set('script', $targetScript);
             }
         }
@@ -186,7 +185,7 @@ class ToolsController extends AppController
                     'Latn'
                 );
                 if ($transcr)
-                    $this->set('convertedText', $transcr[0]['text']);
+                    $this->set('convertedText', $transcr['text']);
             } else {
                 $this->set('convertedText', $text);
             }

app/models/transcription.php
@@ -396,9 +396,7 @@ class Transcription extends AppModel
         } else {
             $transcr['id'] = 'autogenerated';
         }
-        $result[] = $transcr;
-
-        return $result;
+        return $transcr;
     }
 
     private function _generateTranscription($sentenceId, $text, $langScript, $targetScript) {
@@ -463,11 +461,11 @@ class Transcription extends AppModel
          * the order we want transcriptions to be displayed. */
         $insertPos = 0;
         foreach ($transcriptions as $transcr) {
-            if (strcmp($transcr['script'], $newTranscr[0]['script']) > 0)
+            if (strcmp($transcr['script'], $newTranscr['script']) > 0)
                 break;
             $insertPos++;
         }
-        array_splice($transcriptions, $insertPos, 0, $newTranscr);
+        array_splice($transcriptions, $insertPos, 0, array($newTranscr));
     }
 
     public function addGeneratedTranscriptions($transcriptions, $sentence) {

app/tests/cases/models/transcription.test.php
@@ -227,16 +227,14 @@ class TranscriptionTestCase extends CakeTestCase {
 
         $result = $this->Transcription->generateTranscription($jpnSentence, 'Hrkt');
         $expected = array(
-            array(
-                'sentence_id' => 6,
-                'script' => 'Hrkt',
-                'text' => 'autogenerated furigana',
-                'user_id' => null,
-                'readonly' => false,
-                'needsReview' => true,
-                'type' => 'altscript',
-                'id' => 'autogenerated',
-            ),
+            'sentence_id' => 6,
+            'script' => 'Hrkt',
+            'text' => 'autogenerated furigana',
+            'user_id' => null,
+            'readonly' => false,
+            'needsReview' => true,
+            'type' => 'altscript',
+            'id' => 'autogenerated',
         );
         $this->assertEqual($expected, $result);
     }
@@ -245,16 +243,14 @@ class TranscriptionTestCase extends CakeTestCase {
         $this->Transcription->deleteAll('1=1');
         $jpnSentence = $this->Transcription->Sentence->findById(6);
         $expected = array(
-            array(
-                'id' => 'autogenerated',
-                'sentence_id' => 6,
-                'script' => 'Hrkt',
-                'text' => 'autogenerated furigana',
-                'user_id' => null,
-                'readonly' => false,
-                'type' => 'altscript',
-                'needsReview' => true,
-            ),
+            'id' => 'autogenerated',
+            'sentence_id' => 6,
+            'script' => 'Hrkt',
+            'text' => 'autogenerated furigana',
+            'user_id' => null,
+            'readonly' => false,
+            'type' => 'altscript',
+            'needsReview' => true,
         );
 
         $result = $this->Transcription->generateTranscription($jpnSentence, 'Hrkt');
@@ -290,25 +286,17 @@ class TranscriptionTestCase extends CakeTestCase {
 
         $created = $this->Transcription->generateTranscription($jpnSentence, 'Hrkt', true, $data);
 
-        $created = array_map(
-            function ($r) {
-                unset($r['modified']);
-                unset($r['created']);
-                return $r;
-            },
-            $created
-        );
+        unset($created['modified']);
+        unset($created['created']);
         $expected = array(
-            array(
-                'id' => 4,
-                'sentence_id' => 10,
-                'script' => 'Hrkt',
-                'text' => 'あああ',
-                'user_id' => 33,
-                'readonly' => false,
-                'needsReview' => true,
-                'type' => 'altscript',
-            ),
+            'id' => 4,
+            'sentence_id' => 10,
+            'script' => 'Hrkt',
+            'text' => 'あああ',
+            'user_id' => 33,
+            'readonly' => false,
+            'needsReview' => true,
+            'type' => 'altscript',
         );
         $this->assertEqual($expected, $created);
     }
@@ -325,25 +313,17 @@ class TranscriptionTestCase extends CakeTestCase {
 
         $updated = $this->Transcription->generateTranscription($jpnSentence, 'Hrkt', true, $data);
 
-        $updated = array_map(
-            function ($r) {
-                unset($r['modified']);
-                unset($r['created']);
-                return $r;
-            },
-            $updated
-        );
+        unset($updated['modified']);
+        unset($updated['created']);
         $expected = array(
-            array(
-                'id' => 3,
-                'sentence_id' => 10,
-                'script' => 'Hrkt',
-                'text' => 'あああ',
-                'user_id' => 33,
-                'readonly' => false,
-                'needsReview' => true,
-                'type' => 'altscript',
-            ),
+            'id' => 3,
+            'sentence_id' => 10,
+            'script' => 'Hrkt',
+            'text' => 'あああ',
+            'user_id' => 33,
+            'readonly' => false,
+            'needsReview' => true,
+            'type' => 'altscript',
         );
         $this->assertEqual($expected, $updated);
     }
@@ -503,7 +483,7 @@ class TranscriptionTestCase extends CakeTestCase {
         ));
 
         $result = $this->Transcription->addGeneratedTranscriptions(
-            $result,
+            array($result),
             $cmnSentence
         );
 

app/views/helpers/transcriptions.php
@@ -71,7 +71,7 @@ class TranscriptionsHelper extends AppHelper
         }
     }
 
-    private function displayTranscription(
+    public function displayTranscription(
         $transcr,
         $lang,
         $sentenceOwnerId

commit e008bf77246fcf0fac51fb2ebff0bf9725859107

Fix regression introduced by 16fb2ed7

app/controllers/transcriptions_controller.php
@@ -92,7 +92,7 @@ class TranscriptionsController extends AppController
             $this->header('HTTP/1.1 400 Bad transcription');
         }
 
-        $this->setViewVars($saved, $sentenceId, $sentence);
+        $this->setViewVars(array($saved), $sentenceId, $sentence);
         $this->render('view');
     }
 
@@ -141,7 +141,7 @@ class TranscriptionsController extends AppController
             return $canEdit && $saved;
         }
 
-        $this->setViewVars($saved, $sentenceId);
+        $this->setViewVars(array($saved), $sentenceId);
         $this->render('view');
     }
 
@@ -169,8 +169,8 @@ class TranscriptionsController extends AppController
         $this->set(compact('username'));
     }
 
-    private function setViewVars($transcription, $sentenceId, $sentence = null) {
-        if ($transcription) {
+    private function setViewVars($transcriptions, $sentenceId, $sentence = null) {
+        if ($transcriptions) {
             if (!$sentence) {
                 $sentence = $this->Sentence->findById(
                     $sentenceId,
@@ -183,7 +183,7 @@ class TranscriptionsController extends AppController
             }
         }
 
-        $this->set('transcr', $transcription);
+        $this->set('transcr', $transcriptions);
         $this->set('validationErrors', $this->Transcription->validationErrors);
         $this->layout = null;
     }

app/views/transcriptions/view.ctp
@@ -32,6 +32,6 @@ if ($validationErrors) {
 
 if ($transcr && $lang) {
     echo $javascript->link('transcriptions.edit_in_place.js', true);
-    $transcriptions->displayTranscription($transcr, $lang, $sentenceOwnerId);
+    $transcriptions->displayTranscriptions($transcr, $lang, $sentenceOwnerId);
 }
 ?>