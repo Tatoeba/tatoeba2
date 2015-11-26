Tatoeba/tatoeba2

commit 6df6b05219af4f7ff190e95b8256d0487c8cc4b4

Avoid setting script if already set

app/vendors/shells/transcriptions.php
@@ -72,6 +72,7 @@ class TranscriptionsShell extends Shell {
     private function setScript() {
         $proceeded = $this->allSentencesOperation('_setScript', array(
             'lang' => $this->Transcription->langsInNeedOfScriptAutodetection(),
+            'script' => null,
         ));
         echo "\nScript set for $proceeded sentences.\n";
     }

commit a535b1a6fd66a6d5409ee55e01c71dea211f9de7

Fix regression introduced in 6df6b052
Since we change the 'script' field, we don’t want to mention it
in the find() conditions, otherwise it messes up with batch
range calculation.

app/vendors/shells/transcriptions.php
@@ -79,7 +79,6 @@ class TranscriptionsShell extends Shell {
     private function setContributionsScript() {
         $conditions = array(
             'sentence_lang' => $this->Transcription->langsInNeedOfScriptAutodetection(),
-            'script' => null,
         );
         $proceeded = $this->batchOperation(
             'Contribution',
@@ -95,7 +94,6 @@ class TranscriptionsShell extends Shell {
     private function setSentencesScript() {
         $proceeded = $this->allSentencesOperation('_setScript', array(
             'lang' => $this->Transcription->langsInNeedOfScriptAutodetection(),
-            'script' => null,
         ));
         echo "\nScript set for $proceeded sentences.\n";
     }