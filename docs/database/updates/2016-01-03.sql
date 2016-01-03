8181de4cffbf28d7adca6a8051e75447fd4fb93f
Trang <tranglich@gmail.com>
2016-01-03 21:35:48 +0100

    #677 Init values of visibility and editable_by.
    Every list with is_public=true has visibility=public and editable_by=anyone.

--- docs/database/updates/2016-01-03.sql
+++ docs/database/updates/2016-01-03.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `sentences_lists` ADD `visibility` enum('private', 'public') NOT NULL DEFAULT 'private';
 ALTER TABLE `sentences_lists` ADD `editable_by` enum('creator', 'anyone') NOT NULL DEFAULT 'creator';
+
+UPDATE sentences_lists SET visibility = 'public' WHERE is_public = 1;
+UPDATE sentences_lists SET editable_by = 'anyone' WHERE is_public = 1;
