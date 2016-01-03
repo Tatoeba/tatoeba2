a9b349c9d406c413c11b0a00b130cbc09f484693
Trang <tranglich@gmail.com>
2016-01-03 21:12:27 +0100

    #677 New fields in database.

---
+++ docs/database/updates/2016-01-03.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sentences_lists` ADD `visibility` enum('private', 'public') NOT NULL DEFAULT 'private';
+ALTER TABLE `sentences_lists` ADD `editable_by` enum('creator', 'anyone') NOT NULL DEFAULT 'creator';
