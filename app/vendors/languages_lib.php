038e91f6c18c09bb77a2eaaee5a40c49a1803299
RyckRichards <rvernautjr@gmail.com>
2016-01-15 23:52:23 -0200

    #954 Added language Keningau Murut

--- app/vendors/languages_lib.php
+++ app/vendors/languages_lib.php
@@ -513,7 +513,8 @@ class LanguagesLib
                 'ban' => __d('languages', 'Balinese',true),
                 'aii' => __d('languages', 'Assyrian',true),
                 'tvl' => __d('languages', 'Tuvaluan',true),
-
+                'kxi' => __d('languages', 'Keningau Murut,),
+				
             );
         }
         return $languages;
