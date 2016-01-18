23091bf52b66a31dba9373ed7e5f0c1035294816
時流 <jiru@users.noreply.github.com>
2016-01-18 10:42:39 +0000

    Add missing semicolon
    So that concatenation of all these SQL files
    do not result into a syntax error.

--- docs/database/tables/sentences_sentences_lists.sql
+++ docs/database/tables/sentences_sentences_lists.sql
@@ -15,4 +15,4 @@ CREATE TABLE `sentences_sentences_lists` (
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `list_id` (`sentences_list_id`,`sentence_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
