661998696af3bcaa9238d15414e1db57f2739d08
RyckRichards <rvernautjr@gmail.com>
2016-07-13 21:47:59 -0300

    #1252 Inserted missing commas

--- app/vendors/languages_lib.php
+++ app/vendors/languages_lib.php
@@ -603,8 +603,8 @@ class LanguagesLib
             "ary",
             "aii",
             "afb",
-            "pus"
-            "snd"
+            "pus",
+            "snd",
         );
 
         if (in_array($lang, $rightToLeftLangs)) {
