6597e7b9dbe9ce55a214e4ba683e8042d5ab5ff3
zachleigh <zachleigh@fastmail.jp>
2016-10-24 06:02:56 +0000

    It's 2016, not 2015...

---
+++ docs/database/updates/2016-10.25.sql
@@ -0,0 +1,13 @@
+# Move current id to hash column
+ALTER TABLE `vocabulary` ADD hash BINARY(16) NOT NULL;
+UPDATE `vocabulary` SET hash = id;
+
+# Change id to auto-increment int
+ALTER TABLE `vocabulary` DROP PRIMARY KEY;
+UPDATE vocabulary SET id = 0;
+ALTER TABLE `vocabulary` MODIFY id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT;
+
+# Update users_vocabulary vocabulary_id column
+UPDATE `users_vocabulary`
+    LEFT OUTER JOIN `vocabulary` ON(vocabulary.hash=users_vocabulary.vocabulary_id)
+    SET vocabulary_id = vocabulary.id;
