e6b0d5a0d9d7af870c8f91d68ffb5c60f44e504e
Trang <tranglich@gmail.com>
2016-12-18 20:07:50 +0100

    Fix unique key for users_vocabulary
    With the renaming of `vocabulary_id` into `hash`, the unique index was set on (user_id, hash) while it should have been on (user_id, vocabulary_id). This was preventing users from adding unexisting vocabulary to their list.

--- docs/database/updates/2016-10-26.sql
+++ docs/database/updates/2016-10-26.sql
@@ -13,3 +13,7 @@ ALTER TABLE `users_vocabulary` ADD vocabulary_id int(11) NOT NULL;
 UPDATE `users_vocabulary`
     LEFT OUTER JOIN `vocabulary` ON(vocabulary.hash=users_vocabulary.hash)
     SET vocabulary_id = vocabulary.id;
+
+# Update index
+ALTER TABLE users_vocabulary DROP INDEX user_vocabulary;
+ALTER TABLE users_vocabulary ADD UNIQUE KEY `user_vocabulary` (`user_id`,`vocabulary_id`);
