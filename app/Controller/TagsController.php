d9a30f2a74c2aeffdb6f69598c9092bfb891582f
Trang HO <tranglich@gmail.com>
2017-01-23 00:25:54 +0000

    Fix error in tags: add_tag_post

--- app/Controller/TagsController.php
+++ app/Controller/TagsController.php
@@ -63,6 +63,9 @@ class TagsController extends AppController
             'search'
         );
 
+        $this->Security->unlockedActions = array(
+            'add_tag_post'
+        );
     }
 
     /**
