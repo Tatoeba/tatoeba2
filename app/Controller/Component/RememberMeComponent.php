Tatoeba/tatoeba2

commit 15439d847d46c556618955902431e5a54e8a6d04

Fix the "remember me" functionality
Since the new hashing method uses a salt that is directly
inserted in the hash, we can’t look up users based on a newly
hashed password any more.

app/Controller/Component/RememberMeComponent.php
@@ -91,7 +91,6 @@ class RememberMeComponent extends Component
         $user = $model->find('first', array(
             'conditions' => array(
                 'username' => $cookie['username'],
-                'password' => $this->Auth->password($cookie['password'])
             )
         ));
 

app/Test/Case/Controller/AppControllerTest.php
@@ -5,6 +5,7 @@ App::import('Component', 'Cookie');
 class AppControllerTest extends ControllerTestCase {
 	public $fixtures = array(
 		'app.sentence',
+		'app.user',
 	);
 
 	private $interfaceLangCookie = false;
@@ -18,11 +19,13 @@ class AppControllerTest extends ControllerTestCase {
 			array('pt_BR', 'BR', 'Português (BR)'),
 		));
 		Configure::write('App.base', ''); // prevent using the filesystem path as base
+		$mockedComponents = array();
+		if (strpos($method, "testBeforeFilter_") !== false) {
+			$mockedComponents = array('Cookie' => array('read', 'write'));
+		}
 		$this->controller = $this->generate('App', array(
 			'methods' => array('redirect', 'bar'),
-			'components' => array(
-				'Cookie' => array('read', 'write')
-			)
+			'components' => $mockedComponents,
 		));
 		$this->controller->Auth->allowedActions = array('bar');
 	}
@@ -150,5 +153,14 @@ class AppControllerTest extends ControllerTestCase {
 			->method('redirect');
 
 		$this->testAction('/cmn/foo/bar', array('method' => 'GET'));
+	}
+
+	function testRememberMeAutomaticallyLogsInUser() {
+		$this->controller->RememberMe->remember('contributor', '123456');
+		$this->assertFalse($this->controller->Auth->loggedIn());
+
+		$this->testAction('/eng/foo/bar', array('method' => 'GET'));
+
+		$this->assertTrue($this->controller->Auth->loggedIn());
 	}
 }

commit 1c52fa0d93d31865217ac605300e9761650256ef

Properly check the password on "remember me"
The previous commit 15439d847d46c556618955902431e5a54e8a6d04
accidentally removed the password check. It is that find()
in RememberMeComponent->check() that authenticate the user
during the "remember me" process.

Let’s use the password hash instead of the plaintext for
improved security.

app/Controller/Component/RememberMeComponent.php
@@ -91,6 +91,7 @@ class RememberMeComponent extends Component
         $user = $model->find('first', array(
             'conditions' => array(
                 'username' => $cookie['username'],
+                'password' => $cookie['password'],
             )
         ));
 

app/Controller/UsersController.php
@@ -272,7 +272,7 @@ class UsersController extends AppController
             $this->RememberMe->delete();
         } else {
             $this->RememberMe->remember(
-                $this->request->data['User']['username'], $this->request->data['User']['password']
+                $this->request->data['User']['username'], $this->User->field('password')
             );
         }
 

app/Test/Case/Controller/AppControllerTest.php
@@ -31,6 +31,7 @@ class AppControllerTest extends ControllerTestCase {
 	}
 
 	function tearDown() {
+		$this->controller->Auth->Session->destroy();
 		unset($this->controller);
 	}
 
@@ -156,11 +157,19 @@ class AppControllerTest extends ControllerTestCase {
 	}
 
 	function testRememberMeAutomaticallyLogsInUser() {
-		$this->controller->RememberMe->remember('contributor', '123456');
+		$this->controller->RememberMe->remember('contributor', '0 $2a$10$Dn8/JT1xViULUEBCR5HiquLCXXB4/K3N2Nzc0PRZ.bfbmoApO55l6');
 		$this->assertFalse($this->controller->Auth->loggedIn());
 
 		$this->testAction('/eng/foo/bar', array('method' => 'GET'));
 
 		$this->assertTrue($this->controller->Auth->loggedIn());
+	}
+
+	function testRememberMeFailsIfIncorrectPassword() {
+		$this->controller->RememberMe->remember('contributor', '0 $2a$10$Dn8/JT1xViULUEBCR5HiquLCXXB4/K3N2Nzc0PRZ.bfbmoApO55l4');
+
+		$this->testAction('/eng/foo/bar', array('method' => 'GET'));
+
+		$this->assertFalse($this->controller->Auth->loggedIn());
 	}
 }