0ecf126c94aeeb22e694830bc9c905c052dd641e
時流 <jiru@users.noreply.github.com>
2018-06-26 19:36:06 +0000

    Add test for notification of comment on sentence

---
+++ app/Test/Case/Controller/SentenceCommentsControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+App::uses('SentenceCommentsController', 'Controller');
+
+class SentenceCommentsControllerTest extends ControllerTestCase {
+
+    public $fixtures = array(
+        'app.aco',
+        'app.aro',
+        'app.aros_aco',
+        'app.contribution',
+        'app.sentence_comment',
+        'app.sentence',
+        'app.user',
+    );
+
+    public function setUp() {
+        Configure::write('Acl.database', 'test');
+    }
+
+    public function startTest($method) {
+        $this->controller = $this->generate('SentenceComments', array(
+            'components' => array('Mailer' => array('sendSentenceCommentNotification'))
+        ));
+    }
+
+    public function tearDown() {
+        unset($this->controller);
+    }
+
+    private function logInAs($username) {
+        $user = $this->controller->User->findByUsername($username);
+        $this->controller->Auth->login($user['User']);
+    }
+
+    public function testSave_sendsNotificationToOwner() {
+        $this->logInAs('contributor');
+        $comment = array(
+            'sentence_id' => '1',
+            'sentence_text' => 'The fundamental cause of the problem is that in'
+                              .' the modern world, idiots are full of confidence,'
+                              .' while the intelligent are full of doubt.',
+            'text' => 'Very well said!',
+        );
+
+        $this->controller->Mailer
+            ->expects($this->once())
+            ->method('sendSentenceCommentNotification')
+            ->with('kazuki@example.net', $comment, 'kazuki@example.net');
+
+        $this->testAction('/eng/sentence_comments/save', array(
+            'data' => array(
+                'SentenceComment' => $comment,
+            )
+        ));
+    }
+}
