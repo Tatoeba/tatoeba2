Tatoeba/tatoeba2

commit 70ef3140f86dbfe878c7873b53cf227b29815b46

Fix copy-pasting furigana in Chrome #1077
That’s a very ugly hack for a bug that should be solved in Chrome.

In Chrome, copy-pasting a Japanese sentence takes the furigana as-is.
For example, copying the following rendered HTML

  <p><ruby>私<rp>（</rp><rt>わたし</rt><rp>）</rp></ruby>はいいの</p>

puts 私わたしはいいの in the clipboard, which is very confusing. It
should either put 私はいいの or 私（わたし）はいいの (using rp tags).

app/webroot/js/generic_functions.js
@@ -107,3 +107,12 @@ $(document).ready(function() {
         });
     });
 });
+
+// Fix for Chrome: prevent copy-pasting furigana when selecting a sentence
+// https://stackoverflow.com/questions/13438391
+$(document).on('copy', function (e) {
+    e.preventDefault();
+    $('rt').css('visibility', 'hidden');
+    e.originalEvent.clipboardData.setData('text', window.getSelection().toString());
+    $('rt').css('visibility', 'visible');
+});

commit 1cd66137805f4ee00237ca177e15f200a878357a

Fix copy-pasting hack for Chrome
When selecting a text inside a text field, window.getSelection()
is not the way to get the selected text. As a result, that code
prevented copy-pasting in text fields.

This bug was introduced by 70ef3140f86dbfe878c7873b53cf227b29815b46.
Closes #1593.
Refs #1077.

app/webroot/js/generic_functions.js
@@ -111,8 +111,11 @@ $(document).ready(function() {
 // Fix for Chrome: prevent copy-pasting furigana when selecting a sentence
 // https://stackoverflow.com/questions/13438391
 $(document).on('copy', function (e) {
-    e.preventDefault();
-    $('rt').css('visibility', 'hidden');
-    e.originalEvent.clipboardData.setData('text', window.getSelection().toString());
-    $('rt').css('visibility', 'visible');
+    var sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+        e.preventDefault();
+        $('rt').css('visibility', 'hidden');
+        e.originalEvent.clipboardData.setData('text', sel.toString());
+        $('rt').css('visibility', 'visible');
+    }
 });