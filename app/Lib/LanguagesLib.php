ad9fa1e9deae08cb8bc861899f5b0ecede5ebfc0
RyckRichards <rvj1444@gmail.com>
2018-07-10 11:28:36 -0300

    #1600 Added language Albanian (Gheg)

--- app/Lib/LanguagesLib.php
+++ app/Lib/LanguagesLib.php
@@ -555,6 +555,7 @@ class LanguagesLib
                 'cmo' => __d('languages', 'Mnong, Central'),
                 'koi' => __d('languages', 'Komi-Permyak'),
                 'ike' => __d('languages', 'Inuktitut'), 
+                'aln' => __d('languages', 'Albanian (Gheg)'),
             );
         }
         return $languages;
