1c1c818f57e053f2791638c9b52deed5cf11d495
RyckRichards <rvj1444@gmail.com>
2018-07-10 11:15:55 -0300

    #1601 Added language Aklanon

--- app/Lib/LanguagesLib.php
+++ app/Lib/LanguagesLib.php
@@ -555,6 +555,7 @@ class LanguagesLib
                 'cmo' => __d('languages', 'Mnong, Central'),
                 'koi' => __d('languages', 'Komi-Permyak'),
                 'ike' => __d('languages', 'Inuktitut'), 
+                'akl' => __d('languages', 'Aklanon'),
             );
         }
         return $languages;
