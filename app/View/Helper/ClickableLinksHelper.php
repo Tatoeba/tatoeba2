Tatoeba/tatoeba2

commit bf5472cef6ef4409e1bd5fed7d5035d09d3d1b98

Stop cutting through entities when shortening URLs
Fixes #698.

app/View/Helper/ClickableLinksHelper.php
@@ -64,10 +64,15 @@ class ClickableLinksHelper extends AppHelper
 
 
             foreach (array_unique($urls[1]) as $url) {
-                if (mb_strlen($url) > $maxUrlLength) {
-                    $urlText = mb_substr($url, 0, $offset1)
-                        . '...'
-                        . mb_substr($url, -$offset2);
+                $displayedChars = preg_split(
+                    '/(&[^;]+;|.)/u',
+                    $url,
+                    -1,
+                    PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY
+                );
+                if (count($displayedChars) > $maxUrlLength) {
+                    array_splice($displayedChars, $offset1, -$offset2, array('.', '.', '.'));
+                    $urlText = implode($displayedChars);
                 } else {
                     $urlText = $url;
                 }

app/Test/Case/View/Helper/MessagesHelperTest.php
@@ -27,6 +27,8 @@ class MessagesHelperTest extends CakeTestCase {
                 => 'with entity <a href="http://example.com/foo?bar=1&amp;baz=2" target="_blank">http://example.com/foo?bar=1&amp;baz=2</a>',
             'long link http://example.com/some-page-there?p=yesAndThisParam23=no'
                 => 'long link <a href="http://example.com/some-page-there?p=yesAndThisParam23=no" target="_blank">http://example.com/some-page-th...ThisParam23=no</a>',
+            'long link with entities http://example.com/some-page-there?p=yes&&&&&&&Param23=no'
+                => 'long link with entities <a href="http://example.com/some-page-there?p=yes&amp;&amp;&amp;&amp;&amp;&amp;&amp;Param23=no" target="_blank">http://example.com/some-page-th...&amp;&amp;&amp;&amp;Param23=no</a>',
         );
         foreach ($tests as $text => $formatedHTML) {
             $result = $this->Messages->formatedContent($text);

commit 00954cad5a33d310983b77058d7a28ffcd58c100

Fix links ending with an HTML entity
Regression introduced by bf5472cef6ef4409e1bd5fed7d5035d09d3d1b98.

app/View/Helper/ClickableLinksHelper.php
@@ -79,7 +79,7 @@ class ClickableLinksHelper extends AppHelper
 
                 // Checking last character and taking it out if it's punctuation
                 $unwantedLastCharacters = array('!', '.', ',', ';', ':');
-                $lastCharacter = mb_substr($url, -1, 1);
+                $lastCharacter = end($displayedChars);
                 if (in_array($lastCharacter, $unwantedLastCharacters)) {
                     $url = mb_substr($url, 0, -1);
                     $urlText = mb_substr($urlText, 0, -1);

app/Test/Case/View/Helper/MessagesHelperTest.php
@@ -35,6 +35,10 @@ class MessagesHelperTest extends CakeTestCase {
                 => 'link <a href="http://example.com/parenthesis)" target="_blank">http://example.com/parenthesis)</a>',
             'link http://example.com/parenthesis http://example.com/parenthesis)'
                 => 'link <a href="http://example.com/parenthesis" target="_blank">http://example.com/parenthesis</a> <a href="http://example.com/parenthesis)" target="_blank">http://example.com/parenthesis)</a>',
+            'link http://example.com/page&'
+                => 'link <a href="http://example.com/page&amp;" target="_blank">http://example.com/page&amp;</a>',
+            'link http://example.com/page;'
+                => 'link <a href="http://example.com/page" target="_blank">http://example.com/page</a>;',
         );
         foreach ($tests as $text => $formatedHTML) {
             $result = $this->Messages->formatedContent($text);