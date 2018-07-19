7e942f1698483ecf0d118844efd09183d1395056
時流 <jiru@users.noreply.github.com>
2018-07-19 17:12:46 +0000

    Fix SQL syntax error

--- docs/database/updates/2018-06-19.sql
+++ docs/database/updates/2018-06-19.sql
@@ -1,3 +1,3 @@
 ALTER TABLE sentences
-  ADD `license` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+  ADD `license` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   ADD `based_on_id` int(11) DEFAULT NULL;
