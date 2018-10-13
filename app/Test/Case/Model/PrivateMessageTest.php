ccb1d21cee6be3463487e7af5282c4bb04c0f0d9
時流 <jiru@users.noreply.github.com>
2018-10-13 01:53:38 +0000

    Add test for draft edition

--- app/Test/Case/Model/PrivateMessageTest.php
+++ app/Test/Case/Model/PrivateMessageTest.php
@@ -48,4 +48,36 @@ class PrivateMessageTest extends CakeTestCase {
         $pm = $this->PrivateMessage->findById($id);
         $this->assertEqual($expectedPm, $pm['PrivateMessage']);
     }
+
+    public function testSaveDraft_editsExistingDraft() {
+        $draftId = 5;
+        $date = '2017/10/13 01:07:10';
+        $postData = array(
+            'PrivateMessage' => array(
+                'recpt' => 'advanced_contributor',
+                'title' => 'My feelings',
+                'content' => 'I\'m worrying about Tom. What do you think?',
+                'messageId' => $draftId,
+                'submitType' => 'saveDraft',
+            ),
+        );
+
+        $this->PrivateMessage->saveDraft(4, $date, $postData);
+
+        $expectedPm = array(
+            'id'      => $draftId,
+            'recpt'   => 0,
+            'sender'  => 4,
+            'user_id' => 4,
+            'date'    => '2017-10-13 01:07:10',
+            'folder'  => 'Drafts',
+            'title'   => 'My feelings',
+            'content' => 'I\'m worrying about Tom. What do you think?',
+            'sent'    => 0,
+            'isnonread' => 1,
+            'draft_recpts' => 'advanced_contributor',
+        );
+        $pm = $this->PrivateMessage->findById($draftId);
+        $this->assertEqual($expectedPm, $pm['PrivateMessage']);
+    }
 }
