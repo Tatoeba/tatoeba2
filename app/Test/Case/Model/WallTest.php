239f070db766610d0c1e140c0e6bbafa756173ef
時流 <jiru@users.noreply.github.com>
2018-10-19 02:13:24 +0000

    Add a first test for the Wall model

---
+++ app/Test/Case/Model/WallTest.php
@@ -0,0 +1,45 @@
+<?php
+App::uses('Wall', 'Model');
+
+class WallTest extends CakeTestCase {
+
+    public $fixtures = array(
+        'app.wall',
+    );
+
+    public function setUp() {
+        parent::setUp();
+        $this->Wall = ClassRegistry::init('Wall');
+    }
+
+    public function tearDown() {
+        unset($this->Wall);
+
+        parent::tearDown();
+    }
+
+    public function testSave_createsPost() {
+        $newPost = array(
+            'owner' => 2,
+            'date' => '2018-01-02 03:04:05',
+            'content' => 'Hi everyone!',
+        );
+        $expected = array('Wall' => array(
+            'owner' => 2,
+            'date' => '2018-01-02 03:04:05',
+            'content' => 'Hi everyone!',
+//            'modified' => date(),
+            'lft' => 5,
+            'rght' => 6,
+            'id' => 3,
+        ));
+
+        $before = $this->Wall->find('count');
+        $saved = $this->Wall->save($newPost);
+        $after = $this->Wall->find('count');
+
+        $this->assertEquals(1, $after - $before);
+        unset($saved['Wall']['modified']);
+        $this->assertEquals($expected, $saved);
+    }
+}
