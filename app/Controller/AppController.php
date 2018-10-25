Tatoeba/tatoeba2

commit d094cb296e92e57a9c5ef783e4dde4460e0ae5f5

Fix redirection to last page on the Wall
Ref. #1637

Set value for $object regardless if it's an array or not. In the case of the Wall, $object is actually null and without $object being set to the model name, the rest doesn't work.

Added a test for this, but can't get it to pass. There's some error I don't have time investigate right now.

app/Controller/AppController.php
@@ -282,8 +282,8 @@ class AppController extends Controller
     {
         if (is_array($object)) {
             $scope = $object;
-            $object = $this->modelClass;
         }
+        $object = $this->modelClass;
         $findOptions = array_key_exists($object, $this->paginate) ? $this->paginate[$object] : $this->paginate;
         if (is_array($scope) && !empty($scope)) {
             if (!isset($findOptions['conditions'])) {

app/Controller/WallController.php
@@ -25,6 +25,8 @@
  * @link     http://tatoeba.org
  */
 
+App::uses('AppController', 'Controller');
+
 /**
  * Controller for the wall.
  *

app/Test/Case/Controller/WallControllerTest.php
@@ -1,0 +1,52 @@
+<?php
+App::uses('WallController', 'Controller');
+
+class WallControllerTest extends ControllerTestCase {
+
+    public $fixtures = array(
+        'app.wall',
+        'app.wall_thread',
+        'app.user',
+        'app.users_language'
+    );
+
+    public function setUp() {
+        Configure::write('App.base', ''); // prevent using the filesystem path as base
+        $this->controller = $this->generate('Wall', array(
+            'methods' => array('redirect'),
+        ));
+    }
+
+    public function endTest($method) {
+        unset($this->controller);
+    }
+
+    public function testPaginateRedirectsPageOutOfBoundsToLastPage() {
+        $userId = 1;
+        $lastPage = 3; // TODO Should be 2
+
+        $initialDate = new DateTime('2018-01-01');
+		for ($i = 0; $i <= 15; $i++) {
+            $date = $initialDate->format('Y-m-d H:i:s');
+			$message = array(
+                'content' => "Wall message $i",
+                'date' => $date,
+				'owner' => $userId,
+            );
+            $this->controller->Wall->save($message);
+            $this->controller->update_thread_date(
+                $this->controller->Wall->id, $date 
+            );
+
+            $this->controller->Wall->id = null;
+            $initialDate->modify("+1 day");
+		}
+        
+        $this->controller
+             ->expects($this->once())
+             ->method('redirect')
+             ->with("/eng/wall/index/page:$lastPage");
+        $this->testAction("/eng/wall/index/page:9999999");
+    }
+}
+?>

commit ada1f33034b5d575ceefa0e4f13fcd0d0075e91f

Fix SentencesControllerTest
Fixes testPaginateRedirectsPageOutOfBoundsToLastPage() in
Test/Case/Controller/SentencesControllerTest.php.

Regression introduced by
d094cb296e92e57a9c5ef783e4dde4460e0ae5f5.

CakePHP initializes $this->modelClass in
Controller::__construct() based on the name of the
controller, but for some reason, it later resets it to
$this->uses[0] in Controller::constructClasses().
(I really don’t understand why.) As a result, it equals
"Audio" for SentencesController, which breaks that code.

app/Controller/AppController.php
@@ -283,7 +283,9 @@ class AppController extends Controller
         if (is_array($object)) {
             $scope = $object;
         }
-        $object = $this->modelClass;
+        if (is_null($object)) {
+            $object = $this->modelClass;
+        }
         $findOptions = array_key_exists($object, $this->paginate) ? $this->paginate[$object] : $this->paginate;
         if (is_array($scope) && !empty($scope)) {
             if (!isset($findOptions['conditions'])) {