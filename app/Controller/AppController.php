Tatoeba/tatoeba2

commit ada1f33034b5d575ceefa0e4f13fcd0d0075e91f

Fix SentencesControllerTest
Fixes testPaginateRedirectsPageOutOfBoundsToLastPage() in
Test/Case/Controller/SentencesControllerTest.php.

Regression introduced by
d094cb296e92e57a9c5ef783e4dde4460e0ae5f5.

CakePHP initializes $this->modelClass in
Controller::__construct() based on the name of the
controller, but for some reason, it later resets it to
$this->uses[0] in Controller::constructClasses().
(I really don’t understand why.) As a result, it equals
"Audio" for SentencesController, which breaks that code.

app/Controller/AppController.php
@@ -283,7 +283,9 @@ class AppController extends Controller
         if (is_array($object)) {
             $scope = $object;
         }
-        $object = $this->modelClass;
+        if (is_null($object)) {
+            $object = $this->modelClass;
+        }
         $findOptions = array_key_exists($object, $this->paginate) ? $this->paginate[$object] : $this->paginate;
         if (is_array($scope) && !empty($scope)) {
             if (!isset($findOptions['conditions'])) {

commit e1e5138d6b25785c1ccb31e96900c5b02bafdc90

Fix UsersControllerTest
Fix testPaginateRedirectsPageOutOfBoundsToLastPage().

Regression introduced by
ada1f33034b5d575ceefa0e4f13fcd0d0075e91f.

app/Controller/AppController.php
@@ -282,8 +282,8 @@ class AppController extends Controller
     {
         if (is_array($object)) {
             $scope = $object;
-        }
-        if (is_null($object)) {
+            $object = $this->modelClass;
+        } elseif (is_null($object)) {
             $object = $this->modelClass;
         }
         $findOptions = array_key_exists($object, $this->paginate) ? $this->paginate[$object] : $this->paginate;
e1e5138d6b25785c1ccb31e96900c5b02bafdc90
時流 <jiru@users.noreply.github.com>
2018-10-26 00:30:26 +0000

    Fix UsersControllerTest
    Fix testPaginateRedirectsPageOutOfBoundsToLastPage().
    
    Regression introduced by
    ada1f33034b5d575ceefa0e4f13fcd0d0075e91f.

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -282,8 +282,8 @@ class AppController extends Controller
     {
         if (is_array($object)) {
             $scope = $object;
-        }
-        if (is_null($object)) {
+            $object = $this->modelClass;
+        } elseif (is_null($object)) {
             $object = $this->modelClass;
         }
         $findOptions = array_key_exists($object, $this->paginate) ? $this->paginate[$object] : $this->paginate;
