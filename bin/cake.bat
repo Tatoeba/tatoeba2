a5c63be088c58eaaf154d2a9b385c3caee3923af
時流 <jiru@users.noreply.github.com>
2018-11-03 04:50:01 +0000

    Add console executable from CakePHP 3 skeleton app

--- bin/cake.bat
+++ bin/cake.bat
@@ -1,30 +1,26 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
-:: Bake is a shell script for running CakePHP bake script
+:: Cake is a Windows batch script for invoking CakePHP shell commands
 ::
-:: CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
-:: Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+:: CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+:: Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
 ::
 :: Licensed under The MIT License
 :: Redistributions of files must retain the above copyright notice.
 ::
-:: @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
-:: @link          http://cakephp.org CakePHP(tm) Project
-:: @package       app.Console
-:: @since         CakePHP(tm) v 2.0
-:: @license       http://www.opensource.org/licenses/mit-license.php MIT License
+:: @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+:: @link          https://cakephp.org CakePHP(tm) Project
+:: @since         2.0.0
+:: @license       https://opensource.org/licenses/mit-license.php MIT License
 ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: In order for this script to work as intended, the cake\console\ folder must be in your PATH
-
-@echo.
 @echo off
 
 SET app=%0
 SET lib=%~dp0
 
-php -q "%lib%cake.php" -working "%CD% " %*
+php "%lib%cake.php" %*
 
 echo.
 
