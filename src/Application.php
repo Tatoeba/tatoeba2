89ed5487d47bb727de56bf1d599d727de64b6fee
時流 <jiru@users.noreply.github.com>
2018-11-03 04:53:42 +0000

    Remove the loading of plugins we don’t use (yet)

--- src/Application.php
+++ src/Application.php
@@ -31,33 +31,6 @@ use Cake\Routing\Middleware\RoutingMiddleware;
 class Application extends BaseApplication
 {
     /**
-     * {@inheritDoc}
-     */
-    public function bootstrap()
-    {
-        // Call parent to load bootstrap from files.
-        parent::bootstrap();
-
-        if (PHP_SAPI === 'cli') {
-            try {
-                $this->addPlugin('Bake');
-            } catch (MissingPluginException $e) {
-                // Do not halt if the plugin is missing
-            }
-
-            $this->addPlugin('Migrations');
-        }
-
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
-        if (Configure::read('debug')) {
-            $this->addPlugin(\DebugKit\Plugin::class);
-        }
-    }
-
-    /**
      * Setup the middleware queue your application will use.
      *
      * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
