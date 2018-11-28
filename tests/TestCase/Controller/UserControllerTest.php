373dc7b406705de6a8b3c5ef864d48abc7b97bc5
時流 <jiru@users.noreply.github.com>
2018-11-28 23:35:04 +0000

    Rewrite UserController test for CakePHP 3

--- tests/TestCase/Controller/UserControllerTest.php
+++ tests/TestCase/Controller/UserControllerTest.php
@@ -3,48 +3,45 @@ namespace App\Test\TestCase\Controller;
 
 use App\Controller\UserController;
 use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\IntegrationTestCase;
 
-class UserControllerTest extends ControllerTestCase {
-
-    public $fixtures = array(
+class UserControllerTest extends IntegrationTestCase
+{
+    public $fixtures = [
         'app.aros',
         'app.acos',
         'app.aros_acos',
         'app.users',
-        'app.sentences',
+        'app.groups',
         'app.users_languages'
-    );
+    ];
 
-    private $oldPasswords = array();
+    private $oldPasswords = [];
 
     public function setUp() {
-        $_COOKIE = array();
+        parent::setUp();
         Configure::write('Acl.database', 'test');
-    }
+        Configure::write('Security.salt', 'ze@9422#5dS?!99xx');
 
-    public function startTest($method) {
-        $this->controller = $this->generate('User');
-        $users = $this->controller->User->find('all', array(
-            'fields' => array('username', 'password'),
-        ));
-        $this->oldPasswords = Set::combine($users, '{n}.User.username', '{n}.User.password');
-    }
-
-    public function tearDown() {
-        $this->controller->Auth->Session->destroy();
-        unset($this->controller);
+        $users = TableRegistry::get('Users');
+        $users = $users->find()->select(['username', 'password'])->all();
+        $this->oldPasswords = $users->combine('username', 'password')->toArray();
     }
 
     private function logInAs($username) {
-        $user = $this->controller->User->find('first', array(
-            'conditions' => array('username' => $username),
-        ));
-        $this->controller->Auth->login($user['User']);
+        $users = TableRegistry::get('Users');
+        $user = $users->findByUsername($username)->first();
+        $this->session(['Auth' => [ 'User' => $user->toArray()]]);
+
+        $this->enableCsrfToken();
+        $this->enableSecurityToken();
     }
 
     private function assertPassword($what, $username) {
-        $user = $this->controller->User->findByUsername($username);
-        $currentPassword = $user['User']['password'];
+        $users = TableRegistry::get('Users');
+        $user = $users->findByUsername($username)->first();
+        $currentPassword = $user->password;
         $oldPassword = $this->oldPasswords[$username];
         if ($oldPassword) {
             $message = "Password of '$username' $what";
@@ -61,8 +58,7 @@ class UserControllerTest extends ControllerTestCase {
     }
 
     private function assertFlashMessage($message) {
-        $flash = $this->controller->Session->read('Message.flash');
-        $this->assertEquals($message, $flash['message'], "Flash message equals '$message'");
+        $this->assertSession($message, 'Flash.flash.0.message');
     }
 
     public function testSavePassword_changesPassword() {
@@ -70,15 +66,11 @@ class UserControllerTest extends ControllerTestCase {
         $oldPassword = '123456';
         $newPassword = '9{FA0E;pL#R(5JllB{wHWTO;6';
         $this->logInAs($username);
-        $this->testAction('/eng/save_password', array(
-            'data' => array(
-                'User' => array(
-                    'old_password' => $oldPassword,
-                    'new_password' => $newPassword,
-                    'new_password2' => $newPassword,
-                )
-            )
-        ));
+        $this->post('/eng/user/save_password', [
+            'old_password' => $oldPassword,
+            'new_password' => $newPassword,
+            'new_password2' => $newPassword,
+        ]);
         $this->assertPassword('changed', $username);
     }
 
@@ -87,15 +79,12 @@ class UserControllerTest extends ControllerTestCase {
         $oldPassword = '123456';
         $newPassword = '';
         $this->logInAs($username);
-        $this->testAction('/eng/save_password', array(
-            'data' => array(
-                'User' => array(
-                    'old_password' => $oldPassword,
-                    'new_password' => $newPassword,
-                    'new_password2' => $newPassword,
-                )
-            )
-        ));
+        $this->enableRetainFlashMessages();
+        $this->post('/eng/user/save_password', [
+            'old_password' => $oldPassword,
+            'new_password' => $newPassword,
+            'new_password2' => $newPassword,
+        ]);
         $this->assertPassword("didn't change", $username);
         $this->assertFlashMessage('New password cannot be empty.');
     }
@@ -105,15 +94,11 @@ class UserControllerTest extends ControllerTestCase {
         $oldPassword = 'incorrect password';
         $newPassword = '9{FA0E;pL#R(5JllB{wHWTO;6';
         $this->logInAs($username);
-        $this->testAction('/eng/save_password', array(
-            'data' => array(
-                'User' => array(
-                    'old_password' => $oldPassword,
-                    'new_password' => $newPassword,
-                    'new_password2' => $newPassword,
-                )
-            )
-        ));
+        $this->post('/eng/user/save_password', [
+            'old_password' => $oldPassword,
+            'new_password' => $newPassword,
+            'new_password2' => $newPassword,
+        ]);
         $this->assertPassword("didn't change", $username);
         $this->assertFlashMessage('Password error. Please try again.');
     }
@@ -122,15 +107,11 @@ class UserControllerTest extends ControllerTestCase {
         $username = 'contributor';
         $oldPassword = '123456';
         $this->logInAs($username);
-        $this->testAction('/eng/save_password', array(
-            'data' => array(
-                'User' => array(
-                    'old_password' => $oldPassword,
-                    'new_password' => 'something',
-                    'new_password2' => 'something different',
-                )
-            )
-        ));
+        $this->post('/eng/user/save_password', [
+            'old_password' => $oldPassword,
+            'new_password' => 'something',
+            'new_password2' => 'something different',
+        ]);
         $this->assertPassword("didn't change", $username);
         $this->assertFlashMessage('New passwords do not match.');
     }
@@ -139,15 +120,10 @@ class UserControllerTest extends ControllerTestCase {
         $username = 'contributor';
         $newEmail = 'contributor_newemail@example.org';
         $this->logInAs($username);
-        $oldEmail = $this->controller->Auth->user('email');
-        $this->testAction('/eng/save_basic', array(
-            'data' => array(
-                'User' => array(
-                    'email' => $newEmail,
-                )
-            )
-        ));
-        $this->assertEquals($this->controller->Auth->user('username'), $username);
-        $this->assertEquals($this->controller->Auth->user('email'), $newEmail);
+        $this->post('/eng/user/save_basic', [
+            'email' => $newEmail,
+        ]);
+        $this->assertEquals($this->_controller->Auth->user('username'), $username);
+        $this->assertEquals($this->_controller->Auth->user('email'), $newEmail);
     }
 }
