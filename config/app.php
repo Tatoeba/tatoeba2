1d2a16b4cc74f2ff122f920c76f03d707367a713
時流 <jiru@users.noreply.github.com>
2018-11-29 06:49:58 +0000

    Disable deprecation warnings for CakePHP 4
    We’re not even done with migrating to CakePHP 3,
    so no need to worry about version 4 yet.

--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
      *   breathing room to complete logging or error handling.
      */
     'Error' => [
-        'errorLevel' => E_ALL & ~E_DEPRECATED,
+        'errorLevel' => E_ALL & ~E_USER_DEPRECATED,
         'exceptionRenderer' => 'Cake\Error\ExceptionRenderer',
         'skipLog' => [],
         'log' => true,
