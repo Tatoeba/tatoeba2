9b3aed9ef3e8d5ecb1ea1bf552a946f0c5c056a3
Trang <trang@tatoeba.org>
2019-01-26 15:50:24 +0100

    Update runner.sh
    Ref. #1756

--- docs/cron/runner.sh
+++ docs/cron/runner.sh
@@ -3,8 +3,8 @@
 cmd="$1"
 shift 1
 
-DB_USER=$(php -r 'require "/var/www-prod/app/config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["login"];')
-DB_PASS=$(php -r 'require "/var/www-prod/app/config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["password"];')
-DB=$(php -r 'require "/var/www-prod/app/config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["database"];')
+DB_USER=$(php -r 'require "/var/www-prod/config/paths.php"; $config = include "/var/www-prod/config/app_local.php"; echo $config["Datasources"]["default"]["username"];')
+DB_PASS=$(php -r 'require "/var/www-prod/config/paths.php"; $config = include "/var/www-prod/config/app_local.php"; echo $config["Datasources"]["default"]["password"];')
+DB=$(php -r 'require "/var/www-prod/config/paths.php"; $config = include "/var/www-prod/config/app_local.php"; echo $config["Datasources"]["default"]["database"];')
 
 DB_PASS="$DB_PASS" DB_USER="$DB_USER" DB="$DB" "$cmd" "$@"
