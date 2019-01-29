1eae3ffab0adc8b3b497ccd4787d7f057c4452be
Trang <trang@tatoeba.org>
2019-01-29 08:51:56 +0100

    Update dump_db.sh
    Removed the triggers and routines. It would prevent to fully export the database.
    
    Added missing code from the prod version, to keep the previous dump.

--- docs/cron/dump_db.sh
+++ docs/cron/dump_db.sh
@@ -4,5 +4,10 @@ set -e
 PATH=$PATH:/usr/local/mysql/bin
 HOME='/home/debian'
 
-tar -zcf "$HOME""/dump/db_old.tar.gz" "$HOME""/dump/db.sql"
-mysqldump -u "$DB_USER" -p"$DB_PASS" --routines --triggers "$DB" > "$HOME""/dump/db.sql"
+if [[ -f "$HOME/dump/db.tar.gz" ]]; then
+    mv "$HOME/dump/db.tar.gz" "$HOME/dump/db_old.tar.gz"
+fi
+
+mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB" > "$HOME""/dump/db.sql"
+tar -zcf "$HOME""/dump/db.tar.gz" "$HOME""/dump/db.sql"
+rm "$HOME/dump/db.sql"
