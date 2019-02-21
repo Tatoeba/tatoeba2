5ba981795a0f9a4fb619a418428217e76a9db593
Trang <trang@tatoeba.org>
2019-02-21 20:56:46 +0100

    Update database scripts
    Removed the acl files in the 'import' folder because they are outdated and therefore useless.
    
    Removed the creation of tables in the script to import groups and users. The tables are already defined in the 'tables' folder. We don't want to maintain the definition in two places.
    
    Updated the scripts in the 'acl' folder to use `bin/cake` instead of just `cake`. This way new devs can directly run `sh docs/database/acl/{file}`.

--- docs/database/import/groups.sql
+++ docs/database/import/groups.sql
@@ -1,53 +1,7 @@
--- MySQL dump 10.13  Distrib 5.5.25, for Linux (x86_64)
---
--- Host: localhost    Database: tatoeba_prod
--- ------------------------------------------------------
--- Server version	5.5.25-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `groups`
---
-
-DROP TABLE IF EXISTS `groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `groups`
---
-
-LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'admin','2009-01-11 21:23:50','2009-01-11 21:23:50'),(2,'moderator','2009-01-11 21:24:03','2009-01-11 21:24:03'),(3,'trusted_user','2009-01-11 21:24:13','2009-01-11 21:24:13'),(4,'user','2009-01-11 21:24:22','2009-01-11 21:24:22'),(5,'inactive','2009-01-11 21:24:29','2009-01-11 21:24:29'),(6,'spammer','2009-01-31 02:43:41','2009-01-31 02:43:41');
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-04-07 11:40:08
+INSERT INTO `groups` VALUES 
+(1,'admin','2009-01-11 21:23:50','2009-01-11 21:23:50'),
+(2,'moderator','2009-01-11 21:24:03','2009-01-11 21:24:03'),
+(3,'trusted_user','2009-01-11 21:24:13','2009-01-11 21:24:13'),
+(4,'user','2009-01-11 21:24:22','2009-01-11 21:24:22'),
+(5,'inactive','2009-01-11 21:24:29','2009-01-11 21:24:29'),
+(6,'spammer','2009-01-31 02:43:41','2009-01-31 02:43:41');
--- docs/database/import/users.sql
+++ docs/database/import/users.sql
@@ -1,57 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Client: localhost
--- Généré le : Dim 07 Avril 2013 à 12:19
--- Version du serveur: 5.5.16
--- Version de PHP: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `tatoeba_2`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(62) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `since` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `last_time_active` int(11) NOT NULL DEFAULT '0',
-  `level` tinyint(2) NOT NULL DEFAULT '0',
-  `group_id` tinyint(4) NOT NULL,
-  `send_notifications` tinyint(1) NOT NULL DEFAULT '1',
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `birthday` datetime DEFAULT NULL,
-  `description` blob NOT NULL,
-  `settings` blob NOT NULL,
-  `homepage` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `image` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `country_id` varchar(2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `login` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
--- Contenu de la table `users`
---
-
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `since`, `last_time_active`, `level`, `group_id`, `send_notifications`, `name`, `birthday`, `description`, `settings`, `homepage`, `image`, `country_id`) VALUES
 
 (1, 'admin', '1 $2a$10$ze.r64Pv1E.CTHUNFpFkVuqxuYlbj00aqqeRDd.kumCTIVtkZ/Q8C', 'admin@fakemail.com', '2013-04-07 12:15:16', 0, 1, 1, 1, '', NULL, '', '', '', '', NULL),
@@ -60,7 +6,3 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `since`, `last_time_
 (4, 'contributor', '1 $2a$10$lGq/QSz3nwHsDAHubtPC7ebT2dnpVTtcSWCV1LJ/vhOwNuJp3Jxay', 'contributor@fakemail.com', '2013-04-07 12:17:02', 0, 1, 4, 1, '', NULL, '', '', '', '', NULL),
 (5, 'inactive', '1 $2a$10$vCE03mwxDuuhISbs9mRnTekuwqPrnTfLjHLoH0zGBg53JE1Z3hl/q', 'inactive@fakemail.com', '2013-04-07 12:17:29', 0, 1, 5, 1, '', NULL, '', '', '', '', NULL),
 (6, 'spammer', '1 $2a$10$x68Xtpx56iQubb0LHZN/3ez8auzxe3EnOj8GfY7Xn2DIKjpJIBEry', 'spammer@fakemail.com', '2013-04-07 12:17:54', 0, 1, 6, 1, '', NULL, '', '', '', '', NULL);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
