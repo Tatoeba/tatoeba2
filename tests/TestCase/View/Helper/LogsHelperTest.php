64f1c03ea53b1ecc5e35d6cb3b1fa0d52b211697
時流 <jiru@users.noreply.github.com>
2019-03-12 12:01:55 +0000

    Add test for LogsHelper::obsoletize()
    Refs #1813.

---
+++ tests/TestCase/View/Helper/LogsHelperTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\LogsHelper;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+class LogsHelperTest extends TestCase
+{
+    public $fixtures = [
+        'app.contributions',
+    ];
+
+    public $LogsHelper;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $view = new View();
+        $this->LogsHelper = new LogsHelper($view);
+    }
+
+    public function tearDown()
+    {
+        unset($this->LogsHelper);
+        parent::tearDown();
+    }
+
+    public function testObsoletize()
+    {
+        $contributions = TableRegistry::get('Contributions');
+        $latests = $contributions->find()->where(['sentence_id' => 35])->toList();
+
+        $this->LogsHelper->obsoletize($latests);
+
+        $this->assertTrue($latests[0]->obsolete);
+    }
+}
