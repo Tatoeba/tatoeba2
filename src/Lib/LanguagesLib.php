884ef19e470a5e08eadf6ccf21b5ce48ff954da3
RyckRichards <rvj1444@gmail.com>
2019-03-24 20:51:57 -0300

    #1820 Added Ligurian

--- src/Lib/LanguagesLib.php
+++ src/Lib/LanguagesLib.php
@@ -562,6 +562,7 @@ class LanguagesLib
                 'aoz' => __d('languages', 'Uab Meto'), 
                 'shy' => __d('languages', 'Tachawit'),
                 'fkv' => __d('languages', 'Kven Finnish'),
+                'lij' => __d('languages', 'Ligurian'),
             );
         }
         return $languages;
