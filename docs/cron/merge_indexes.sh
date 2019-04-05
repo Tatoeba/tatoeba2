4a039626e6da9b09881e5e4a8cfac9f097e75f28
時流 <jiru@users.noreply.github.com>
2019-04-05 17:00:05 +0000

    Update cron files for Manticore
    With Sphinx, we had our own .service file that used to run
    searchd with --console. This options prevents Sphinx from
    creating a PID file, so we needed to set the PID file manually.
    Now we use manticore’s .deb and it comes with a .service file
    that does not run searchd with --console.
    
    Refs #1518.

--- docs/cron/merge_indexes.sh
+++ docs/cron/merge_indexes.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-PIDFILE=/var/run/sphinxsearch/searchd.pid
-
-systemctl show -p MainPID --value sphinxsearch > $PIDFILE
 /var/www-prod/bin/cake sphinx_indexes merge >/dev/null 2>&1
--- docs/cron/refresh_delta.sh
+++ docs/cron/refresh_delta.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-PIDFILE=/var/run/sphinxsearch/searchd.pid
-
-systemctl show -p MainPID --value sphinxsearch > $PIDFILE
 /usr/bin/time -p -a -o /tmp/sphinx.delta.update.log /var/www-prod/bin/cake sphinx_indexes update delta >/dev/null 2>&1
