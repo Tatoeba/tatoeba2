2e21c5db2357c5478ecf7dccd96481a3993ad7ec
時流 <jiru@users.noreply.github.com>
2019-04-24 03:23:57 +0000

    Use tags.csv in ngrams regeneration cron task
    It’s used to exclude sentences tagged with @change flag.

--- docs/cron/regenerate-ngrams.sh
+++ docs/cron/regenerate-ngrams.sh
@@ -4,7 +4,8 @@ set -e
 NGRAMS_DB=/etc/ngrams.db
 NGRAMS_DB_TMP=/etc/ngrams.db.new
 SENTENCES_DETAILED_CSV=/var/www-downloads/exports/sentences_detailed.csv
+TAGS_CSV=/var/www-downloads/exports/tags.csv
 
-nice -n 19 tatodetect-generate-ngrams.py "$SENTENCES_DETAILED_CSV" "$NGRAMS_DB_TMP"
+nice -n 19 tatodetect-generate-ngrams.py "$SENTENCES_DETAILED_CSV" "$NGRAMS_DB_TMP" "$TAGS_CSV"
 mv "$NGRAMS_DB_TMP" "$NGRAMS_DB"
 /etc/init.d/tatodetect restart
