Tatoeba/tatoeba2

commit c3d1307bacc3d3953c0ba78381e139a852f0ae3b

Use good old hidden form instead of ng-hide
because ng-hide only starts working
after dom content is loaded.

src/View/Helper/PrivateMessagesHelper.php
@@ -79,14 +79,12 @@ class PrivateMessagesHelper extends AppHelper
 
         <div class="body">
             <div class="pmFields">
-            <div ng-hide="true">
             <?php
             if (!$isReply) {
-                echo $this->Form->input('messageId', array('value' => $pm->id));
+                echo $this->Form->hidden('messageId', array('value' => $pm->id));
             }
-            echo $this->Form->input('submitType', array('value' => ''));
+            echo $this->Form->hidden('submitType', array('value' => ''));
             ?>
-            </div>
             <?php
             echo $this->Form->control('recipients', [
                 'label' => __x('message', 'To'),

commit ae89d35bf437159b6beec3cb3f663d87b4352d68

Fix auth token error when saving drafts
Having the input as hidden lead to an error when saving a draft:

Tampered field 'submitType' in POST data (expected value '' but found 'saveDraft') Cake\Controller\Exception\AuthSecurityException

This bug was introduced by c3d1307bacc3d3953c0ba78381e139a852f0ae3b.

src/View/Helper/PrivateMessagesHelper.php
@@ -84,6 +84,7 @@ class PrivateMessagesHelper extends AppHelper
                 echo $this->Form->hidden('messageId', array('value' => $pm->id));
             }
             echo $this->Form->hidden('submitType', array('value' => ''));
+            $this->Form->unlockField('submitType');
             ?>
             <?php
             echo $this->Form->control('recipients', [