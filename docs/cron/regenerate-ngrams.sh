e5e2383aa15e38c16038477f4f538a38e1f26134
時流 <jiru@users.noreply.github.com>
2019-05-14 18:05:44 +0000

    Run ngrams generation script as unprivileged user
    for better security.

--- docs/cron/regenerate-ngrams.sh
+++ docs/cron/regenerate-ngrams.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+TATODETECT_USER=tatodetect
 NGRAMS_DB=/etc/ngrams.db
-NGRAMS_DB_TMP=/etc/ngrams.db.new
+NGRAMS_DB_TMP=/tmp/ngrams.db.new
 SENTENCES_DETAILED_CSV=/var/www-downloads/exports/sentences_detailed.csv
 TAGS_CSV=/var/www-downloads/exports/tags.csv
 
-nice -n 19 tatodetect-generate-ngrams.py "$SENTENCES_DETAILED_CSV" "$NGRAMS_DB_TMP" "$TAGS_CSV"
+sudo -u $TATODETECT_USER nice -n 19 tatodetect-generate-ngrams.py "$SENTENCES_DETAILED_CSV" "$NGRAMS_DB_TMP" "$TAGS_CSV"
+chown root:root "$NGRAMS_DB_TMP"
 mv "$NGRAMS_DB_TMP" "$NGRAMS_DB"
 systemctl restart tatodetect
