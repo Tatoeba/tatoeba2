46f3398730d7b267573564da41eae8f3279925cd
時流 <jiru@users.noreply.github.com>
2019-05-16 20:39:20 +0000

    Fix test in specific context
    Fix test testSetSentencesScript() when running only
    TranscriptionsShellTest and when AutoTranscriptions.enabled
    is false. It otherwise passed when running the whole test
    suite because SentenceDerivationShellTest, that was run
    just before, created the reindex_flags table.
    
    AutoTranscriptions.enabled is false

--- tests/TestCase/Shell/TranscriptionsShellTest.php
+++ tests/TestCase/Shell/TranscriptionsShellTest.php
@@ -14,6 +14,7 @@ class TranscriptionsShellTest extends ConsoleIntegrationTestCase
         'app.transcriptions',
         'app.users',
         'app.contributions',
+        'app.reindex_flags',
     ];
 
     public $io;
