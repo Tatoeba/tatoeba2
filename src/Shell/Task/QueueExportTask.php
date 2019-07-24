f20340a6e59888b0805f1e938138ae3dc8a7050c
時流 <jiru@users.noreply.github.com>
2019-07-24 16:54:31 +0800

    Prevent requeueing of failed export jobs
    It’s a functionality we don’t want for the moment.

--- src/Shell/Task/QueueExportTask.php
+++ src/Shell/Task/QueueExportTask.php
@@ -5,6 +5,8 @@ namespace App\Shell\Task;
 use Queue\Shell\Task\QueueTask;
 
 class QueueExportTask extends QueueTask {
+    public $retries = 0;
+
     public function run(array $config, $jobId) {
         $this->loadModel('Exports');
         return $this->Exports->runExport($config, $jobId);
