0878304439c5db4d002b2b84244881bd1914ae38
Yorwba <yorwb4@gmail.com>
2020-02-03 22:40:49 +0100

    Re-add Chrome copy-paste hack
    The hack was reverted in 0fbcf53, because it interfered with the copy
    button feature.

--- webroot/js/generic_functions.js
+++ webroot/js/generic_functions.js
@@ -146,3 +146,15 @@ if (!Array.prototype.find) {
       writable: true
     });
   }
+
+// Fix for Chrome: prevent copy-pasting furigana when selecting a sentence
+// https://stackoverflow.com/questions/13438391
+$(document).on('copy', function (e) {
+    var sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+        e.preventDefault();
+        $('rt').css('visibility', 'hidden');
+        e.originalEvent.clipboardData.setData('text', sel.toString());
+        $('rt').css('visibility', 'visible');
+    }
+});
