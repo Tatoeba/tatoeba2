3e0b751efaaccb6e7bc47fe4648cdc53face5159
Yorwba <yorwb4@gmail.com>
2020-02-03 23:07:13 +0100

    Prevent the copy-paste fix for Chrome from breaking in Firefox

--- webroot/js/generic_functions.js
+++ webroot/js/generic_functions.js
@@ -150,6 +150,10 @@ if (!Array.prototype.find) {
 // Fix for Chrome: prevent copy-pasting furigana when selecting a sentence
 // https://stackoverflow.com/questions/13438391
 $(document).on('copy', function (e) {
+    // ClipboardJS uses a textarea to implement copying in Firefox.
+    // We shouldn't mess with that event.
+    if(e.target.tagName == 'TEXTAREA') return;
+
     var sel = window.getSelection();
     if (sel.rangeCount > 0) {
         e.preventDefault();
