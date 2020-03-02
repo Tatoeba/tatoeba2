112773b0e9585b4e666b9b040e26f383ddea25e8
時流 <jiru@users.noreply.github.com>
2020-03-02 21:29:22 +0800

    Add a new better script to update transltions
    Replace the old script with a new one based on the
    Transifex client. The new script does the same thing
    with less code. In addition, the new script also:
    
    * downloads in parallel
    * retreives the Terms of Service translations
    * retreives languages that are not listed in lang_map
    
    The -a option is meant to be used on dev.tatoeba.org only.
    
    lang_map is now only useful for languages that use a
    different directory name than the Transifex language code.

---
+++ tools/update-translations.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e
+
+check_prerequistes() {
+  if ! which tx >/dev/null 2>&1; then
+    echo "Please install the transifex client first:"
+    echo "  https://docs.transifex.com/client/installing-the-client"
+    exit 1
+  fi
+
+  rcfile="$HOME/.transifexrc"
+  if [ ! -f "$rcfile" ]; then
+    echo "Creating $rcfile..."
+    cat > "$rcfile" <<EOF
+[https://www.transifex.com]
+api_hostname = https://api.transifex.com
+hostname = https://www.transifex.com
+username = 
+password = 
+EOF
+  fi
+  if grep -q "^username[[:space:]]\+=[[:space:]]*$" $rcfile; then
+    echo "Please add your transifex username and password in $rcfile"
+    exit 1
+  fi
+}
+
+pull_translations() {
+  echo "Pulling translations from Transifex..."
+
+  tx_params="--no-interactive --parallel -f -a"
+  if [ "$1" -eq 1 ]; then
+    tx pull $tx_params
+  else
+    tx pull $tx_params -r tatoeba_website.tatoebaResource,tatoeba_website.countries,tatoeba_website.tatoeba-languages,tatoeba_website.admin
+    tx pull $tx_params --minimum-perc 100 -r tatoeba-terms-of-use.terms-of-use
+  fi
+}
+
+compile_po_files() {
+  echo "Compiling PO files..."
+  for file in src/Locale/*/*.po; do
+    msgfmt -o ${file%po}mo "$file"
+  done
+}
+
+remove_cakephp_cached_translation() {
+  echo "Cleaning CakePHP cache..."
+  find ./tmp/cache/persistent/ -type f \! -name empty \! -name 'myapp_cake_core_translations_cake_*' -exec rm -f {} \;
+}
+
+pull_all=0
+if [ "$1" = "-a" ]; then
+  pull_all=1
+elif [ "$#" -gt 0 ]; then
+  echo "Usage: $0 [-a]"
+  echo " -a: force pulling all translations, even incomplete terms of use"
+  exit 1
+fi
+
+check_prerequistes
+pull_translations $pull_all
+compile_po_files
+remove_cakephp_cached_translation
+
+exit 0
