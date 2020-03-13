a842720a093d0a164865ad4ca168e06d779ff024
時流 <jiru@users.noreply.github.com>
2020-03-13 14:19:01 +0800

    Don’t import everything from Sentence entity
    We don’t need all the virtual fields.
    We don’t need the hash stuff.
    So better not extend from Sentence.

--- src/Model/Entity/Translation.php
+++ src/Model/Entity/Translation.php
@@ -1,8 +1,31 @@
 <?php
 namespace App\Model\Entity;
 
-use App\Model\Entity\Sentence;
+use Cake\ORM\Entity;
+use App\Lib\LanguagesLib;
 
-class Translation extends Sentence
-{   
+class Translation extends Entity
+{
+    use LanguageNameTrait;
+
+    protected $_virtual = [
+        'lang_name',
+        'dir',
+        'lang_tag',
+    ];
+
+    protected function _getLangName()
+    {
+        return $this->codeToNameAlone($this->lang);
+    }
+
+    protected function _getDir()
+    {
+        return LanguagesLib::getLanguageDirection($this->lang);
+    }
+
+    protected function _getLangTag()
+    {
+        return LanguagesLib::languageTag($this->lang, $this->script);
+    }
 }
