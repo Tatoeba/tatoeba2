b03b07a386eb57bd7df08767ec3cfea797f3ecfb
時流 <jiru@users.noreply.github.com>
2020-03-13 23:04:40 +0800

    Stop quoting SQL column identifiers
    We used to do that because of Sinogram model, but we got
    rid of it in 323f8b4079a4ecf3407cb10344003771fe4e04d2,
    so we have no reason to keep doing that.

--- config/app.php
+++ config/app.php
@@ -282,7 +282,7 @@ return [
              * decreases performance because each query needs to be traversed and
              * manipulated before being executed.
              */
-            'quoteIdentifiers' => true,
+            'quoteIdentifiers' => false,
 
             /**
              * During development, if using MySQL < 5.6, uncommenting the
@@ -311,7 +311,7 @@ return [
             //'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
-            'quoteIdentifiers' => true,
+            'quoteIdentifiers' => false,
             'log' => false,
             //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
             'url' => env('DATABASE_TEST_URL', null),
