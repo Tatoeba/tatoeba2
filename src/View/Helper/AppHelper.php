b79d629d30791c54bbea3dd69f66343ea70fa3cd
Andreas Perstinger <andipersti@gmail.com>
2020-03-24 21:19:20 +0100

    Improve safeForAngular (should also work for arrays)
    In some places (especially for selection input forms) it is necessary to
    sanitize all values of a (multidimensional) array or tree.

--- src/View/Helper/AppHelper.php
+++ src/View/Helper/AppHelper.php
@@ -66,12 +66,17 @@ class AppHelper extends Helper
      * by AngularJS which could lead to XSS attacks. So we have to sanitize all unsafe
      * strings in our templates.
      *
-     * @param string $string The unsafe string
+     * @param string|array $subject If $subject is an array, all values will be sanitized
+     *                              recursively
      *
      * @return string
      **/
-    public function safeForAngular($string) {
-        return str_replace('{{', "{{ '{{' }}", $string);
+    public function safeForAngular($subject) {
+        if (is_array($subject)) {
+            return array_map([$this, 'safeForAngular'], $subject);
+        } else {
+            return str_replace('{{', "{{ '{{' }}", $subject);
+        }
     }
 }
 ?>
