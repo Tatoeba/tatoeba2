09ced6f4e80000e07bc9b66f17b30c2cdb859ce5
Andreas Perstinger <andipersti@gmail.com>
2020-03-24 19:37:40 +0100

    Add helper function for sanitizing user input sent to AngularJS
    We can use a trick by gillux to simplify the xss prevention measures.

--- src/View/Helper/AppHelper.php
+++ src/View/Helper/AppHelper.php
@@ -30,7 +30,9 @@ use Cake\View\Helper;
 
 
 /**
- * Controller for contributions.
+ * Base helper class
+ *
+ * Contains general functions which are useful for several different helpers and templates.
  *
  * @category App
  * @package  Helper
@@ -55,6 +57,21 @@ class AppHelper extends Helper
         }
 
         return parent::url($url, $full);
+    }
+
+    /**
+     * Sanitize user input sent to AngularJS
+     *
+     * If user input contains '{{...}}' the part inside the braces will be interpolated
+     * by AngularJS which could lead to XSS attacks. So we have to sanitize all unsafe
+     * strings in our templates.
+     *
+     * @param string $string The unsafe string
+     *
+     * @return string
+     **/
+    public function safeForAngular($string) {
+        return str_replace('{{', "{{ '{{' }}", $string);
     }
 }
 ?>
---
+++ tests/TestCase/View/Helper/AppHelperTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use App\View\Helper\AppHelper;
+
+class AppHelperTest extends TestCase {
+
+    public function setUp() {
+        parent::setUp();
+        $View = new View();
+        $this->AppHelper = new AppHelper($View);
+    }
+
+    public function tearDown() {
+        unset($this->AppHelper);
+        parent::tearDown();
+    }
+
+    public function safeForAngularProvider() {
+        return [
+            ['', ''],
+            ['abc', 'abc'],
+            ['{}', '{}'],
+            ['{{}}', "{{ '{{' }}}}"],
+            ['{{{{}}}}', "{{ '{{' }}{{ '{{' }}}}}}"],
+            ['{{{}}}}', "{{ '{{' }}{}}}}"],
+        ];
+    }
+
+    /**
+     * @dataProvider safeForAngularProvider
+     **/
+    public function testSafeForAngular($string, $expected) {
+        $result = $this->AppHelper->safeForAngular($string);
+        $this->assertEquals($expected, $result);
+    }
+}
