81d4f9852caaa376074f527c487ddd7ac57231c3
RyckRichards <rvj1444@gmail.com>
2020-04-08 18:01:52 -0300

    Added language Nyungar (nys)

--- src/Lib/LanguagesLib.php
+++ src/Lib/LanguagesLib.php
@@ -586,6 +586,7 @@ class LanguagesLib
                 'bis' => __d('languages', 'Bislama'),
                 'stq' => __d('languages', 'Saterland Frisian'),
                 'fro' => __d('languages', 'Old French'),
+                'nys' => __d('languages', 'Nyungar'),
             );
         }
         return $languages;
