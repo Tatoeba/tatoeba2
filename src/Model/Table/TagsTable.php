Tatoeba/tatoeba2

commit 3842e94591b47bd02c07a3c2f7d1c5e9b2a8d1b3

Fix bugs introduced by recent changes to the tags feature
1) In the callback function for 'findOrCreate' the entity needs to be
   returned
2) In the controller a duplicated tag was sent back to the client

src/Model/Table/TagsTable.php
@@ -94,36 +94,34 @@ class TagsTable extends Table
             }
         }
 
-        $tag = $this->newEntity([
+        $newTag = $this->newEntity([
             'name' => $tagName,
             'user_id' => $userId,
         ]);
 
-        if ($tag->hasErrors()) {
+        if ($newTag->hasErrors()) {
             return false;
         }
 
-        $added = $this->findOrCreate(
-            ['name' => $tag->name],
-            function ($entity) use ($tag) { $entity = $tag; }
+        $tag = $this->findOrCreate(
+            ['name' => $newTag->name],
+            function ($entity) use ($newTag) { return $newTag; }
         );
 
-        if ($added) {
-            $event = new Event('Model.Tag.tagAdded', $this, ['tagName' => $added->name]);
+        if ($tag && !isset($tag->nbrOfSentences)) {
+            $event = new Event('Model.Tag.tagAdded', $this, ['tagName' => $tag->name]);
             $this->getEventManager()->dispatch($event);
-
-            if ($sentenceId != null) {
-                $added->link = $this->TagsSentences->tagSentence(
-                    $sentenceId,
-                    $added->id,
-                    $userId
-                );
-            }
-
-            return $added;
         }
 
-        return false;
+        if ($tag && $sentenceId != null) {
+            $tag->link = $this->TagsSentences->tagSentence(
+                $sentenceId,
+                $tag->id,
+                $userId
+            );
+        }
+
+        return $tag;
     }
 
     public function removeTagFromSentence($tagId, $sentenceId) {

src/Controller/TagsController.php
@@ -69,7 +69,7 @@ class TagsController extends AppController
             $username = CurrentUser::get("username");
             $tag = $this->Tags->addTag($tagName, $userId, $sentenceId);
 
-            $isSaved = $tag && $tag->link;
+            $isSaved = $tag && $tag->link && !$tag->link->alreadyExists;
             $this->set('isSaved', $isSaved);
             if ($isSaved) {
                 $this->set('tagName', $tag->name);

src/Template/Tags/add_tag_post.ctp
@@ -24,9 +24,7 @@
  * @license  Affero General Public License
  * @link     https://tatoeba.org
  */
-
 if ($isSaved) {
     $this->Tags->displayTag($tagName, $tagId, $sentenceId, $userId, $username, $date, $sentenceLang);
 }
 ?>
-

tests/TestCase/Controller/TagsControllerTest.php
@@ -85,6 +85,16 @@ class TagsControllerTest extends IntegrationTestCase {
         $this->assertResponseNotContains(' cut after 9');
     }
 
+    public function testAddTagPost_duplicateTagReturnsEmptyResponse ()
+    {
+        $this->logInAs('advanced_contributor');
+        $this->ajaxPost('/eng/tags/add_tag_post', [
+            'sentence_id' => 8,
+            'tag_name' => '@needs native check',
+        ]);
+        $this->assertResponseEmpty();
+    }
+
     public function testSearch_asGuest() {
         $this->enableCsrfToken();
         $this->enableSecurityToken();

commit 939ae46990b5eb921e939ce60f619563f6bc8d70

Dispatch event for both existing and new tags
3842e94591b47bd02c07a3c2f7d1c5e9b2a8d1b3 introduced a bug which
prevented dispatching the `Model.Tag.tagAdded` event whenever an already
existing tag was added to a sentence.

src/Model/Table/TagsTable.php
@@ -108,17 +108,17 @@ class TagsTable extends Table
             function ($entity) use ($newTag) { return $newTag; }
         );
 
-        if ($tag && !isset($tag->nbrOfSentences)) {
+        if ($tag) {
             $event = new Event('Model.Tag.tagAdded', $this, ['tagName' => $tag->name]);
             $this->getEventManager()->dispatch($event);
-        }
 
-        if ($tag && $sentenceId != null) {
-            $tag->link = $this->TagsSentences->tagSentence(
-                $sentenceId,
-                $tag->id,
-                $userId
-            );
+            if ($sentenceId != null) {
+                $tag->link = $this->TagsSentences->tagSentence(
+                    $sentenceId,
+                    $tag->id,
+                    $userId
+                );
+            }
         }
 
         return $tag;