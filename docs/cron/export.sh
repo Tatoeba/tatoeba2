0956474ac629a1c83003b51ccfe101c7a212f1d7
lbdx <lbeaudoux@gmail.com>
2020-06-06 21:29:00 +0200

    use awk instead of python split script

--- docs/cron/export.sh
+++ docs/cron/export.sh
@@ -55,14 +55,87 @@ split_file sentences.csv
 split_file sentences_CC0.csv
 split_file transcriptions.csv
 
-python3 "$ROOT/docs/cron/split_files.py" \
-    user_languages.csv \
-    links.csv \
-    tags.csv \
-    sentences_in_lists.csv \
-    sentences_with_audio.csv \
-    --indir "$DL_DIR" \
-    --outdir "$TEMP_DIR"
+# split links by language pair
+mysql --skip-column-names --batch tatoeba -e \
+    "SELECT 
+      COALESCE(sentence_lang, '\N'), 
+      COALESCE(translation_lang, '\N'),
+      sentence_id, 
+      translation_id
+     FROM sentences_translations" | \
+  awk -F"\t" -v dir=$TEMP_DIR 'BEGIN {OFS = "\t"} {
+      src_lg = ($1 == "\N" ? "unknown" : $1);
+      tgt_lg = ($2 == "\N" ? "unknown" : $2);
+      fpath = dir "/" src_lg "/" src_lg "-" tgt_lg "_links.tsv";
+      print $3, $4 >> fpath;
+      close(fpath)
+  }'
+
+# split user languages by language
+mysql --skip-column-names --batch tatoeba -e \
+    "SELECT
+       COALESCE(ul.language_code, '\N'), 
+       COALESCE(ul.level, '\N'), 
+       COALESCE(u.username, '\N'), 
+       ul.details
+     FROM users_languages ul 
+       LEFT JOIN users u ON ul.of_user_id = u.id
+     ORDER BY ul.language_code ASC, ul.level DESC, u.username ASC" | \
+  awk -F"\t" -v dir=$TEMP_DIR '{
+      lg = ($1 == "\N" ? "unknown" : $1);      
+      fpath = dir "/" lg "/" lg "_user_languages.tsv";
+      print >> fpath
+  }'
+
+# split tags by language
+mysql --skip-column-names --batch tatoeba -e \
+    "SELECT DISTINCT 
+       COALESCE(s.lang, '\N'), 
+       ts.sentence_id, 
+       t.name 
+     FROM `tags` t 
+       JOIN `tags_sentences` ts ON t.id = ts.tag_id
+       JOIN `sentences` s ON t.id = s.id" | \
+  awk -F"\t" -v dir=$TEMP_DIR 'BEGIN {OFS = "\t"} {
+      lg = ($1 == "\N" ? "unknown" : $1);
+      fpath = dir "/" lg "/" lg "_tags.tsv";
+      print $2, $3 >> fpath
+  }'
+
+# split sentences in lists by language
+mysql --skip-column-names --batch tatoeba -e \
+    "SELECT 
+       COALESCE(s.lang, '\N'), 
+       sl.id, 
+       s_sl.sentence_id
+     FROM sentences_sentences_lists s_sl
+       JOIN sentences_lists sl ON s_sl.sentences_list_id = sl.id
+       JOIN sentences s ON s_sl.sentence_id = s.id
+     WHERE sl.visibility != 'private'
+     ORDER BY sl.id ASC, s_sl.sentence_id" | \
+  awk -F"\t" -v dir=$TEMP_DIR 'BEGIN {OFS = "\t"} {
+      lg = ($1 == "\N" ? "unknown" : $1);
+      fpath = dir "/" lg "/" lg "_sentences_in_lists.tsv";
+      print $2, $3 >> fpath
+  }'      
+
+# split sentences with audio by language
+mysql --skip-column-names --batch tatoeba -e \
+    "SELECT
+       COALESCE(s.lang, '\N'), 
+       a.sentence_id, 
+       COALESCE(u.username, '\N'), 
+       COALESCE(u.audio_license, '\N'), 
+       COALESCE(u.audio_attribution_url, '\N')
+     FROM audios a 
+       LEFT JOIN users u on u.id = a.user_id
+       JOIN sentences s ON a.sentence_id = s.id
+     ORDER BY sentence_id ASC" | \
+  awk -F"\t" -v dir=$TEMP_DIR 'BEGIN {OFS = "\t"} {
+      lg = ($1 == "\N" ? "unknown" : $1);
+      fpath = dir "/" lg "/" lg "_sentences_with_audio.tsv";
+      print $2, $3, $4, $5 >> fpath
+  }'    
 
 find $TEMP_DIR -path '*tsv' -exec bzip2 -qf '{}' +
 rm -rf $DL_DIR/per_language
