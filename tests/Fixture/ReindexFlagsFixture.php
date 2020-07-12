48ff8eb3268dee565b4953b3521248804faaaaf5
Andreas Perstinger <andipersti@gmail.com>
2020-07-12 17:17:53 +0200

    Rewrite fixture for ReindexFlags
    We cannot import the schema from the app database because we do not have
    access to it when we run the testsuite.

--- tests/Fixture/ReindexFlagsFixture.php
+++ tests/Fixture/ReindexFlagsFixture.php
@@ -4,5 +4,21 @@ namespace App\Test\Fixture;
 use Cake\TestSuite\Fixture\TestFixture;
 
 class ReindexFlagsFixture extends TestFixture {
-    public $import = ['model' => 'ReindexFlags'];
+	public $name = 'ReindexFlag';
+
+	public $fields = array(
+		'id' => ['type' => 'integer', 'null' => false, 'default' => null],
+		'sentence_id' => ['type' => 'integer', 'null' => false, 'default' => null],
+		'lang' => ['type' => 'string', 'null' => true, 'default' => null, 'length' => 4],
+		'indexed' => ['type' => 'boolean', 'null' => false, 'default' => '0'],
+		'type' => ['type' => 'string', 'null' => false],
+		'_indexes' => [
+			'idx_sentence_id' => ['type' => 'index', 'columns' => ['sentence_id'], 'length' => []],
+		],
+		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]],
+		'_options' => ['charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB']
+	);
+
+	public $records = array(
+	);
 }
