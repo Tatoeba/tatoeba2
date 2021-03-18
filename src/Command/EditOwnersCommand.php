00ebea1f9aac4cb4d723d20188385b83ceca0656
時流 <jiru@users.noreply.github.com>
2021-03-18 16:18:53 +0100

    Add a command to edit sentence owners

---
+++ src/Command/EditOwnersCommand.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Command;
+
+use App\Command\EditCommand_;
+use App\Model\CurrentUser;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+use Cake\Datasource\Exception\RecordNotFoundException;
+
+class EditOwnersCommand extends EditCommand_
+{
+    protected function buildOptionParser(ConsoleOptionParser $parser) {
+        $parser = parent::buildOptionParser($parser);
+        $parser
+            ->setDescription('Change sentence owners.')
+            ->addArgument('owner', [
+                'help' => 'username of the new owner.',
+                'required' => true,
+            ]);
+        return $parser;
+    }
+
+    protected function editOwner($ids, $newOwner, $newOwnerId) {
+        foreach ($ids as $id) {
+            try {
+                $result = $this->Sentences->setOwner($id, $newOwnerId, CurrentUser::get('role'));
+            } catch (RecordNotFoundException $e) {
+                $result = false;
+            }
+
+            if ($result) {
+                $this->log[] = "id $id - Owner set to $newOwner";
+            } else {
+                $this->log[] = "id $id - Record not found or could not save changes";
+            }
+            $this->total++;
+        }
+    }
+
+    public function execute(Arguments $args, ConsoleIo $io) {
+        $this->becomeUser($args, $io);
+
+        $newOwner = $args->getArgument('owner');
+        $newOwnerId = $this->Users->getIdFromUsername($newOwner);
+        if (!$newOwnerId) {
+            $io->error("User '$newOwner' does not exist!");
+            $this->abort();
+        }
+
+        $ids = $this->readIdsFromFile($args, $io);
+
+        $this->editOwner($ids, $newOwner, $newOwnerId);
+
+        $this->printLog($io);
+    }
+}
---
+++ tests/TestCase/Command/EditOwnersCommandTest.php
@@ -0,0 +1,120 @@
+<?php
+namespace App\Test\TestCase\Command;
+
+use Cake\Console\Command;
+use Cake\Filesystem\File;
+use Cake\Filesystem\Folder;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+class EditOwnersCommandTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    public $fixtures = [
+        'app.links',
+        'app.reindex_flags',
+        'app.sentences',
+        'app.users',
+        'app.users_languages',
+    ];
+
+    const TESTDIR = TMP . 'edit_owners_tests' . DS;
+
+    public static function setUpBeforeClass() {
+        new Folder(self::TESTDIR, true, 0755);
+    }
+
+    public static function tearDownAfterClass() {
+        $folder = new Folder(self::TESTDIR);
+        $folder->delete();
+    }
+
+    public function setUp() {
+        parent::setUp();
+        $this->UseCommandRunner();
+        $this->Sentences = TableRegistry::getTableLocator()->get('Sentences');
+    }
+
+    private function create_test_file($ids) {
+        $path = self::TESTDIR . 'input_test';
+        $file = new File($path);
+        $file->write(implode("\n", $ids));
+        $file->close();
+        return $path;
+    }
+
+    public function testExecute_changesOwner() {
+        $ids = [14, 40, 41];
+        $path = $this->create_test_file([14, 40, 41]);
+        $this->exec("edit_owners admin $path contributor");
+
+        $this->assertExitCode(Command::CODE_SUCCESS);
+
+        foreach ($ids as $id) {
+            $sentence = $this->Sentences->get($id);
+            $this->assertEquals(4, $sentence->user_id);
+        }
+    }
+
+    public function successesProvider() {
+        // username, ids, owner, number of changes
+        return [
+            'all ids adopted by contributor' =>
+                ['contributor', [14, 15, 16], 'contributor', 3],
+            'all ids adopted by advanced_contributor' =>
+                ['advanced_contributor', [14, 15, 16, 40, 41], 'advanced_contributor', 5],
+            'some ids adopted by contributor' =>
+                ['contributor', [14, 15, 16, 40, 41, 8], 'contributor', 3],
+            'some ids adopted by contributor using admin power' =>
+                ['admin', [14, 15, 16, 40, 41, 8], 'contributor', 5],
+            'with wrong ids' =>
+                ['admin', [14, 999999, 999998], 'contributor', 1],
+            'empty file' => ['admin', [], 'contributor', 0],
+        ];
+    }
+
+    private function countOwned($ids, $owner) {
+        if (empty($ids)) {
+            return 0;
+        } else {
+            $ownerId = $this->Sentences->Users->findByUsername($owner)->first()->id;
+            return $this->Sentences
+                        ->find()
+                        ->where(['id IN' => $ids, 'user_id' => $ownerId])
+                        ->count();
+        }
+    }
+
+    /**
+     * @dataProvider successesProvider
+     **/
+    public function testExecute_severalScenarios($user, $ids, $newOwner, $changes) {
+        $path = $this->create_test_file($ids);
+        $before = $this->countOwned($ids, $newOwner);
+        $this->exec("edit_owners $user $path $newOwner");
+        $after = $this->countOwned($ids, $newOwner);
+        $this->assertExitCode(Command::CODE_SUCCESS);
+        $this->assertEquals($changes, $after - $before);
+    }
+
+    public function failuresProvider() {
+        return [
+            'without any required argument' => ['edit_owners'],
+            'without file' => ['edit_owners admin'],
+            'without owner' => ['edit_owners admin stdin'],
+            'with unknown file' => ['edit_owners admin unknown_file contributor'],
+            'with unknown owner' => ['edit_owners admin stdin unknown_owner'],
+            'as unknown user' => ['edit_owners unknown_user stdin contributor'],
+        ];
+    }
+
+    /**
+     * @dataProvider failuresProvider
+     */
+    public function testExecute_failures($command) {
+        $this->exec($command);
+        $this->assertExitCode(Command::CODE_ERROR);
+    }
+}
