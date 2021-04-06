6abd46faa21d0e2ec0c5360a1b1579738cffe70c
lbdx <lbeaudoux@gmail.com>
2021-04-06 17:42:26 +0200

    Move index change script to updates dir

--- docs/database/updates/2021-04-06.sql
+++ docs/database/updates/2021-04-06.sql
@@ -3,5 +3,4 @@ CREATE INDEX sentence_lang_translation_lang_idx
 ON sentences_translations(sentence_lang, translation_lang);
 
 -- Delete the single-column index on sentence_lang that has become useless
-DROP INDEX sentence_lang 
-ON sentences_translations;
+DROP INDEX sentence_lang ON sentences_translations;
