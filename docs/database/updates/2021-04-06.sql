ccc5ef088fccae73dbb2cbdbe12ae5f767670eb2
lbdx <lbeaudoux@gmail.com>
2021-04-15 11:22:38 +0200

    Revert commits

--- docs/database/updates/2021-04-06.sql
+++
@@ -1,6 +0,0 @@
--- Add composite index on languages to speed up queries on language pairs
-CREATE INDEX sentence_lang_translation_lang_idx
-ON sentences_translations(sentence_lang, translation_lang);
-
--- Delete the single-column index on sentence_lang that has become useless
-DROP INDEX sentence_lang ON sentences_translations;
