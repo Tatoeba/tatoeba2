Tatoeba/tatoeba2

commit 6b812985924f3fcec7d0626275a8fa372fd7fce6

Fix page listing transcriptions of user

src/Controller/TranscriptionsController.php
@@ -146,27 +146,19 @@ class TranscriptionsController extends AppController
     }
 
     public function of($username) {
-        $userId = $this->User->getIdFromUsername($username);
+        $this->loadModel('Users');
+        $userId = $this->Users->getIdFromUsername($username);
         if ($userId) {
-            $result = $this->paginate('Transcription', array(
-                'Transcription.user_id' => $userId
-            ));
-            $sentencesWithTranscription = array();
-            foreach ($result as $data) {
-                $sentenceId = $data['Sentence']['id'];
-                if (!isset($sentencesWithTranscription[$sentenceId])) {
-                    $sentencesWithTranscription[$sentenceId] = array(
-                        'Sentence' => $data['Sentence'],
-                        'Transcription' => array()
-                    );
-                }
-                $data['Transcription']['User'] = $data['User'];
-                $sentencesWithTranscription[$sentenceId]['Transcription'][] =
-                    $data['Transcription'];
-            }
-            $this->set(compact('sentencesWithTranscription'));
+            $this->paginate = [
+                'conditions' => [
+                    'Transcriptions.user_id' => $userId
+                ]
+            ];
+            $results = $this->paginate('Transcriptions');
+            $this->set('results', $results);
         }
-        $this->set(compact('username'));
+        $this->set('userId', $userId);
+        $this->set('username', $username);
     }
 
     private function setViewVars($transcriptions, $sentenceId, $sentence = null) {

src/Template/Transcriptions/of.ctp
@@ -40,8 +40,13 @@ if (isset($sentencesWithTranscription)) {
 <div id="main_content">
 <div class="module">
 <?php
-if (isset($sentencesWithTranscription)) {
-    if (count($sentencesWithTranscription) == 0) {
+if (empty($userId)) {
+    echo $this->Html->tag('h2', format(
+        __("There's no user called {username}"),
+        array('username' => $username)
+    ));
+} else if (isset($results)) {
+    if (count($results) == 0) {
         echo $this->Html->tag('h2', format(
             __('{username} does not have any transcriptions'),
             array('username' => $username)
@@ -49,7 +54,7 @@ if (isset($sentencesWithTranscription)) {
     } else {
         $title = $this->Paginator->counter(
             array(
-                'format' => $title . ' ' . __("(total %count%)")
+                'format' => $title . ' ' . __("(total {{count}})")
             )
         );
         echo $this->Html->tag('h2', $title);
@@ -59,7 +64,8 @@ if (isset($sentencesWithTranscription)) {
         $type = 'mainSentence';
         $parentId = null;
         $withAudio = false;
-        foreach ($sentencesWithTranscription as $sentence) {
+        foreach ($results as $result) {
+            $sentence = $result->sentence;
             $this->Sentences->displayGenericSentence(
                 $sentence,
                 $type,
@@ -70,11 +76,6 @@ if (isset($sentencesWithTranscription)) {
 
         $this->Pagination->display();
     }
-} else {
-    echo $this->Html->tag('h2', format(
-        __("There's no user called {username}"),
-        array('username' => $username)
-    ));
 }
 ?>
 </div>

commit 6c62f556e85e31dce16e7beb096f45449afccbf1

Fix regressions in /transcriptions/of/xxx
Fir regressions introduced in
6b812985924f3fcec7d0626275a8fa372fd7fce6.

On page /transcriptions/of/xxx, pagination was
not working and transcriptions were not displayed
any more; only their sentences were.

src/Controller/TranscriptionsController.php
@@ -35,9 +35,9 @@ class TranscriptionsController extends AppController
     );
 
     public $paginate = array(
-        'contain' => array('User', 'Sentence'),
+        'contain' => array('Users', 'Sentences'),
         'limit' => 100,
-        'order' => 'Transcription.modified DESC'
+        'order' => array('Transcriptions.modified' => 'desc'),
     );
 
     public function beforeFilter(Event $event)
@@ -160,12 +160,20 @@ class TranscriptionsController extends AppController
         $this->loadModel('Users');
         $userId = $this->Users->getIdFromUsername($username);
         if ($userId) {
-            $this->paginate = [
-                'conditions' => [
-                    'Transcriptions.user_id' => $userId
-                ]
-            ];
-            $results = $this->paginate('Transcriptions');
+            $query = $this->Transcriptions
+                ->find()
+                ->where(['Transcriptions.user_id' => $userId])
+                ->mapReduce(
+                    function ($transcr, $key, $mapReduce) {
+                        $mapReduce->emitIntermediate($transcr, $transcr->sentence_id);
+                    },
+                    function ($transcrs, $sentenceId, $mapReduce) {
+                        $sentence = $transcrs[0]->sentence;
+                        $sentence->transcriptions = $transcrs;
+                        $mapReduce->emit($sentence, $sentenceId);
+                    }
+                );
+            $results = $this->paginate($query);
             $this->set('results', $results);
         }
         $this->set('userId', $userId);

src/Template/Transcriptions/of.ctp
@@ -65,8 +65,7 @@ if (empty($userId)) {
         $type = 'mainSentence';
         $parentId = null;
         $withAudio = false;
-        foreach ($results as $result) {
-            $sentence = $result->sentence;
+        foreach ($results as $sentence) {
             $this->Sentences->displayGenericSentence(
                 $sentence,
                 $type,