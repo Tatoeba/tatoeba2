09c1fcb6d75d69da8e67f50d30864f398c40136e
時流 <jiru@users.noreply.github.com>
2022-05-10 02:48:52 +0200

    Update unit test
    Update according to parent commit.

--- tests/TestCase/Model/Entity/AudioTest.php
+++ tests/TestCase/Model/Entity/AudioTest.php
@@ -51,25 +51,26 @@ class AudioTest extends TestCase
         $this->assertNull($this->Audio->external);
     }
 
-    public function filePathProvider() {
-        // audio id, sentence id, expected audio file path
+    public function fileProvider() {
+        // audio id, sentence id, expected audio file path, expected pretty filename
         return [
-            [       1,       2, '/foo/bar/000/001/2-1.mp3'       ],
-            [    1234,    5678, '/foo/bar/001/234/5678-1234.mp3' ],
-            [  999999,       3, '/foo/bar/999/999/3-999999.mp3'  ],
-            [ 1000000,       4, '/foo/bar/000/000/4-1000000.mp3' ],
-            [ 1000001,       4, '/foo/bar/000/001/4-1000001.mp3' ],
+            [       1,       2, '/foo/bar/000/001/1.mp3',       '2-1.mp3'       ],
+            [    1234,    5678, '/foo/bar/001/234/1234.mp3',    '5678-1234.mp3' ],
+            [  999999,       3, '/foo/bar/999/999/999999.mp3',  '3-999999.mp3'  ],
+            [ 1000000,       4, '/foo/bar/000/000/1000000.mp3', '4-1000000.mp3' ],
+            [ 1000001,       4, '/foo/bar/000/001/1000001.mp3', '4-1000001.mp3' ],
         ];
     }
 
     /**
-     * @dataProvider filePathProvider
+     * @dataProvider fileProvider
      */
-    public function testGet_file_path($audioId, $sentenceId, $expectedPath)
+    public function testGet_file($audioId, $sentenceId, $expectedPath, $expectedPrettyFilename)
     {
         $this->Audio->id = $audioId;
         $this->Audio->sentence_id = $sentenceId;
         $this->assertEquals($expectedPath, $this->Audio->file_path);
+        $this->assertEquals($expectedPrettyFilename, $this->Audio->pretty_filename);
     }
 
     public function testGet_attributionUrl_returnsNullWithoutProperDataSet() {
