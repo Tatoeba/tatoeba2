6588c2a45e6c65fb9c50529fc22b8824b57f75a5
Miles Rodriguez <70669145+DJ-Saidez@users.noreply.github.com>
2022-08-28 22:28:34 -0700

    Update LanguagesLib.php

--- src/Lib/LanguagesLib.php
+++ src/Lib/LanguagesLib.php
@@ -690,6 +690,7 @@ class LanguagesLib
                 'swc' => __d('languages', 'Congo Swahili'),
                 'rhg' => __d('languages', 'Rohingya'),
                 'mfa' => __d('languages', 'Kelantan-Pattani Malay'),
+                'nap' => __d('languages', 'Neapolitan'),
             );
         }
         return $languages;
