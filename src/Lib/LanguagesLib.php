ab82c3aa276fb6a8747b8138b619a6ec33449f39
Miles Rodriguez <70669145+DJ-Saidez@users.noreply.github.com>
2022-09-10 13:19:35 -0700

    Added language Saraiki (#2981)

--- src/Lib/LanguagesLib.php
+++ src/Lib/LanguagesLib.php
@@ -690,6 +690,7 @@ class LanguagesLib
                 'swc' => __d('languages', 'Congo Swahili'),
                 'rhg' => __d('languages', 'Rohingya'),
                 'mfa' => __d('languages', 'Kelantan-Pattani Malay'),
+                'skr' => __d('languages', 'Saraiki'),
             );
         }
         return $languages;
@@ -762,7 +763,8 @@ class LanguagesLib
             "ajp",
             "ayl",
             "sdh",
-            "ckb"
+            "ckb",
+            "skr",
         );
 
         $autoLangs = array(
