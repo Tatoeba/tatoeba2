70f0c2d1c8739f5d71102d9c91f889fce2d0c9cc
Miles Rodriguez <70669145+DJ-Saidez@users.noreply.github.com>
2023-02-02 12:20:29 -0800

    Update LanguagesLib.php

--- src/Lib/LanguagesLib.php
+++ src/Lib/LanguagesLib.php
@@ -692,6 +692,7 @@ class LanguagesLib
                 'mfa' => __d('languages', 'Kelantan-Pattani Malay'),
                 'skr' => __d('languages', 'Saraiki'),
                 'nap' => __d('languages', 'Neapolitan'),
+                'syl' => __d('languages', 'Sylheti'),
 
             );
         }
