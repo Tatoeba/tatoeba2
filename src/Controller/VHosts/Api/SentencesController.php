81b6cdb558116f14f400348768c17891000236d5
時流 <jiru@users.noreply.github.com>
2023-06-01 10:48:53 +0700

    Use API-specific queries
    Rely less on code that runs the website to perform queries
    so that the API won’t be impacted by future changes.
    
    We can also use slightly less expensive queries since the API
    is read only.

--- src/Controller/VHosts/Api/SentencesController.php
+++ src/Controller/VHosts/Api/SentencesController.php
@@ -2,6 +2,7 @@
 namespace App\Controller\VHosts\Api;
 
 use App\Controller\VHosts\Api\ApiController;
+use Cake\ORM\Query;
 
 class SentencesController extends ApiController
 {
@@ -17,18 +18,66 @@ class SentencesController extends ApiController
         ];
         $exposedFields = $sentence;
         $exposedFields['translations'] = $sentence;
-        $exposedFields['translations']['indirect_translations'] = $sentence;
         return compact('exposedFields');
+    }
+
+    private function fields() {
+        return [
+            'id',
+            'text',
+            'lang',
+            'user_id',
+            'correctness',
+            'script',
+            'license',
+        ];
+    }
+
+    private function contain() {
+        $audioContainment = function (Query $q) {
+            $q->select(['id', 'external', 'sentence_id'])
+              ->contain(['Users' => ['fields' => ['username', 'audio_license']]]);
+            return $q;
+        };
+        $transcriptionsContainment = [
+            'fields' => ['sentence_id', 'script', 'text', 'needsReview'],
+        ];
+        $indirTranslationsContainment = function (Query $q) use ($audioContainment, $transcriptionsContainment) {
+            $q->select($this->fields())
+              ->contain([
+                  'Users' => ['fields' => ['id', 'username']],
+                  'Audios' => $audioContainment,
+                  'Transcriptions' => $transcriptionsContainment,
+              ]);
+            return $q;
+        };
+        $translationsContainment = function (Query $q) use ($audioContainment, $transcriptionsContainment, $indirTranslationsContainment) {
+            $q->select($this->fields())
+              ->contain([
+                  'Users' => ['fields' => ['id', 'username']],
+                  'Audios' => $audioContainment,
+                  'Transcriptions' => $transcriptionsContainment,
+                  'IndirectTranslations' => $indirTranslationsContainment,
+              ]);
+            return $q;
+        };
+
+        return [
+            'Translations' => $translationsContainment,
+            'Users' => ['fields' => ['id', 'username']],
+            'Audios' => $audioContainment,
+            'Transcriptions' => $transcriptionsContainment,
+        ];
     }
 
     public function get($id) {
         $this->loadModel('Sentences');
         $query = $this->Sentences
-            ->find('exposedFields', $this->exposedFields())
             ->find('filteredTranslations')
-            ->select($this->Sentences->fields())
+            ->find('exposedFields', $this->exposedFields())
+            ->select($this->fields())
             ->where(['Sentences.id' => $id])
-            ->contain($this->Sentences->contain(['translations' => true]));
+            ->contain($this->contain());
 
         $results = $query->firstOrFail();
         $response = [
