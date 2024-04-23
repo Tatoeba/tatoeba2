a907c02c9b41b909cb24ac5db95ec0c2cec5f751
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2024-04-23 13:17:50 -0400

    Add form size check and comments
    - This also remaps the image size check to the "change" event so the user
    doesn't need to submit the "too large" image to check if it's size is valid

--- webroot/js/user/edit_profile.js
+++ webroot/js/user/edit_profile.js
@@ -1,15 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
+    /*
+    Note: The default nginx request body size (client_max_body_size)
+    is only 1MB. Form submissions for avatar images often exceed this
+    limit and must be checked to prevent HTTP 413 responses.
+    */
+    const MAX_IMAGE_BYTES = 1024 * 1024; // 1MB
+    const MAX_FORM_BYTES = MAX_IMAGE_BYTES + 4096; // 1MB + 4KB
     let avatar_upload_form = document.getElementById("upload-avatar-form");
-    avatar_upload_form.addEventListener("submit", e => {
+    let form_too_large = false;
+    avatar_upload_form.addEventListener("change", async (event) => {
         let files = document.getElementById("upload-avatar-files").files;
-        if (files && files.length == 1) {
-            let f = files[0];
-            if (f.size > 1024 * 1024) {
+        if (files && files.length >= 1) {
+            // Reject too large images
+            if (files[0].size > MAX_IMAGE_BYTES) {
+                event.preventDefault();
                 alert(avatar_upload_form.getAttribute("msg-too-large-image"));
-
-                // Stop form submission
-                e.preventDefault();
+                avatar_upload_form.reset();
             }
+
+            // Update form size
+            let form_data = new FormData(avatar_upload_form);
+            const response = new Response(form_data);
+            await response.blob()
+            .then(blob => {
+                console.log(blob.size)
+                if (blob.size > MAX_FORM_BYTES) {
+                    form_too_large = true;
+                }
+            })
+            .catch((error) => {
+                alert(avatar_upload_form.getAttribute("msg-too-large-image"));
+                avatar_upload_form.reset();
+            });
         }
     })
+
+    avatar_upload_form.addEventListener("submit", async (event) => {
+        // Reject too large form submissions
+        if (form_too_large) {
+            alert(avatar_upload_form.getAttribute("msg-too-large-image"));
+            event.preventDefault();
+        }
+    });
 })
