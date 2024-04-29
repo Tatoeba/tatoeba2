d3e17344c44f231a5f7a97bb0510eb29253bc3de
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2024-04-29 13:49:15 -0400

    Simplify form size check

--- webroot/js/user/edit_profile.js
+++ webroot/js/user/edit_profile.js
@@ -4,28 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
     is only 1MB. Form submissions for avatar images often exceed this
     limit and must be checked to prevent HTTP 413 responses.
     */
-    const MAX_IMAGE_BYTES = 1024 * 1024; // 1MB
-    const MAX_FORM_BYTES = MAX_IMAGE_BYTES + 4096; // 1MB + 4KB
+    const MAX_FORM_BYTES = 1024 * 1024; // 1MB
     let avatar_upload_form = document.getElementById("upload-avatar-form");
-    let form_too_large = false;
     avatar_upload_form.addEventListener("change", async (event) => {
         let files = document.getElementById("upload-avatar-files").files;
         if (files && files.length >= 1) {
-            // Reject too large images
-            if (files[0].size > MAX_IMAGE_BYTES) {
-                event.preventDefault();
-                alert(avatar_upload_form.getAttribute("msg-too-large-image"));
-                avatar_upload_form.reset();
-            }
-
-            // Update form size
+            // Reject too large images based on total form size
             let form_data = new FormData(avatar_upload_form);
             const response = new Response(form_data);
             await response.blob()
             .then(blob => {
-                console.log(blob.size)
-                if (blob.size > MAX_FORM_BYTES) {
-                    form_too_large = true;
+                if (blob.size >= MAX_FORM_BYTES) {
+                    alert(avatar_upload_form.getAttribute("msg-too-large-image"));
+                    avatar_upload_form.reset();
                 }
             })
             .catch(() => {
@@ -34,12 +25,4 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
     })
-
-    avatar_upload_form.addEventListener("submit", async (event) => {
-        // Reject too large form submissions
-        if (form_too_large) {
-            alert(avatar_upload_form.getAttribute("msg-too-large-image"));
-            event.preventDefault();
-        }
-    });
 })
