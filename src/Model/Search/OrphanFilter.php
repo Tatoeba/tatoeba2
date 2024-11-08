16f6814ae77cf3202f768154defa626ae5719906
時流 <jiru@users.noreply.github.com>
2024-11-08 13:21:58 +0800

    Simplify orphan filter
    Take advantage of parent class BoolFilter logic.

--- src/Model/Search/OrphanFilter.php
+++ src/Model/Search/OrphanFilter.php
@@ -4,10 +4,7 @@ namespace App\Model\Search;
 
 class OrphanFilter extends BoolFilter {
 
-    public function __construct(bool $value) {
-        parent::__construct($value);
-        $this->anyOf([0]);
-    }
+    protected $valueForTrue = 0;
 
     protected function getAttributeName() {
         return 'user_id';
