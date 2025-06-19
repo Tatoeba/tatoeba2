Tatoeba/tatoeba2

commit f600e17de89b17e2141fa171fb6f18d047b7f4e7

Prevent hidden Wall posts from bumping threads
Closes #3137.

src/Model/Table/WallTable.php
@@ -104,6 +104,26 @@ class WallTable extends Table
             $this->WallThreads->save($newThreadData);
         }
 
+        if ($entity->hidden) {
+            $root = $this->getRootMessageOfReply($entity->id);
+            $this->recalculateThreadDateIgnoringHiddenPosts($root);
+        }
+    }
+
+    private function recalculateThreadDateIgnoringHiddenPosts($root)
+    {
+        $result = $this->find()
+            ->select(['latest_date' => 'MAX(date)'])
+            ->where([
+                'lft >=' => $root->lft,
+                'rght <=' => $root->rght,
+                'hidden IS' => false,
+            ])
+            ->first();
+
+        $thread = $this->WallThreads->get($root->id);
+        $thread->last_message_date = $result->latest_date;
+        $this->WallThreads->save($thread);
     }
 
 

tests/TestCase/Model/Table/WallTableTest.php
@@ -286,4 +286,13 @@ class WallTest extends TestCase {
 
         I18n::setLocale($prevLocale);
     }
+
+    public function testHidingAMessageRecalculatesThreadDateIgnoringHiddenPosts() {
+        $reply = $this->Wall->get(2);
+        $reply->hidden = true;
+        $this->Wall->save($reply);
+
+        $threadDate = $this->Wall->WallThreads->get(1)->last_message_date;
+        $this->assertEquals('2014-04-15 16:37:11', $threadDate);
+    }
 }

commit ca4390d81e2cd36c6da2ce201d37531670253e7f

Fix regression in threads unbumping
Regression introduced by f600e17de89b17e2141fa171fb6f18d047b7f4e7.

Refs #3137.

src/Model/Table/WallTable.php
@@ -121,9 +121,11 @@ class WallTable extends Table
             ])
             ->first();
 
-        $thread = $this->WallThreads->get($root->id);
-        $thread->last_message_date = $result->latest_date;
-        $this->WallThreads->save($thread);
+        if ($result->latest_date) {
+            $thread = $this->WallThreads->get($root->id);
+            $thread->last_message_date = $result->latest_date;
+            $this->WallThreads->save($thread);
+        }
     }
 
 

tests/TestCase/Model/Table/WallTableTest.php
@@ -297,4 +297,16 @@ class WallTest extends TestCase {
         $threadDate = $this->Wall->WallThreads->get(1)->last_message_date;
         $this->assertEquals('2014-04-15 16:37:11', $threadDate);
     }
+
+    public function testHidingAStandaloneMessageDoesNotRecalculatesThreadDate() {
+        $messageId = 3;
+        $oldThreadDate = $this->Wall->WallThreads->get($messageId)->last_message_date;
+
+        $message = $this->Wall->get($messageId);
+        $message->hidden = true;
+        $this->Wall->save($message);
+
+        $newThreadDate = $this->Wall->WallThreads->get($messageId)->last_message_date;
+        $this->assertEquals($oldThreadDate, $newThreadDate);
+    }
 }