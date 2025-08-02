82c19e112da1363dc9b9189c4212303f90c9f75b
時流 <jiru@users.noreply.github.com>
2025-08-02 12:12:14 +0000

    Vectorize language icons based on flag of Mexico

--- tools/generate_flags.sh
+++ tools/generate_flags.sh
@@ -396,6 +396,8 @@ gen_flag ENM webroot/img/flags/Flag_of_England.svg squared
 gen_flag LIJ webroot/img/flags/Flag_of_England.svg squared
 gen_flag NGT webroot/img/flags/lao.svg centered
 gen_flag HNJ webroot/img/flags/lao.svg centered
+gen_flag NGU webroot/img/flags/nah.svg centered
+gen_flag NLV webroot/img/flags/nah.svg centered
 
 # Flags consisting of ISO code only
 gen_flag GUW
