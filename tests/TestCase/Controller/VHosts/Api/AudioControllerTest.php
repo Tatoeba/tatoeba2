c10990755ae94d63afc440b1c13cd4c17e869e49
時流 <jiru@users.noreply.github.com>
2025-08-29 07:28:39 +0000

    Add /unstable/audio API endpoint
    Simple implementation that allows to browse all audios
    by author and language.
    
    There is room for code factorization.
    
    I added failOnRisky="true" to phpunit.xml because of this test
    AudioControllerTest::testSearch_ordersResultsByIdAsc().
    
    It needs at least two results to assert that they are ordered by id.
    So this test can potentially not perform any assertion, for example
    if only one result is returned. In this case, it should be considered
    as a failure.

--- tests/TestCase/Controller/VHosts/Api/AudioControllerTest.php
+++ tests/TestCase/Controller/VHosts/Api/AudioControllerTest.php
@@ -4,11 +4,13 @@ namespace App\Test\TestCase\Controller\VHosts\Api;
 use App\Test\TestCase\Controller\AudioIntegrationTestTrait;
 use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
+use Helmich\JsonAssert\JsonAssertions;
 
 class AudioControllerTest extends TestCase
 {
-    use IntegrationTestTrait;
     use AudioIntegrationTestTrait;
+    use IntegrationTestTrait;
+    use JsonAssertions;
 
     public $fixtures = [
         'app.Audios',
@@ -82,5 +84,135 @@ class AudioControllerTest extends TestCase
         $this->assertResponseOk();
 
         $this->deleteAudioStorageDir();
+    }
+
+    public function testSearch_matchesSchema()
+    {
+        $this->get("http://api.example.com/unstable/audio");
+        $this->assertResponseOk();
+        $this->assertContentType('application/json');
+
+        $actual = $this->_getBodyAsString();
+        $schema = [
+            'type'       => 'object',
+            'required'   => ['data', 'paging'],
+            'properties' => [
+                'data'       => [
+                    'type'       => 'array',
+                    'items'      => SentencesControllerTest::AUDIO_JSON_SCHEMA,
+                ],
+                'paging' => SentencesControllerTest::PAGING_JSON_SCHEMA,
+            ]
+        ];
+        $schema['properties']['data']['items']['required'][] = 'sentence';
+        $schema['properties']['data']['items']['properties']['sentence'] = SentencesControllerTest::SENTENCE_JSON_SCHEMA;
+        $this->assertJsonDocumentMatchesSchema($actual, $schema);
+    }
+
+    public function testSearch_ordersResultsByIdAsc()
+    {
+        $this->get("http://api.example.com/unstable/audio");
+
+        $actual = $this->_getBodyAsString();
+
+        foreach (json_decode($actual)->data as $current) {
+            if (isset($prev)) {
+                $this->assertGreaterThan($prev->id, $current->id);
+            }
+            $prev = $current;
+        }
+    }
+
+    public function testSearch_lang_ok()
+    {
+        $this->get("http://api.example.com/unstable/audio?lang=spa");
+        $this->assertResponseOk();
+
+        $actual = $this->_getBodyAsString();
+        $expected = [
+            '$.data' => new \PHPUnit\Framework\Constraint\Count(1),
+            '$.data[0].sentence.lang' => 'spa',
+            '$.paging.total' => 1,
+        ];
+        $this->assertJsonDocumentMatches($actual, $expected);
+    }
+
+    public function testSearch_lang_invalid()
+    {
+        $this->get("http://api.example.com/unstable/audio?lang=invalid");
+        $this->assertResponseCode(400);
+    }
+
+    public function testSearch_author_ok()
+    {
+        $this->get("http://api.example.com/unstable/audio?author=contributor");
+        $this->assertResponseOk();
+
+        $actual = $this->_getBodyAsString();
+        $expected = [
+            '$.data' => new \PHPUnit\Framework\Constraint\Count(1),
+            '$.data[0].author' => 'contributor',
+            '$.paging.total' => 1,
+        ];
+        $this->assertJsonDocumentMatches($actual, $expected);
+    }
+
+    public function testSearch_author_invalid()
+    {
+        $this->get("http://api.example.com/unstable/audio?author=invalid");
+        $this->assertResponseCode(400);
+    }
+
+    public function testSearch_limit_ok()
+    {
+        $this->get("http://api.example.com/unstable/audio?limit=1");
+        $this->assertResponseOk();
+
+        $actual = $this->_getBodyAsString();
+        $expected = [
+            '$.data' => new \PHPUnit\Framework\Constraint\Count(1),
+            '$.paging.has_next' => true,
+            '$.paging.cursor_end' => 1,
+            '$.paging.next' => 'http://api.example.com/unstable/audio?limit=1&after=1',
+        ];
+        $this->assertJsonDocumentMatches($actual, $expected);
+    }
+
+    public function testSearch_limit_invalid()
+    {
+        $this->get("http://api.example.com/unstable/audio?limit=invalid");
+        $this->assertResponseCode(400);
+    }
+
+    public function testSearch_after_ok()
+    {
+        $this->get("http://api.example.com/unstable/audio?after=1");
+        $this->assertResponseOk();
+
+        $actual = $this->_getBodyAsString();
+        $expected = [
+            '$.data[0].id' => new \PHPUnit\Framework\Constraint\GreaterThan(1),
+            '$.paging.first' => 'http://api.example.com/unstable/audio',
+        ];
+        $this->assertJsonDocumentMatches($actual, $expected);
+    }
+
+    public function testSearch_after_invalid()
+    {
+        $this->get("http://api.example.com/unstable/audio?after=invalid");
+        $this->assertResponseCode(400);
+    }
+
+    public function testSearch_doesNotReturnAudioWithoutLicense()
+    {
+        $this->get("http://api.example.com/unstable/audio?lang=fra");
+        $this->assertResponseOk();
+
+        $actual = $this->_getBodyAsString();
+        $expected = [
+            '$.data' => new \PHPUnit\Framework\Constraint\Count(1),
+            '$.paging.total' => 1,
+        ];
+        $this->assertJsonDocumentMatches($actual, $expected);
     }
 }
