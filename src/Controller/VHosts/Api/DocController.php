Tatoeba/tatoeba2

commit f3a6c1e23403e3ca7c99d61e87d816eaa189600c

Return 404 error if no openapi spec file
If there is no openapi spec file, return a 404 error
instead of letting openapi-explorer fail with the error
"Unable to load the Spec: ..."

src/Controller/VHosts/Api/DocController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\VHosts\Api;
 
 use Cake\Controller\Controller;
+use Cake\Filesystem\File;
+use Cake\Http\Exception\NotFoundException;
 
 class DocController extends Controller
 {
@@ -10,8 +12,22 @@ class DocController extends Controller
     {
     }
 
+    private function getOpenapiSpec($version)
+    {
+        $version = $this->getRequest()->getParam('version');
+        $specFilename = "openapi-$version.json";
+        $specFile = new File(WWW_ROOT . 'api' . DS . $specFilename);
+        if ($specFile->exists()) {
+            return "/$specFilename";
+        } else {
+            throw new NotFoundException();
+        }
+    }
+
     public function show()
     {
-        $this->set('version', $this->getRequest()->getParam('version'));
+        $version = $this->getRequest()->getParam('version');
+        $specurl = $this->getOpenapiSpec($version);
+        $this->set('specurl', $specurl);
     }
 }

src/Template/VHosts/Api/Doc/show.ctp
@@ -3,7 +3,6 @@ use Cake\Routing\Router;
 $this->Html->script('/js/openapi-explorer.min.js', ['block' => 'script', 'type' => 'module']);
 $this->Html->css('/css/openapi-explorer.css', ['block' => 'css']);
 $this->assign('title', 'Tatoeba API');
-$specurl = "/openapi-$version.json";
 $this->assign('navlinks', '<li>' . $this->Html->Link('OpenAPI file', $specurl) . '</li>');
 ?>
 <openapi-explorer

tests/TestCase/Controller/VHosts/Api/ApiControllerTest.php
@@ -20,6 +20,12 @@ class ApiControllerTest extends TestCase
         $this->assertResponseOk();
     }
 
+    public function test_docNonInvalidVersion()
+    {
+        $this->get("http://api.example.com/non-existent");
+        $this->assertResponseCode(404);
+    }
+
     public function test_invalidController()
     {
         $this->get("http://api.example.com/no/such/controller");

commit cdbfccaf210a7d52a4ac2471dac8edebdb5951d3

Fix API doc title
Broken by f3a6c1e23403e3ca7c99d61e87d816eaa189600c.
The view layout needs $version variable to display its title properly.

src/Controller/VHosts/Api/DocController.php
@@ -28,6 +28,6 @@ class DocController extends Controller
     {
         $version = $this->getRequest()->getParam('version');
         $specurl = $this->getOpenapiSpec($version);
-        $this->set('specurl', $specurl);
+        $this->set(compact('specurl', 'version'));
     }
 }