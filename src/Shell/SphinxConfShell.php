Tatoeba/tatoeba2

commit 4beb12e6b8f9a32c0623d0a0d4fb8bcaa37bbf83

Index sentence owner self-proclaimed nativeness
This will ease searching for sentences and translations
using "is native" filter on both sentences and translations
(currently we cannot do it on translations).

This will also remove the limitation of having to have a
single source language filter selected in order to use the
"is native" sentence filter.

This will also remove the need to work around the maximum
number of values in filters (see issue #2166, closed by
c26b5dbbd9ca37128c769f222db48038fe92c754).

The only drawback is that we need additional work to keep
the Manticore index updated whenever a user’s nativeness
information changes.

src/Shell/SphinxConfShell.php
@@ -626,7 +626,7 @@ EOT;
         sql_query = \
             select \
                 r.id, r.text, r.created, r.modified, r.user_id, r.ucorrectness, r.has_audio, \
-                r.origin_known, r.is_original, \
+                r.origin_known, r.is_original, r.owner_is_native, \
                 GROUP_CONCAT(distinct tags.tag_id) as tags_id, \
                 GROUP_CONCAT(distinct lists.sentences_list_id) as lists_id, \
                 CONCAT('[', COALESCE(GROUP_CONCAT(distinct r.trans),''), ']') as trans \
@@ -641,13 +641,15 @@ EOT;
                     (COUNT(audios_sent_start.id) > 0) as has_audio, \
                     (sent_start.based_on_id IS NOT NULL) as origin_known, \
                     (sent_start.based_on_id = 0) as is_original, \
+                    COALESCE(ul_sent_start.level = 5, 0) as owner_is_native, \
                     \
                     CONCAT('{', \
                         'l:\"',sent_end.lang,'\",', \
                         'd:',MIN( IF(trans.sentence_id = transtrans.translation_id,1,2) ),',', \
                         'u:',COALESCE(sent_end.user_id, 0),',', \
                         'c:',sent_end.correctness + 1,',', \
-                        'a:',COUNT(audios_sent_end.id) > 0, \
+                        'a:',COUNT(audios_sent_end.id) > 0,',', \
+                        'n:',ul_sent_end.level = 5, \
                     '}') as trans \
                 from \
                     sentences sent_start \
@@ -667,6 +669,12 @@ EOT;
                     audios audios_sent_end ON sent_end.id = audios_sent_end.sentence_id \
                 left join \
                     audios audios_sent_start ON sent_start.id = audios_sent_start.sentence_id \
+                left join \
+                    users_languages ul_sent_start ON sent_start.user_id = ul_sent_start.of_user_id \
+                                                 AND sent_start.lang = ul_sent_start.language_code \
+                left join \
+                    users_languages ul_sent_end ON sent_end.user_id = ul_sent_end.of_user_id \
+                                               AND sent_end.lang = ul_sent_end.language_code \
                 where \
                     sent_start.lang = '$lang' \
                     and sent_start.id >= \$start and sent_start.id <= \$end \
@@ -694,6 +702,7 @@ EOT;
         sql_attr_multi = uint lists_id from field; SELECT id FROM sentences_lists ;
         sql_attr_bool = origin_known
         sql_attr_bool = is_original
+        sql_attr_bool = owner_is_native
         sql_attr_json = trans
 
         sql_joined_field = \

commit 2c7552267e20efac8c2dd13a8c366dac602e760d

Fix sentence indexation query
Broken by 4beb12e6b8f9a32c0623d0a0d4fb8bcaa37bbf83.

ul_sent_end.level may be NULL, and this NULL value silently bubbles up,
changing the whole JSON object into a NULL value. As a result, all
translations added by members in languages not listed on their profile
are not indexed and cannot be found!

src/Shell/SphinxConfShell.php
@@ -673,7 +673,7 @@ EOT;
                         'u:',COALESCE(sent_end.user_id, 0),',', \
                         'c:',sent_end.correctness + 1,',', \
                         'a:',COUNT(audios_sent_end.id) > 0,',', \
-                        'n:',ul_sent_end.level = 5, \
+                        'n:',COALESCE(ul_sent_end.level = 5, 0), \
                     '}') as trans \
                 from \
                     sentences sent_start \