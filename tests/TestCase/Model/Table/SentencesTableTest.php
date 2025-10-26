f4b9c83bf6846aa0dab2783288c757ce41ebcc72
Franklin Pezzuti Dyer <franklin@dyer.me>
2025-10-26 16:22:23 -0600

    added test checking that updating an existing sentence also updates the user's last_contribution time

--- tests/TestCase/Model/Table/SentencesTableTest.php
+++ tests/TestCase/Model/Table/SentencesTableTest.php
@@ -1530,4 +1530,23 @@ class SentencesTableTest extends TestCase {
 
         $this->assertGreaterThan($previousLastContribution, $newLastContribution);
     }
+
+    public function testEditSentence_UpdatesLastContributionField() {
+        $user = $this->Sentence->Users->get(7);
+        CurrentUser::store($user);
+        
+        Time::setTestNow(new Time('2019-02-01 00:00:00')); 
+        $this->Sentence->saveNewSentence('This is my new English sentence.', 'eng', 1);
+        $user = $this->Sentence->Users->get(7);
+        $previousLastContribution = $user->last_contribution;
+
+        Time::setTestNow(new Time('2019-02-02 00:00:00')); 
+        $before = $this->Sentence->get(7);
+        $data = ['id' => '7', 'text' => 'This is the new text of sentence #7.'];
+        $after = $this->Sentence->editSentence($data);
+        $user = $this->Sentence->Users->get(7);
+        $newLastContribution = $user->last_contribution;
+
+        $this->assertGreaterThan($previousLastContribution, $newLastContribution);
+    }
 }
