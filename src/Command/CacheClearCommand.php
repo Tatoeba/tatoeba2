9212c1d7ab1eb83eebf2bec1e104e2a814ceefee
時流 <jiru@users.noreply.github.com>
2026-04-28 18:37:30 +0000

    Port "cake clear cache" hack to CakePHP 4.x

--- src/Command/CacheClearCommand.php
+++ src/Command/CacheClearCommand.php
@@ -1,26 +1,31 @@
 <?php
-namespace App\Shell;
+declare(strict_types=1);
+
+namespace App\Command;
 
 use Cake\Cache\Cache;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
 
 /**
- * Wrapper around the Cache Shell.
+ * Wrapper around the ClearCache Command
  *
  * We disable cache for CLI tools in config/bootstrap_cli.php to avoid file
  * permission problems, however running "cake cache clear" silently has
  * no effect when cache is disabled, which is very confusing.
  * This wrapper makes sure cache is enabled just when we want to clear it.
  */
-class CacheShell extends \Cake\Shell\CacheShell
+class CacheClearCommand extends \Cake\Command\CacheClearCommand
 {
-    public function clear($prefix = null)
+    public function execute(Arguments $args, ConsoleIo $io): ?int
     {
         if (Cache::enabled()) {
-            parent::clear($prefix);
+            $ret = parent::execute($args, $io);
         } else {
             Cache::enable();
-            parent::clear($prefix);
+            $ret = parent::execute($args, $io);
             Cache::disable();
         }
+        return $ret;
     }
 }
