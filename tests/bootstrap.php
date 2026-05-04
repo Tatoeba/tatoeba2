99df99fe4174a0624bc9545a1ff122581ee05505
時流 <jiru@users.noreply.github.com>
2026-05-04 07:24:19 +0000

    Merge tests/bootstrap.php from skeleton app
    This merges relevant changes made to that file
    in CakePHP skeleton app https://github.com/cakephp/app.
    
    In particular, CakePHP test suite expect the session id to be set to 'cli',
    otherwise it fails to generate correct tokens for SecurityComponent.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,27 @@
 <?php
+declare(strict_types=1);
+
 /**
  * Test runner bootstrap.
  *
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
-use \Cake\Cache\Cache;
-use \Cake\Core\Configure;
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
-$_SERVER['PHP_SELF'] = '/';
+if (empty($_SERVER['HTTP_HOST']) && !Configure::read('App.fullBaseUrl')) {
+    Configure::write('App.fullBaseUrl', 'http://localhost');
+}
+
+// Fixate sessionid early on, as php7.2+
+// does not allow the sessionid to be set after stdout
+// has been written to.
+session_id('cli');
 
 // Make sure search is disabled in order to prevent
 // update of Sphinx attributes in SphinxBehavior.
