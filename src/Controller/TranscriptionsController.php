Tatoeba/tatoeba2

commit 632ea38e25faf737932e5d4e8b27fd73acec4a2f

Fix returned sentence transcription type in API
The transcription type won’t be correct unless the "Sentences"
containment includes both "lang" and "script" fields.
The code responsible for fetching these fields is actually in
TranscriptionTable::beforeFind(), but it defaults to *all* fields,
whereas we usually only only need "lang" and "script".

Restricting it to only "lang" and "script" revealed that other
fields are needed by TranscriptionTable::findTranscription(),
but that is a special case.

We can handle this special case by using a closure for the contain,
allowing to add extra fields.

src/Controller/TranscriptionsController.php
@@ -58,7 +58,13 @@ class TranscriptionsController extends AppController
     }
 
     public function reset($sentenceId, $script) {
-        $transcr = $this->Transcriptions->findTranscription($sentenceId, $script);
+        $transcr = $this->Transcriptions
+            ->findTranscription($sentenceId, $script)
+            ->contain('Sentences', function ($q) {
+                // add all Sentences fields in the result
+                return $q->select($this->Transcriptions->Sentences);
+            })
+            ->first();
         $transcrOwnerId = null;
         if ($transcr) {
             $transcrOwnerId = $transcr->user_id;
@@ -97,7 +103,14 @@ class TranscriptionsController extends AppController
     }
 
     public function save($sentenceId, $script) {
-        $transcr = $this->Transcriptions->findTranscription($sentenceId, $script);
+        $transcr = $this->Transcriptions
+            ->findTranscription($sentenceId, $script)
+            ->contain('Sentences', function ($q) {
+                // add Sentences.user_id in the result
+                return $q->select(['user_id']);
+            })
+            ->first();
+
         $transcrOwnerId = null;
         if ($transcr) {
             $transcrOwnerId = $transcr->user_id;
@@ -146,7 +159,7 @@ class TranscriptionsController extends AppController
     private function returnTranscriptionData($saved, $sentenceId, $script) {
         if ($saved) {
             $transcription = $this->Transcriptions->findTranscription($sentenceId, $script);
-            $this->set('result', $transcription);
+            $this->set('result', $transcription->first());
             $this->loadComponent('RequestHandler');
             $this->set('_serialize', ['result']);
             $this->RequestHandler->renderAs($this, 'json');    

src/Controller/VHosts/Api/SentencesController.php
@@ -51,7 +51,6 @@ class SentencesController extends ApiController
         };
         $transcriptionsContainment = [
             'fields' => ['sentence_id', 'script', 'text', 'needsReview'],
-            'Sentences' => ['fields' => ['lang']],
         ];
         $indirTranslationsContainment = function (Query $q) use ($audioContainment, $transcriptionsContainment) {
             $q->select($this->fields())

src/Model/Table/TranscriptionsTable.php
@@ -145,7 +145,9 @@ class TranscriptionsTable extends Table
         // For cases where we want to query without joining with
         // sentences, we use a custom option: `withoutSentences`.
         if (!isset($options['withoutSentences']) || !$options['withoutSentences']) {
-            $query->contain(['Sentences']);
+            $query->contain('Sentences', function ($q) {
+                return $q->select(['lang', 'script']);
+            });
         }
     }
 
@@ -426,7 +428,7 @@ class TranscriptionsTable extends Table
             'Transcriptions.script' => $script
         ])->contain([
             'Users' => ['fields' => ['username']]
-        ])->first();
+        ]);
     }
 
     private function insertTranscriptionOrdered(&$transcriptions, $newTranscr) {

tests/TestCase/Controller/VHosts/Api/SentencesControllerTest.php
@@ -240,6 +240,7 @@ class MainControllerTest extends TestCase
             ]
         ];
         $this->assertJsonDocumentMatchesSchema($actual, $schema);
+        $this->assertJsonValueEquals($actual, '$.data[1].transcriptions[0].type', 'altscript');
     }
 
     public function testSearch_limitsTranslationsLanguage()

commit 239d937c9f54848d2b0ff55272e92dd5e71dc524

Fix sentences display on /transcriptions/of/{user}
Without all the necessary sentence fields, like text, we can’t display
sentences properly. This is likely a regression introduced in
632ea38e25faf737932e5d4e8b27fd73acec4a2f.

Closes #3288.

src/Controller/TranscriptionsController.php
@@ -166,6 +166,10 @@ class TranscriptionsController extends AppController
             $query = $this->Transcriptions
                 ->find()
                 ->where(['Transcriptions.user_id' => $userId])
+                ->contain(['Sentences' => function (\Cake\ORM\Query $q) {
+                    $fields = $q->getRepository()->fields();
+                    return $q->select($fields);
+                }])
                 ->mapReduce(
                     function ($transcr, $key, $mapReduce) {
                         $mapReduce->emitIntermediate($transcr, $transcr->sentence_id);