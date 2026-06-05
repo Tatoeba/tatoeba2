Tatoeba/tatoeba2

commit 1288c57fe91a93bc21c6580d51ed0d98f12b4756

Port CookieComponent code to CakePHP 4.x
There is no CookieComponent any more in CakePHP 4,
we must use methods from the ServerRequest object instead.

config/bootstrap.php
@@ -289,3 +289,7 @@ Cake\I18n\FrozenTime::setToStringFormat('yyyy-MM-dd HH:mm:ss');
 Cake\I18n\FrozenTime::$niceFormat = [\IntlDateFormatter::LONG, \IntlDateFormatter::LONG];
 
 Cake\Validation\Validator::addDefaultProvider('appvalidation', 'App\Validation\Validation');
+
+Cake\Http\Cookie\Cookie::setDefaults([
+    'expires' => new \DateTime('+1 month'),
+]);

src/Controller/ActivitiesController.php
@@ -29,6 +29,7 @@ namespace App\Controller;
 use App\Controller\AppController;
 use App\Model\CurrentUser;
 use Cake\Event\Event;
+use Cake\Http\Cookie\Cookie;
 
 /**
  * Controller for activities (i.e. things that contributors can do in Tatoeba).
@@ -119,12 +120,7 @@ class ActivitiesController extends AppController
             $sort = $this->request->getQuery('sort', 'created');
             $langTo = $this->request->getQuery('langTo');
 
-            $this->Cookie->write(
-                'not_translated_into_lang',
-                $langTo,
-                false,
-                '+1 month'
-            );
+            $this->response = $this->response->withCookie(Cookie::create('not_translated_into_lang', $langTo));
 
             $searchParams = array(
                 'from' => $langFrom,
@@ -140,7 +136,7 @@ class ActivitiesController extends AppController
                 '?' => $searchParams
             ));
         }
-        $notTranslatedInto = $this->Cookie->read('not_translated_into_lang');
+        $notTranslatedInto = $this->request->getCookie('not_translated_into_lang');
         $this->set('not_translated_into_lang', $notTranslatedInto);
     }
     

src/Controller/AppController.php
@@ -87,7 +87,6 @@ class AppController extends Controller
 
     public function initialize(): void
     {
-        $this->loadComponent('Cookie');
         $this->loadComponent('Csrf');
     }
 
@@ -159,8 +158,6 @@ class AppController extends Controller
         $this->Security->csrfCheck = false;
         $this->Security->blackHoleCallback = 'blackhole';
 
-        $this->Cookie->domain = TATOEBA_DOMAIN;
-        $this->Cookie->configKey('CakeCookie', 'encryption', false);
         $this->Auth->allow('display');
         $this->Auth->setConfig([
             // This line will call views/elements/session_expired.ctp.
@@ -237,16 +234,16 @@ class AppController extends Controller
 
         // Make some cookie values accessible to views
         // used in translation form and new sentence form
-        $preSelectedLang = $this->Cookie->read('contribute_lang');
+        $preSelectedLang = $this->request->getCookie('contribute_lang');
         $this->set('contribute_lang', $preSelectedLang);
 
         // used in show_all_in
-        $lang = $this->Cookie->read('browse_sentences_in_lang');
+        $lang = $this->request->getCookie('browse_sentences_in_lang');
         $this->set('browse_sentences_in_lang', $lang);
 
         // Use this when displaying the list to which a sentence should be assigned.
         // See views/helpers/menu.php, controllers/sentences_list_controller.php.
-        $mostRecentList = $this->Cookie->read('most_recent_list');
+        $mostRecentList = $this->request->getCookie('most_recent_list');
         $this->set('most_recent_list', $mostRecentList);
 
         $this->loadModel('WikiArticles');

src/Controller/Component/CommonSentenceComponent.php
@@ -27,6 +27,7 @@
 namespace App\Controller\Component;
 
 use Cake\Controller\Component;
+use Cake\Http\Cookie\Cookie;
 use Cake\ORM\TableRegistry;
 
 
@@ -44,7 +45,6 @@ class CommonSentenceComponent extends Component
 {
     public $components = array(
         'LanguageDetection',
-        'Cookie'
     );
 
     /**
@@ -71,7 +71,12 @@ class CommonSentenceComponent extends Component
         $correctness = 0,
         $license = null
     ) {
-        $this->Cookie->write('contribute_lang', $lang, false, "+1 month");
+        $controller = $this->getController();
+        $controller->setResponse(
+            $controller
+                 ->getResponse()
+                 ->withCookie(Cookie::create('contribute_lang', $lang))
+        );
 
         $lang = $this->_setLanguage($lang, $text, $username);
 

src/Controller/SentenceCommentsController.php
@@ -32,6 +32,7 @@ use Cake\Event\Event;
 use Cake\Core\Configure;
 use App\Model\CurrentUser;
 use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\Http\Cookie\Cookie;
 
 /**
  * Controller for sentence comments.
@@ -170,7 +171,7 @@ class SentenceCommentsController extends AppController
         $commentText = $data['text'];
         $sentenceId = $data['sentence_id'];
         // to avoid spammer and repost
-        $lastCom = $this->Cookie->read('hash_last_com');
+        $lastCom = $this->request->getCookie('hash_last_com');
         $thisCom = md5($commentText . $sentenceId);
         if ($lastCom == $thisCom) {
             return $this->redirect('/');
@@ -189,12 +190,7 @@ class SentenceCommentsController extends AppController
         $savedComment = $this->SentenceComments->save($comment);
         if ($savedComment) {
             $this->Flash->set(__('Your comment has been saved.'));
-            $this->Cookie->write(
-                'hash_last_com',
-                $thisCom,
-                false,
-                '+1 month'
-            );
+            $this->response = $this->response->withCookie(Cookie::create('hash_last_com', $thisCom));
         } else if ($comment->getErrors()) {
             foreach($comment->getErrors() as $error) {
                 $firstValidationErrorMessage = reset($error);

src/Controller/SentencesController.php
@@ -37,6 +37,7 @@ use App\Validation\Validation;
 use Cake\Core\Configure;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Event\Event;
+use Cake\Http\Cookie\Cookie;
 use Cake\Routing\Router;
 use Cake\Utility\Hash;
 use Cake\View\ViewBuilder;
@@ -433,7 +434,7 @@ class SentencesController extends AppController
         $translationText = $this->request->getData('value');
 
         // Store selected lang in cookie as default language for drop-downs
-        $this->Cookie->write('contribute_lang', $translationLang, false, "+1 month");
+        $this->response = $this->response->withCookie(Cookie::create('contribute_lang', $translationLang));
 
         if (isset($translationText)
             && trim($translationText) != ''
@@ -620,7 +621,8 @@ class SentencesController extends AppController
         $this->set('total', $total);
         $this->set('totalLimit', $totalLimit);
 
-        $this->Cookie->write('browse_sentences_in_lang', $lang, false, "+1 month");
+        $this->response = $this->response->withCookie(Cookie::create('browse_sentences_in_lang', $lang));
+
         $this->render(null);
     }
 

src/Controller/SentencesListsController.php
@@ -29,6 +29,7 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Core\Configure;
 use Cake\Event\Event;
+use Cake\Http\Cookie\Cookie;
 use Cake\ORM\Query;
 use App\Model\CurrentUser;
 use App\Model\Entity\SentencesList;
@@ -297,10 +298,10 @@ class SentencesListsController extends AppController
             // Retrieve the 'most_recent_list' cookie, and if it matches
             // $listId, erase it. Do this even if the 'remember_list' has
             // not been set, or has been set to false.
-            $mostRecentList = $this->Cookie->read('most_recent_list');
+            $mostRecentList = $this->request->getCookie('most_recent_list');
             if ($mostRecentList == $listId)
             {
-                $this->Cookie->delete('most_recent_list');
+                $this->response = $this->response->withExpiredCookie('most_recent_list');
             }
         }
 
@@ -322,7 +323,8 @@ class SentencesListsController extends AppController
         $userId = $this->Auth->user('id');
         if ($this->SentencesLists->addSentenceToList($sentenceId, $listId, $userId)) {
             $this->set('result', $listId);
-            $this->Cookie->write('most_recent_list', $listId, false, "+1 month");
+            $this->response = $this->response->withCookie(Cookie::create('most_recent_list', $listId));
+
         } else {
             $this->set('result', 'error');
             $this->set('error', __('The sentence could not be added to the list.'));
@@ -454,7 +456,7 @@ class SentencesListsController extends AppController
                 $this->Auth->user('id')
             );
 
-            $this->Cookie->write('most_recent_list', $listId, false, "+1 month");
+            $this->response = $this->response->withCookie(Cookie::create('most_recent_list', $listId));
         }
 
         $this->set('sentence', $result);
@@ -478,7 +480,7 @@ class SentencesListsController extends AppController
             if ($this->SentencesLists->addSentenceToList($sentenceId, $list->id, $userId)) {
                 $list->hasSentence = true;
                 $result = $list;
-                $this->Cookie->write('most_recent_list', $list->id, false, '+1 month');
+                $this->response = $this->response->withCookie(Cookie::create('most_recent_list', $list->id));
             }
         }
 

src/Middleware/LanguageSelectorMiddleware.php
@@ -5,7 +5,6 @@ use App\Lib\LanguagesLib;
 use Cake\Core\Configure;
 use Cake\Http\Cookie\Cookie;
 use Cake\I18n\I18n;
-use Cake\I18n\Time;
 
 /**
  * Middleware for UI language selection
@@ -68,8 +67,7 @@ class LanguageSelectorMiddleware
 
         $response = $response->withCookie(new Cookie(
             'interface_language',
-            $lang,
-            new Time('+1 month')
+            $lang
         ));
         $request = $request->withParam('lang', $lang);
         return $next($request, $response);

commit 72d9982bddd21f0a22320607778a0360c01ab8eb

Fix CSRF cookie expiry date
Fixes a regression introduced in 1288c57fe91a93bc21c6580d51ed0d98f12b4756:

> Cake\Http\Cookie\Cookie::setDefaults([
>     'expires' => new \DateTime('+1 month'),
> ]);

The above code sets the expiry of the CSRF cookie to 1 month in the future,
instead of the default "end with session". Note that it does not seem to be
possible to special-case the CSRF cookie expiry date to "end with session",
because one needs to give a falsy value to do that, however using a falsy
value makes it use the default, that is to say +1 month.

config/bootstrap.php
@@ -299,7 +299,3 @@ Cake\I18n\FrozenTime::setToStringFormat('yyyy-MM-dd HH:mm:ss');
 Cake\I18n\FrozenTime::$niceFormat = [\IntlDateFormatter::LONG, \IntlDateFormatter::LONG];
 
 Cake\Validation\Validator::addDefaultProvider('appvalidation', 'App\Validation\Validation');
-
-Cake\Http\Cookie\Cookie::setDefaults([
-    'expires' => new \DateTime('+1 month'),
-]);

src/Controller/ActivitiesController.php
@@ -29,7 +29,6 @@ namespace App\Controller;
 use App\Controller\AppController;
 use App\Model\CurrentUser;
 use Cake\Event\Event;
-use Cake\Http\Cookie\Cookie;
 
 /**
  * Controller for activities (i.e. things that contributors can do in Tatoeba).
@@ -113,7 +112,7 @@ class ActivitiesController extends AppController
             $sort = $this->request->getQuery('sort', 'created');
             $langTo = $this->request->getQuery('langTo');
 
-            $this->response = $this->response->withCookie(Cookie::create('not_translated_into_lang', $langTo));
+            $this->setOneMonthCookie('not_translated_into_lang', $langTo);
 
             $searchParams = array(
                 'from' => $langFrom,

src/Controller/AppController.php
@@ -31,6 +31,7 @@ use App\Model\CurrentUser;
 use App\Model\Entity\User;
 use Cake\Controller\Controller;
 use Cake\Event\Event;
+use Cake\Http\Cookie\Cookie;
 use Cake\Http\ServerRequest;
 use Cake\Routing\Router;
 
@@ -322,5 +323,10 @@ class AppController extends Controller
                 $session->write('last_used_lang', $current);
             }
         }
+    }
+
+    public function setOneMonthCookie(string $name, $value): void {
+        $cookie = Cookie::create($name, $value, ['expires' => new \DateTime('+1 month')]);
+        $this->response = $this->response->withCookie($cookie);
     }
 }

src/Controller/Component/CommonSentenceComponent.php
@@ -28,7 +28,6 @@ namespace App\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\Datasource\FactoryLocator;
-use Cake\Http\Cookie\Cookie;
 
 
 /**
@@ -72,11 +71,7 @@ class CommonSentenceComponent extends Component
         $license = null
     ) {
         $controller = $this->getController();
-        $controller->setResponse(
-            $controller
-                 ->getResponse()
-                 ->withCookie(Cookie::create('contribute_lang', $lang))
-        );
+        $controller->setOneMonthCookie('contribute_lang', $lang);
 
         $lang = $this->_setLanguage($lang, $text, $username);
 

src/Controller/SentenceCommentsController.php
@@ -32,7 +32,6 @@ use Cake\Event\Event;
 use Cake\Core\Configure;
 use App\Model\CurrentUser;
 use Cake\Datasource\Exception\RecordNotFoundException;
-use Cake\Http\Cookie\Cookie;
 
 /**
  * Controller for sentence comments.
@@ -177,7 +176,7 @@ class SentenceCommentsController extends AppController
         $savedComment = $this->SentenceComments->save($comment);
         if ($savedComment) {
             $this->Flash->set(__('Your comment has been saved.'));
-            $this->response = $this->response->withCookie(Cookie::create('hash_last_com', $thisCom));
+            $this->setOneMonthCookie('hash_last_com', $thisCom);
         } else if ($comment->getErrors()) {
             foreach($comment->getErrors() as $error) {
                 $firstValidationErrorMessage = reset($error);

src/Controller/SentencesController.php
@@ -37,7 +37,6 @@ use App\Validation\Validation;
 use Cake\Core\Configure;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Event\Event;
-use Cake\Http\Cookie\Cookie;
 use Cake\Routing\Router;
 use Cake\Utility\Hash;
 use Cake\View\ViewBuilder;
@@ -389,7 +388,7 @@ class SentencesController extends AppController
         $translationText = $this->request->getData('value');
 
         // Store selected lang in cookie as default language for drop-downs
-        $this->response = $this->response->withCookie(Cookie::create('contribute_lang', $translationLang));
+        $this->setOneMonthCookie('contribute_lang', $translationLang);
 
         if (isset($translationText)
             && trim($translationText) != ''
@@ -574,7 +573,7 @@ class SentencesController extends AppController
         $this->set('total', $total);
         $this->set('totalLimit', $totalLimit);
 
-        $this->response = $this->response->withCookie(Cookie::create('browse_sentences_in_lang', $lang));
+        $this->setOneMonthCookie('browse_sentences_in_lang', $lang);
 
         $this->render(null);
     }

src/Controller/SentencesListsController.php
@@ -29,7 +29,6 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Core\Configure;
 use Cake\Event\Event;
-use Cake\Http\Cookie\Cookie;
 use Cake\ORM\Query;
 use App\Model\CurrentUser;
 use App\Model\Entity\SentencesList;
@@ -291,7 +290,7 @@ class SentencesListsController extends AppController
         $userId = $this->Auth->user('id');
         if ($this->SentencesLists->addSentenceToList($sentenceId, $listId, $userId)) {
             $this->set('result', $listId);
-            $this->response = $this->response->withCookie(Cookie::create('most_recent_list', $listId));
+            $this->setOneMonthCookie('most_recent_list', $listId);
 
         } else {
             $this->set('result', 'error');
@@ -424,7 +423,7 @@ class SentencesListsController extends AppController
                 $this->Auth->user('id')
             );
 
-            $this->response = $this->response->withCookie(Cookie::create('most_recent_list', $listId));
+            $this->setOneMonthCookie('most_recent_list', $listId);
         }
 
         $this->set('sentence', $result);
@@ -448,7 +447,7 @@ class SentencesListsController extends AppController
             if ($this->SentencesLists->addSentenceToList($sentenceId, $list->id, $userId)) {
                 $list->hasSentence = true;
                 $result = $list;
-                $this->response = $this->response->withCookie(Cookie::create('most_recent_list', $list->id));
+                $this->setOneMonthCookie('most_recent_list', $list->id);
             }
         }
 

src/Middleware/LanguageSelectorMiddleware.php
@@ -72,9 +72,10 @@ class LanguageSelectorMiddleware implements MiddlewareInterface
 
         $response = $handler->handle($request);
 
-        return $response->withCookie(new Cookie(
+        return $response->withCookie(Cookie::create(
             'interface_language',
-            $lang
+            $lang,
+            ['expires' => '+1 month']
         ));
     }
 