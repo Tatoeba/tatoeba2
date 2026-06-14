Tatoeba/tatoeba2

commit 89c4cb922d349c8b814aa03a019b9abd432c8723

Fix Form/FormHelper deprecations
This commit addresses the following Form and FormHelper related 3.x deprecation warnings
- FormHelper::input() is deprecated. Use FormHelper::control() instead. See https://book.cakephp.org/3/en/appendices/3-4-migration-guide.html#other-deprecations.
- FormHelpter::create() should take `null` instead of a `string` or `bool` as the context when creating contextless forms. See https://book.cakephp.org/3/en/appendices/3-9-migration-guide.html#deprecations.

src/View/Helper/SentenceAnnotationsHelper.php
@@ -53,16 +53,13 @@ class SentenceAnnotationsHelper extends AppHelper
         <div class="module">
         <h2>Go to...</h2>
         <?php
-            echo $this->Form->create('SentenceAnnotation', array(
+            echo $this->Form->create(null, array(
                 'url' => array('action' => 'show')
             ));
-            echo $this->Form->input(
-                'sentence_id',
-                array(
-                    "type" => "text",
-                    "label" => "Sentence #"
-                )
-            );
+            echo $this->Form->control('sentence_id', array(
+                "type" => "text",
+                "label" => "Sentence #"
+            ));
             echo $this->Form->button('OK');
             echo $this->Form->end();
         ?>
@@ -83,14 +80,14 @@ class SentenceAnnotationsHelper extends AppHelper
         <div class="module">
         <h2>Search</h2>
         <?php
-            echo $this->Form->create('SentenceAnnotation', array(
+            echo $this->Form->create(null, array(
                 'url' => array('action' => 'search')
             ));
-            echo $this->Form->input(
+            echo $this->Form->control(
                 'text',
                 array(
                     "label" => "",
-                    "type" => "text"
+                    "type" => "number"
                 )
             );
             echo $this->Form->button('OK');
@@ -114,13 +111,12 @@ class SentenceAnnotationsHelper extends AppHelper
         <div class="module">
         <h2>Add new index</h2>
         <?php
-        echo $this->Form->create(
-            'SentenceAnnotation', array(
+        echo $this->Form->create(null, array(
                 "url" => array("action" => "save")
             )
         );
         echo $this->Form->hidden('sentence_id', ['value' => $sentenceId]);
-        echo $this->Form->input('meaning_id', ['type' => 'text']);
+        echo $this->Form->control('meaning_id', ['type' => 'number']);
         echo $this->Form->textarea('text', ['cols' => 24, 'rows' => 3]);
         echo $this->Form->button('save');
         echo $this->Form->end();
@@ -145,8 +141,7 @@ class SentenceAnnotationsHelper extends AppHelper
             echo '<h2>Replace</h2>';
 
             $this->Html->script('sentence_annotations.preview.js', array('block' => 'scriptBottom'));
-            echo $this->Form->create(
-                'SentenceAnnotation', array(
+            echo $this->Form->create(null, array(
                     "url" => array("action" => "replace")
                 )
             );
@@ -156,7 +151,7 @@ class SentenceAnnotationsHelper extends AppHelper
                 'value' => $stringToReplace
             ]);
             echo '</div>';
-            echo $this->Form->input('textReplacing', [
+            echo $this->Form->control('textReplacing', [
                     'id' => 'SentenceAnnotationTextReplacing',
                     "label" => "Replace ". h($stringToReplace) ." by:"
             ]);

src/Template/Activities/translate_sentences.ctp
@@ -104,7 +104,7 @@ $langsTo = $this->Languages->profileLanguagesArray();
         <div layout-margin>
             <?php
             echo $this->Form->create(
-                'Activity',
+                null,
                 array(
                     "url" => array("action" => "translate_sentences"),
                     "type" => "get"

src/Template/Audio/import.ctp
@@ -215,7 +215,7 @@ if ($filesToImport): ?>
                       'inside the import directory.'); ?></p>
 <?php endif; ?>
 <?php
-echo $this->Form->input('audioAuthor', ['required' => true]);
+echo $this->Form->control('audioAuthor', ['required' => true]);
 echo $this->Form->submit(__d('admin', 'Import'), $canImport ? [] : ['disabled' => true]);
 echo $this->Form->end();
 ?>

src/Template/Audio/of.ctp
@@ -45,13 +45,13 @@ if (isset($sentencesWithAudio)) {
                    'url' => array('controller' => 'audio', 'action' => 'save_settings'),
                    'type' => 'post',
                ));
-               echo $this->Form->input('audio_license', array(
+               echo $this->Form->control('audio_license', array(
                    'label' => __('License:'),
                    'options' => $this->AudioLicense->getLicenseOptions()
                ));
             ?>
             <md-input-container class="md-block">
-                <?= $this->Form->input('audio_attribution_url', array(
+                <?= $this->Form->control('audio_attribution_url', array(
                     'label' => __('Attribution URL:'),
                 )) ?>
                 <div class='hint'>

src/Template/CategoriesTree/manage.ctp
@@ -56,7 +56,7 @@ $messages = [
         <div class="section md-whiteframe-1dp">
             <?php
                 echo $this->Form->create(
-                    'CreateOrEditCategory', 
+                    null, 
                     [
                         'url' => [
                             'controller' => 'categoriesTree',
@@ -68,7 +68,7 @@ $messages = [
 
             <md-input-container layout="column">
                 <?php
-                    echo $this->Form->input(
+                    echo $this->Form->control(
                         'name', 
                         [
                             'label' => __d('beta', 'Name (required)')
@@ -79,7 +79,7 @@ $messages = [
                     
             <md-input-container layout="column">
                 <?php
-                    echo $this->Form->input(
+                    echo $this->Form->control(
                         'description', 
                         [
                             'label' => __d('beta', 'Description (optional)')
@@ -90,7 +90,7 @@ $messages = [
 
             <md-input-container layout="column">
                 <?php
-                    echo $this->Form->input(
+                    echo $this->Form->control(
                         'parentName', 
                         [
                             'id' => 'parentName',
@@ -122,7 +122,7 @@ $messages = [
         <div class="section md-whiteframe-1dp">
             <?php
                 echo $this->Form->create(
-                    'AttachTag', 
+                    null, 
                     [
                         'url' => [
                             'controller' => 'categories_tree',
@@ -134,7 +134,7 @@ $messages = [
 
             <md-input-container layout="column">
                 <?php
-                    echo $this->Form->input(
+                    echo $this->Form->control(
                         'tagName',
                         [
                             'id' => 'tagName',
@@ -147,7 +147,7 @@ $messages = [
 
             <md-input-container layout="column">
                 <?php
-                    echo $this->Form->input(
+                    echo $this->Form->control(
                         'categoryName', 
                         [
                             'id' => 'categoryName',

src/Template/Element/advanced_search_form.ctp
@@ -21,7 +21,7 @@ $layout = isset($isSidebar) && $isSidebar ? 'column' : 'row';
 
 $this->Html->script('sentences/search.ctrl.js', ['block' => 'scriptBottom']);
 
-echo $this->Form->create('AdvancedSearch', [
+echo $this->Form->create(null, [
     'id' => 'advanced-search',
     'url' => false,
     'name' => 'form',
@@ -59,7 +59,7 @@ echo $this->Form->create('AdvancedSearch', [
         <div layout="column">
             <md-input-container class="md-button-right">
                 <?php
-                echo $this->Form->input('query', array(
+                echo $this->Form->control('query', array(
                     'label' => __('Words:'),
                     'lang' => '',
                     'dir' => 'auto',
@@ -148,7 +148,7 @@ echo $this->Form->create('AdvancedSearch', [
 
             <md-input-container class="md-button-right">
                 <?php
-                echo $this->Form->input('user', array(
+                echo $this->Form->control('user', array(
                     'label' => __('Owner:'),
                     'id' => 'OwnerSearch',
                     'ng-model' => 'filters.user',
@@ -182,7 +182,7 @@ echo $this->Form->create('AdvancedSearch', [
                 <div layout="row" layout-align="center">
                     <label for="orphans" flex><?= __('Is orphan:') ?></label>
                     <?php
-                    echo $this->Form->input('orphans', [
+                    echo $this->Form->control('orphans', [
                         'label' => '',
                         'options' => [
                             /* @translators: dropdown option of "Is orphan" field in search form */
@@ -206,7 +206,7 @@ echo $this->Form->create('AdvancedSearch', [
                 <div layout="row" layout-align="center">
                     <label for="unapproved" flex><?= __('Is unapproved:') ?></label>
                     <?php
-                    echo $this->Form->input('unapproved', array(
+                    echo $this->Form->control('unapproved', array(
                         'label' => '',
                         'options' => array(
                             /* @translators: dropdown option of "Is unapproved" field in search form */
@@ -228,7 +228,7 @@ echo $this->Form->create('AdvancedSearch', [
                 <div layout="row" layout-align="center">
                     <label for="native" flex><?= __('Is owned by a native:') ?></label>
                     <?php
-                    echo $this->Form->input('native', array(
+                    echo $this->Form->control('native', array(
                         'label' => '',
                         'options' => array(
                             /* @translators: dropdown option of "Is owned by a native:" field in search form */
@@ -249,7 +249,7 @@ echo $this->Form->create('AdvancedSearch', [
             <div class="param" layout="row" layout-align="center">
                 <label for="has-audio" flex><?= __('Has audio:') ?></label>
                 <?php
-                echo $this->Form->input('has_audio', array(
+                echo $this->Form->control('has_audio', array(
                     'label' => '',
                     'options' => array(
                         /* @translators: dropdown option of "Has audio" field in search form */
@@ -265,7 +265,7 @@ echo $this->Form->create('AdvancedSearch', [
 
             <md-input-container class="md-button-right">
             <?php
-            echo $this->Form->input('tags', array(
+            echo $this->Form->control('tags', array(
                 'label' => __('Tags:'),
                 'id' => 'TagSearch',
                 'ng-model' => 'filters.tags',
@@ -285,7 +285,7 @@ echo $this->Form->create('AdvancedSearch', [
                 <div flex>
                 <?php
                 $listOptions = $this->Lists->listsAsSelectable($searchableLists->toList());
-                echo $this->Form->input('list', [
+                echo $this->Form->control('list', [
                     'class' => 'list-select',
                     'label' => '',
                     'options' => $this->safeForAngular($listOptions),
@@ -344,7 +344,7 @@ echo $this->Form->create('AdvancedSearch', [
                 <div class="param" layout="row" layout-align="center">
                     <label for="trans-link" flex><?= __('Link:') ?></label>
                     <?php
-                    echo $this->Form->input('trans_link', array(
+                    echo $this->Form->control('trans_link', array(
                         'label' => '',
                         'options' => array(
                             /* @translators: dropdown option of "Link" field in search form */
@@ -362,7 +362,7 @@ echo $this->Form->create('AdvancedSearch', [
 
                 <md-input-container class="md-button-right">
                     <?php
-                    echo $this->Form->input('trans_user', array(
+                    echo $this->Form->control('trans_user', array(
                         'label' => __('Owner:'),
                         'id' => 'TranslatorSearch',
                         'ng-model' => 'filters.trans_user',
@@ -379,7 +379,7 @@ echo $this->Form->create('AdvancedSearch', [
                     <div layout="row" layout-align="center">
                         <label for="trans-orphan" flex><?= __('Is orphan:') ?></label>
                         <?php
-                        echo $this->Form->input('trans_orphan', array(
+                        echo $this->Form->control('trans_orphan', array(
                             'label' => '',
                             'options' => array(
                                 '' => __x('orphan', 'Any'),
@@ -400,7 +400,7 @@ echo $this->Form->create('AdvancedSearch', [
                     <div layout="row" layout-align="center">
                         <label for="trans-unapproved" flex><?= __('Is unapproved:') ?></label>
                         <?php
-                        echo $this->Form->input('trans_unapproved', array(
+                        echo $this->Form->control('trans_unapproved', array(
                             'label' => '',
                             'options' => array(
                                 '' => __x('unapproved', 'Any'),
@@ -442,7 +442,7 @@ echo $this->Form->create('AdvancedSearch', [
                 <div class="param" layout="row" layout-align="center">
                     <label for="trans-has-audio" flex><?= __('Has audio:') ?></label>
                     <?php
-                    echo $this->Form->input('trans_has_audio', array(
+                    echo $this->Form->control('trans_has_audio', array(
                         'label' => '',
                         'options' => array(
                             '' => __x('audio', 'Any'),
@@ -466,7 +466,7 @@ echo $this->Form->create('AdvancedSearch', [
                     <?php /* @translators: field name in search form (noun) */ ?>
                     <label for="sort" flex><?= __('Order:') ?></label>
                     <?php
-                    echo $this->Form->input('sort', array(
+                    echo $this->Form->control('sort', array(
                         'label' => '',
                         'options' => array(
                             /* @translators: sort order dropdown option in search form */

src/Template/Element/announcement.ctp
@@ -41,7 +41,7 @@ if (!CurrentUser::hasAcceptedNewTermsOfUse()) {
         'controller' => 'pages', 
         'action' => 'contact'
     ]);
-    echo $this->Form->create('Users', [
+    echo $this->Form->create(null, [
         'class' => 'announcement md-whiteframe-1dp',
         'url' => ['controller' => 'user', 'action' => 'accept_new_terms_of_use']
     ]);

src/Template/Element/private_messages/form.ctp
@@ -58,7 +58,7 @@ if ($isReply) {
 
         <md-input-container>
         <?php
-        echo $this->Form->input('title', [
+        echo $this->Form->control('title', [
             /* @translators: title field label in private message form */
             'label' => __('Title'),
             'value' => $this->safeForAngular($pm->title),

commit 96fcb77822b052c7767b79a2faa423847005b069

Fix regression in form input
Fixes a regression introduced by 89c4cb922d349c8b814aa03a019b9abd432c8723.
The field type was inadvertently changed from "text" to "number".

Closes #3300.

src/View/Helper/SentenceAnnotationsHelper.php
@@ -87,7 +87,7 @@ class SentenceAnnotationsHelper extends AppHelper
                 'text',
                 array(
                     "label" => "",
-                    "type" => "number"
+                    "type" => "text"
                 )
             );
             echo $this->Form->button('OK');