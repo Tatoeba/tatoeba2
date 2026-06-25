Tatoeba/tatoeba2

commit ec9a1749537c9817968d090a29d75d01deb8c142

Refactor "list created by" text

src/View/Helper/ListsHelper.php
@@ -52,6 +52,21 @@ class ListsHelper extends AppHelper
         'Number'
     );
 
+    public function createdByText(?string $username): string
+    {
+        $listAuthor = $this->Html->link(
+            $username,
+            [
+                'controller' => 'user',
+                'action' => 'profile',
+            ]
+        );
+        return format(
+            __('created by {listAuthor}'),
+            compact('listAuthor')
+        );
+    }
+
     /**
      * display an array of lists in an HTML table
      *
@@ -167,20 +182,8 @@ class ListsHelper extends AppHelper
             </div>
 
             <div class="creator">
-            <?php
-            $link = $this->Html->link(
-                $listCreatorName,
-                array(
-                    "controller"=>"user",
-                    "action"=>"profile",
-                    $listCreatorName
-                )
-            );
-            echo format(__('created by {listAuthor}'),
-                         array('listAuthor' => $link));
-
-             ?>
-             </div>
+                <?= $this->createdByText($listCreatorName) ?>
+            </div>
          </td>
 
          <td>

templates/SentencesLists/show.php
@@ -51,18 +51,7 @@ $this->set('title_for_layout', $this->Pages->formatTitle($listName));
     <div class="section md-whiteframe-1dp">
         <h2><?php echo __('About this list'); ?></h2>
         <?php
-        $linkToAuthorProfile = $this->Html->link(
-            $user['username'],
-            array(
-                'controller' => 'user',
-                'action' => 'profile',
-                $user['username']
-            )
-        );
-        $createdBy = format(
-            __('created by {listAuthor}'),
-            array('listAuthor' => $linkToAuthorProfile)
-        );
+        $createdBy = $this->Lists->createdByText($user->username);
         $createdDate = $this->Date->ago($list['created']);
         echo $this->Html->tag('p', $createdBy);
         echo $this->Html->tag('p', $createdDate);

templates/SentencesLists/show_angular.php
@@ -46,18 +46,7 @@ $this->set('title_for_layout', $this->Pages->formatTitle($listName));
     <div class="section md-whiteframe-1dp">
         <h2><?php echo __('About this list'); ?></h2>
         <?php
-        $linkToAuthorProfile = $this->Html->link(
-            $user['username'],
-            array(
-                'controller' => 'user',
-                'action' => 'profile',
-                $user['username']
-            )
-        );
-        $createdBy = format(
-            __('created by {listAuthor}'),
-            array('listAuthor' => $linkToAuthorProfile)
-        );
+        $createdBy = $this->Lists->createdByText($user->username);
         $createdDate = $this->Date->ago($list['created']);
         echo $this->Html->tag('p', $createdBy);
         echo $this->Html->tag('p', $createdDate);

commit 4805386683530dcf7e4beaedc70125765484400e

Fix list owner link
Regression introduced by ec9a1749537c9817968d090a29d75d01deb8c142.

Closes #3307.

src/View/Helper/ListsHelper.php
@@ -64,6 +64,7 @@ class ListsHelper extends AppHelper
                 [
                     'controller' => 'user',
                     'action' => 'profile',
+                    $username,
                 ]
             );
             return format(