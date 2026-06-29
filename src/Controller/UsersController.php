Tatoeba/tatoeba2

commit b92767b9197d0d3a7bc5684baa3c9f95283ee22a

Stop passing query options as paginator settings
This fixes the many deprecation warnings after upgrading to CakePHP 4.5:

> Passing query options as paginator settings is deprecated. Use a custom finder through `finder` config instead. Extra keys found are: [...]

src/Controller/UsersController.php
@@ -482,13 +482,15 @@ class UsersController extends AppController
 
         $this->paginate = array(
             'limit' => 20,
-            'order' => array('role' => 'desc', 'id' => 'asc'),
-            'fields' => array('id', 'username', 'since', 'image', 'role'),
-            'sort' => $this->request->getQuery('sort', 'role'),
-            'direction' => $this->request->getQuery('direction', 'asc'),
+            'order' => [
+                $this->request->getQuery('sort', 'role') => $this->request->getQuery('direction', 'asc'),
+            ],
         );
 
-        $query = $this->Users->find()->where(['Users.role IN' => User::ROLE_CONTRIBUTOR_OR_HIGHER]);
+        $query = $this->Users
+            ->find()
+            ->select(['id', 'username', 'since', 'image', 'role'])
+            ->where(['Users.role IN' => User::ROLE_CONTRIBUTOR_OR_HIGHER]);
         try {
             $users = $this->paginate($query);
         } catch (\Cake\Http\Exception\NotFoundException $e) {
@@ -547,8 +549,12 @@ class UsersController extends AppController
             $lang = $usersLanguages[0]->language_code;
         }
 
-        $this->paginate = $UsersLanguages->getUsersForLanguage($lang);
-        $users = $this->paginate('UsersLanguages');
+        $this->paginate = [
+            'order' => ['UsersLanguages.level' => 'DESC', 'Users.last_contribution' => 'DESC'],
+            'limit' => 30,
+        ];
+        $query = $UsersLanguages->find('paginated', compact('lang'));
+        $users = $this->paginate($query);
 
         $this->set('users', $users);
         $this->set('usersLanguages', $usersLanguages);

src/Controller/LicensingController.php
@@ -39,17 +39,20 @@ class LicensingController extends AppController {
 
     private function paginateAffected($listId) {
         $pagination = [
-            'contain' => [
-                'Sentences' => function($q) {
-                    return $q->select(['id', 'lang', 'text', 'correctness']);
-                },
-            ],
-            'conditions' => ['sentences_list_id' => $listId],
             'limit' => CurrentUser::getSetting('sentences_per_page'),
             'order' => ['Sentences.created']
         ];
         $this->paginate = $pagination;
-        return $this->paginate('SentencesSentencesLists');
+        $query = $this
+            ->SentencesSentencesLists
+            ->find()
+            ->where(['sentences_list_id' => $listId])
+            ->contain([
+                'Sentences' => function($q) {
+                    return $q->select(['id', 'lang', 'text', 'correctness']);
+                }
+            ]);
+        return $this->paginate($query);
     }
 
     public function refresh_license_switch_list() {

src/Controller/PrivateMessagesController.php
@@ -72,13 +72,15 @@ class PrivateMessagesController extends AppController
      */
     public function folder($folder = 'Inbox', $status = 'all')
     {
-        $this->paginate = $this->PrivateMessages->getPaginatedMessages(
-            $this->Auth->user('id'),
-            $folder,
-            $status
-        );
+        $this->paginate = [
+            'order' => ['date' => 'DESC'],
+            'limit' => 20
+        ];
 
-        $content = $this->paginate();
+        $userId = $this->Auth->user('id');
+        $options = compact('userId', 'folder', 'status');
+        $query = $this->PrivateMessages->find('paginated', $options);
+        $content = $this->paginate($query);
 
         $this->set('folder', $folder);
         $this->set('content', $content);

src/Controller/ReviewsController.php
@@ -114,10 +114,13 @@ class ReviewsController extends AppController
             return;
         }
 
-        $this->paginate = $this->UsersSentences->getPaginatedCorpusOf(
-            $userId, $correctnessLabel, $lang
-        );
-        $corpus = $this->paginate('UsersSentences');
+        $this->paginate = [
+            'limit' => 50,
+            'order' => ['modified' => 'DESC']
+        ];
+        $options = compact('userId', 'correctnessLabel', 'lang');
+        $query = $this->UsersSentences->find('paginated', $options);
+        $corpus = $this->paginate($query);
 
         $this->set('username', $username);
         $this->set('corpus', $corpus);

src/Controller/SentenceAnnotationsController.php
@@ -172,24 +172,16 @@ class SentenceAnnotationsController extends AppController
      */
     public function last_modified()
     {
-        $pagination = [
-            'fields' => [
-                'sentence_id',
-                'modified',
-                'user_id',
-                'text'
-            ],
-            'contain' => [
-                'Users' => [
-                    'fields' => ['username']
-                ]
-            ],
+        $this->paginate = [
             'limit' => 200,
             'order' => ['modified' => 'DESC']
         ];
-
-        $this->paginate = $pagination;
-        $results = $this->paginate();
+        $query = $this
+            ->SentenceAnnotations
+            ->find()
+            ->select(['sentence_id', 'modified', 'user_id', 'text'])
+            ->contain(['Users' => ['fields' => ['username']]]);
+        $results = $this->paginate($query);
 
         $this->set('annotations', $results);
     }

src/Controller/SentenceCommentsController.php
@@ -47,20 +47,6 @@ class SentenceCommentsController extends AppController
     public $name = 'SentenceComments';
 
     public $paginate = [
-        'contain' => [
-            'Users' => [
-                'fields' => [
-                    'id',
-                    'username',
-                    'image',
-                ]
-            ],
-            'Sentences' => [
-                'Users' => [
-                    'fields' => ['id', 'username']
-                ]
-            ]
-        ],
         'limit' => 50,
         'order' => ['SentenceComments.id' => 'DESC'],
     ];
@@ -114,7 +100,8 @@ class SentenceCommentsController extends AppController
         }
 
         $finder = ['latest' => $options];
-        $latestComments = $this->paginateOrRedirect($this->SentenceComments, compact('finder'));
+        $query = $this->SentenceComments->find('paginated');
+        $latestComments = $this->paginateOrRedirect($query, compact('finder'));
 
         $commentsPermissions = $this->Permissions->getCommentsOptions($latestComments);
 
@@ -299,11 +286,10 @@ class SentenceCommentsController extends AppController
             return;
         }
 
-        $this->paginate['conditions'] = array(
-            'SentenceComments.user_id' => $userId
-        );
-
-        $userComments = $this->paginate();
+        $query = $this->SentenceComments
+            ->find('paginated')
+            ->where(['SentenceComments.user_id' => $userId]);
+        $userComments = $this->paginate($query);
 
         $commentsPermissions = $this->Permissions->getCommentsOptions($userComments);
 
@@ -331,12 +317,13 @@ class SentenceCommentsController extends AppController
         }
 
         $botsIds = Configure::read('Bots.userIds');
-        $conditions = ['Sentences.user_id' => $userId];
+        $query = $this->SentenceComments
+            ->find('paginated')
+            ->where(['Sentences.user_id' => $userId]);
         if (!empty($botsIds)) {
-            $conditions['SentenceComments.user_id NOT IN'] = $botsIds;
+            $query->where(['SentenceComments.user_id NOT IN' => $botsIds]);
         }
-        $this->paginate['conditions'] = $conditions;
-        $userComments = $this->paginate();
+        $userComments = $this->paginate($query);
 
         $commentsPermissions = $this->Permissions->getCommentsOptions($userComments);
 

src/Controller/SentencesController.php
@@ -476,12 +476,12 @@ class SentencesController extends AppController
             ->find('filteredTranslations', [
                 'translationLang' => $search->getData('to'),
             ])
+            ->applyOptions(compact('sphinx'))
             ->select($this->Sentences->fields())
             ->contain($this->Sentences->contain(['translations' => true]));
 
         $this->paginate = [
             'limit' => $limit,
-            'sphinx' => $sphinx,
         ];
 
         try {
@@ -637,38 +637,33 @@ class SentencesController extends AppController
 
         $this->paginate = array(
             'Sentences' => array(
-                'fields' => array(
-                    'id',
-                    'text',
-                    'lang',
-                    'user_id',
-                    'correctness'
-                ),
-                'contain' => array(
-                    'Transcriptions' => array(
-                        'Users' => array('fields' => array('username')),
-                    ),
-                    'Users' => array(
-                        'fields' => array('username')
-                    )
-                ),
                 'limit' => CurrentUser::getSetting('sentences_per_page'),
                 'order' => ['modified' => 'DESC']
             )
         );
 
-        $conditions = $this->Sentences->find()->where(['user_id' => $userId]);
+        $query = $this->Sentences
+            ->find()
+            ->select(['id', 'text', 'lang', 'user_id', 'correctness'])
+            ->where(['user_id' => $userId])
+            ->contain([
+                'Transcriptions' => [
+                    'Users' => ['fields' => ['username']],
+                ],
+                'Users' => ['fields' => ['username']],
+            ]);
+
         // if the lang is specified then we also filter on the language
         if (!empty($lang)) {
-            $conditions = $conditions->where(['lang' => $lang]);
+            $query->where(['lang' => $lang]);
         }
 
         if ($onlyOriginal) {
-            $conditions = $conditions->where('based_on_id = 0');
+            $query->where('based_on_id = 0');
         }
 
         try {
-            $sentences = $this->paginate($conditions);
+            $sentences = $this->paginate($query);
         } catch (\Cake\Http\Exception\NotFoundException $e) {
             return $this->redirectPaginationToLastPage();
         }

src/Controller/SentencesListsController.php
@@ -46,6 +46,11 @@ class SentencesListsController extends AppController
 {
     public $name = 'SentencesLists';
 
+    public $paginate = [
+        'order' => ['created' => 'DESC'],
+        'limit' => 50
+    ];
+
     /**
      * Before filter.
      *
@@ -76,10 +81,11 @@ class SentencesListsController extends AppController
             return $this->redirect(array('action' => 'index', $search));
         }
 
-        $this->paginate = $this->SentencesLists->getPaginatedLists(
-            $filter, null, ['public', 'listed']
-        );
-        $allLists = $this->paginate();
+        $query = $this->SentencesLists->find('paginated', [
+            'search' => $filter,
+            'visibility' => ['public', 'listed'],
+        ]);
+        $allLists = $this->paginate($query);
 
         $this->set('allLists', $allLists);
         $this->set('filter', $filter ?? '');
@@ -93,10 +99,12 @@ class SentencesListsController extends AppController
             return $this->redirect(array('action' => 'collaborative', $search));
         }
 
-        $this->paginate = $this->SentencesLists->getPaginatedLists(
-            $filter, null, ['public', 'listed'], 'anyone'
-        );
-        $allLists = $this->paginate();
+        $query = $this->SentencesLists->find('paginated', [
+            'search' => $filter,
+            'visibility' => ['public', 'listed'],
+            'editableBy' => 'anyone',
+        ]);
+        $allLists = $this->paginate($query);
 
         $this->set('allLists', $allLists);
         $this->set('filter', $filter ?? '');
@@ -169,8 +177,9 @@ class SentencesListsController extends AppController
         $SentencesSentencesLists = $this->fetchTable('SentencesSentencesLists');
         $this->paginate = [
             'limit' => CurrentUser::getSetting('sentences_per_page'),
-            'sort' => $this->request->getQuery('sort', 'id'),
-            'direction' => $this->request->getQuery('direction', 'desc'),
+            'order' => [
+                $this->request->getQuery('sort', 'id') => $this->request->getQuery('direction', 'desc'),
+            ],
             'sortableFields' => ['id', 'sentence_id'],
         ];
         $finder = ['latest' => $options];
@@ -358,14 +367,15 @@ class SentencesListsController extends AppController
             return;
         }
 
-        $visibility = null;
         if ($username != CurrentUser::get('username')) {
             $visibility = ['public', 'listed'];
         }
-        $this->paginate = $this->SentencesLists->getPaginatedLists(
-            $filter, $username, $visibility
-        );
-        $userLists = $this->paginate();
+        $query = $this->SentencesLists->find('paginated', [
+            'search' => $filter,
+            'username' => $username,
+            'visibility' => $visibility ?? null,
+        ]);
+        $userLists = $this->paginate($query);
 
         $this->set('userLists', $userLists);
         $this->set('filter', $filter ?? '');

commit 3cef0bb5d1e3f74eb8ca8b46103f7bc2b7099927

Fix pagination order in /for_language/{lang}
Order keys in paginate setting that do not belong to
the main table get silently removed, unless specified
in the whitelist of sortable fields.

Regression introduced by b92767b9197d0d3a7bc5684baa3c9f95283ee22a.

Closes #3314.

src/Controller/UsersController.php
@@ -551,6 +551,7 @@ class UsersController extends AppController
 
         $this->paginate = [
             'order' => ['UsersLanguages.level' => 'DESC', 'Users.last_contribution' => 'DESC'],
+            'sortableFields' => ['UsersLanguages.level', 'Users.last_contribution'],
             'limit' => 30,
         ];
         $query = $UsersLanguages->find('paginated', compact('lang'));