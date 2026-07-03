e6e2fabf2fa46f82afe451037ac25a7598288b16
時流 <jiru@users.noreply.github.com>
2026-07-03 09:20:08 +0000

    Fix response packet parsing in Manticore client
    This commit fixes the following problem.
    
    If the response packet meets the following criteria:
    
    - payload_length % 8192 == 1
    - last byte of the payload is 0x30 ("0")
    
    The client will fail to parse correctly and return an error such as:
    
    > failed to read searchd response (status=0, ver=295, len=8193, read=8192)

--- src/Lib/SphinxClient.php
+++ src/Lib/SphinxClient.php
@@ -647,7 +647,7 @@ class SphinxClient
 			while ( $left>0 && !feof($fp) )
 			{
 				$chunk = fread ( $fp, min ( 8192, $left ) );
-				if ( $chunk )
+				if ( $chunk !== false )
 				{
 					$response .= $chunk;
 					$left -= strlen($chunk);
