c290d6b10d1740f3e7932cbe34b242b3877a5660
時流 <jiru@users.noreply.github.com>
2026-07-09 14:20:35 +0000

    Automatically rewrite code for CakePHP 5.1
    Using CakePHP 5.x upgrade tool https://github.com/cakephp/upgrade:
    
    > cake upgrade rector --rules cakephp51 <path/to/app/src>

--- config/app.php
+++ config/app.php
@@ -116,7 +116,7 @@ return [
          * Duration will be set to '+2 minutes' in bootstrap.php when debug = true
          * If you set 'className' => 'Null' core cache will be disabled.
          */
-        '_cake_core_' => [
+        '_cake_translations_' => [
             'className' => FileEngine::class,
             'prefix' => 'myapp_cake_core_',
             'path' => CACHE . 'persistent' . DS,
