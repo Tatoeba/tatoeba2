e2fee350a66cffe8e23327fc6135ef873b293284
Davd-B <7666122@studenti.unige.it>
2026-07-11 00:13:03 +0200

    Updated Creative Commons Logo to SVG 36x36

--- templates/element/foot.php
+++ templates/element/foot.php
@@ -201,7 +201,7 @@
     </div>
 
     <div layout="row" layout-align="center center" class="license">
-        <img alt="Creative Commons License" src="/img/cc-logo.png" />
+        <img alt="Creative Commons License" src="/img/cc-logo.svg" width="36" height="36" style="width: 36px; height: 36px;" />
         <div class="text">
             <?php
             echo __(
